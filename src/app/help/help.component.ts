d1ab4e3fe466b19e80a0940542ca16684661f1e8
Shawn <shawn@practera.com>
2018-10-18 15:19:14 +1100

    [AV2-19]
    structures

---
+++ src/app/help/help.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help',
+  templateUrl: 'help.component.html',
+  styleUrls: ['help.component.scss']
+})
+export class HelpComponent {}
--- src/app/setting/setting.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-setting',
-  templateUrl: 'setting.component.html',
-  styleUrls: ['setting.component.scss']
-})
-export class SettingComponent {}
