d29e8911ac56cba1f7954abfba58b9ba7bb7d3cb
Ronak Amani <ronak@practera.com>
2018-10-19 16:03:53 +1100

    [av2-18]/add progress bar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,28 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
 import { ActivityCardComponent } from '../components/activity-card/activity-card.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#2bbfd4",
+      innerStrokeColor: "#2bbfd4",
+      animationDuration: 300,
+    
+    }),
+    
     RouterModule.forChild([{ path: '', component: HomeComponent }])
   ],
-  declarations: [HomeComponent, ActivityCardComponent]
+  declarations: [HomeComponent, ActivityCardComponent],
+ 
 })
-export class HomeModule {}
+export class HomeModule {
+}
