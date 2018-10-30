bb36801ac3c3457292a68a3d8c7f978a440fafec
Ronak <ronakamani@gmail.com>
2018-10-30 21:22:18 +1100

    [av2-15]/inject activity service in home

---
+++ src/app/services/activity.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityService {
+  public activity = {
+          name :'Activity Name 1'
+    }
+  constructor() {};
+}
