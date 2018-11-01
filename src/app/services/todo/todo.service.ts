d5dd838b883f0961beb91c7d2d14ed05bd0c71b0
Shawn <shawn@practera.com>
2018-11-01 13:07:39 +1100

    [AV2-15]
    indentation

--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -5,17 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class TodoService {
   todo = [
-    { todoType: 'feedback',
+    { 
+      todoType: 'feedback',
       assessmentName :'demo assessmnet',
       teamName: 'Team1',
       time: 'today',
       reviewer : {
         name:'mentor1',
         assignOn: 'today'
-
       }
     }
-];
+  ];
 
   constructor() {};
 }
