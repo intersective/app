b574f6c3467cb9d8265591c56d22f7951fc3345d
Sau Shen Chaw <cshen_581@hotmail.com>
2018-11-05 18:21:49 +0800

    [AV2-9] added request module & interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,26 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RequestModule } from './shared/request/request.module'; 
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+  	BrowserModule, 
+  	IonicModule.forRoot(), 
+  	AppRoutingModule,
+	RequestModule.forRoot({ 
+		appkey: 'b11e7c189b',
+		prefixUrl: 'http://local.practera.com/',
+	}),
+  ],
   providers: [
-    StatusBar,
-    SplashScreen,
+    // StatusBar,
+    // SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
