247581213deb4611830ae9f30931ea79933eda36
Sau Shen Chaw <cshen_581@hotmail.com>
2018-11-05 18:21:49 +0800

    [AV2-9] added request module & interceptor

---
+++ src/app/shared/request/request.module.ts
@@ -0,0 +1,36 @@
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { RequestConfig } from './request.service';
+import { RequestService } from './request.service';
+import { RequestInterceptor } from './request.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+@NgModule({
+  imports: [ CommonModule ],
+  providers: [
+    RequestService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true,
+    },
+  ],
+})
+export class RequestModule {
+  constructor(@Optional() @SkipSelf() parentModule: RequestModule) {
+    if (parentModule) {
+      throw new Error(
+        'RequestModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
+  static forRoot(config: RequestConfig): ModuleWithProviders {
+    return {
+      ngModule: RequestModule,
+      providers: [
+        { provide: RequestConfig, useValue: config }
+      ]
+    };
+  }
+}
---
+++ src/app/shared/request/request.service.ts
@@ -0,0 +1,113 @@
+import { Injectable, Optional } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import * as _ from 'lodash';
+
+export class RequestConfig {
+  appkey = '';
+  prefixUrl = 'http://local.practera.com/';
+}
+
+@Injectable()
+export class RequestService {
+  private appkey: string;
+  private prefixUrl: string;
+
+  constructor(
+    private http: HttpClient,
+    @Optional() config: RequestConfig
+  ) {
+    if (config) {
+      this.appkey = config.appkey;
+      this.prefixUrl = config.prefixUrl;
+    }
+  }
+
+  /**
+   *
+   * @param {'Content-Type': string } header
+   * @returns {HttpHeaders}
+   */
+  appendHeaders(header = {'Content-Type': 'application/json'}) {
+    let headers = new HttpHeaders(header);
+    return headers;
+  }
+
+  /**
+   *
+   * @param options
+   * @returns {any}
+   */
+  setParams(options) {
+    let params: any;
+    if (!_.isEmpty(options)) {
+      params = new HttpParams();
+      _.each(options, (value, key) => {
+        params = params.append(key, value);
+      });
+    }
+    return params;
+  }
+
+  /**
+   *
+   * @param {string} endPoint
+   * @param options
+   * @param headers
+   * @returns {Observable<any>}
+   */
+  get(endPoint: string = '', options?: any, headers?: any): Observable<any> {
+    return this.http.get<any>(this.prefixUrl + endPoint, {
+      headers: this.appendHeaders(headers),
+      params: this.setParams(options)
+    }).pipe(
+      catchError(this.handleError<any>('API Request'))
+    );
+  }
+
+  post(endPoint: string = '', data, options?: any, headers?: any): Observable<any> {
+    return this.http.post<any>(this.prefixUrl + endPoint, data, {
+      headers: this.appendHeaders(headers),
+      params: this.setParams(options)
+    }).pipe(
+      catchError(this.handleError<any>('API Request'))
+    );
+  }
+
+
+  /**
+   *
+   * @returns {string}
+   */
+  public getPrefixUrl() {
+    return this.prefixUrl;
+  }
+
+  /**
+   *
+   * @returns {string}
+   */
+  public getAppkey() {
+    return this.appkey;
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  // further enhance this for error reporting (piwik)
+  private log(message: string) {
+    console.log(message);
+  }
+}
