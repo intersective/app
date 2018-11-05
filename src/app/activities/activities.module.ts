70dad751c5f7c4878b34f240b98b30e20920655e
Ronak Amani <ronak@practera.com>
2018-11-05 16:16:56 +1100

    [av2-22]/base of activity

--- src/app/activities/activities.module.ts
+++ src/app/activities/activities.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivitiesComponent } from './activities.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    NgCircleProgressModule,
     RouterModule.forChild([{ path: '', component: ActivitiesComponent }])
   ],
   declarations: [ActivitiesComponent]
