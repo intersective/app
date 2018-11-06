8f05f3cf37ad512327fc37346cad956dd7dd4dc7
Shawn <shawn@practera.com>
2018-11-06 13:15:41 +1100

    [AV2-61]

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -6,26 +6,26 @@ import { Injectable } from '@angular/core';
 
 export class HomeService {
   todo = [
-    // { 
-    //   todoType: 'review',
-    //   assessmentName :'demo assessmnet',
-    //   teamName: 'Team1',
-    //   time: 'today',
-    //   reviewer : {
-    //     name:'mentor1',
-    //     assignOn: 'today'
-    //   }
-    // },
-    // { 
-    //   todoType: 'review',
-    //   assessmentName :'demo assessmnet2',
-    //   teamName: 'Team1',
-    //   time: '1/1/2009',
-    //   reviewer : {
-    //     name:'mentor1',
-    //     assignOn: 'today'
-    //   }
-    // }
+    { 
+      todoType: 'review',
+      assessmentName :'demo assessmnet',
+      teamName: 'Team1',
+      time: 'today',
+      reviewer : {
+        name:'mentor1',
+        assignOn: 'today'
+      }
+    },
+    { 
+      todoType: 'review',
+      assessmentName :'demo assessmnet2',
+      teamName: 'Team1',
+      time: '1/1/2009',
+      reviewer : {
+        name:'mentor1',
+        assignOn: 'today'
+      }
+    }
   ];
   activity = {
     name :'Activity Name 1'
