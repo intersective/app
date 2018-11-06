aa34bc2d30057280aa92fe7b85aa3c20b286dee6
Shawn <shawn@practera.com>
2018-11-06 17:03:28 +1100

    [AV2-23]

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -2,22 +2,39 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { ActivityService } from './activity.service';
 
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.scss']
 })
-export class ActivityDetailComponent implements OnInit {
-   
-  activity: {}; 
+export class ActivityComponent implements OnInit {
+  
+  id = 0;
+  activity = {
+    name: '',
+    description: '',
+    tasks: []
+  };
 
   constructor(
     private route: ActivatedRoute,
-    private service: ActivitiesService
+    private activityService: ActivityService
   ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.activityService.getActivity(this.id)
+      .subscribe(activity => this.activity = activity);
+  }
+
+  back() {
+    console.log('go back');
+  }
+
+  goto(type, id) {
+    console.log('go to ', type, ' with id ', id);
   }
 
 }
---
+++ src/app/activity/activity.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ActivityService {
+
+  activity = {
+    name: 'Activity1',
+    description: 'This is the description of activity 1',
+    tasks: [
+      {
+        id: 1,
+        type: 'Topic',
+        name: 'Topic name',
+        status: 'done'
+      },
+      {
+        id: 2,
+        type: 'Comm',
+        name: 'Comm name',
+        status: ''
+      },
+      {
+        id: 3,
+        type: 'Assessment',
+        name: 'Assessment name',
+        status: 'pending review'
+      },
+      {
+        id: 4,
+        type: 'Assessment',
+        name: 'Assessment name',
+        status: ''
+      },
+      {
+        id: 5,
+        type: 'Assessment',
+        name: 'Assessment name',
+        status: 'published'
+      }
+    ]
+  };
+
+  constructor() {};
+
+  getActivity(id): Observable {
+    return of(this.activity);
+  }
+}
