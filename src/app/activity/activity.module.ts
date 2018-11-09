22b210089014e1fba563a30a415a5517c996b873
Shawn <shawn@practera.com>
2018-11-09 17:03:39 +1100

    activity service

--- src/app/activity/activity.module.ts
+++ src/app/activity/activity.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivityComponent } from './activity.component';
+import { ActivityService } from './activity.service';
 
 @NgModule({
   imports: [
@@ -12,6 +13,9 @@ import { ActivityComponent } from './activity.component';
   ],
   declarations: [
     ActivityComponent
+  ],
+  providers: [
+    ActivityService
   ]
 })
 export class ActivityModule {}
