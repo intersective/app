081b3800d8db8a8f793603162d48847d8e658095
Ronak Amani <ronak@practera.com>
2018-11-13 10:04:35 +1100

    [av2-22]/use service for getting milestons

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,108 +1,32 @@
 import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
 import { Location } from  '@angular/common';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { MilestoneService } from './project.service';
 
 @Component({
   selector: 'app-project',
   templateUrl: 'project.component.html',
   styleUrls: ['project.component.scss']
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit{
 
-  constructor(location: Location) {};
+  constructor(
+    location: Location,
+    // private route: ActivatedRoute,
+    private milestoneService: MilestoneService) {};
+
   location: Location;
+  public activeMileStoneId = '';
+
   
-  public levels = [
-    {
-      id: 1,
-      name: 'fundamental',
-      project_id: 55,
-      progress :1,
-      description: ' You are now part of the learning community. Whether you are a Project Stakeholder, Consulting Mentor or a University Student about to embark on your first consulting project, welcome. This Fundamentals milestone will provide you with an overview, explain your role and provide you with tips and tricks for a successful learning experience ',
-      Activity :[
-        {
-          name: 'Test Activity one',
-          is_locked: false,
-          progress: 0.34,
-          hasFeedback: true
-        },
-        {
-          name: 'Activity two',
-          is_locked: true,
-          progress: 0.74
-          
-        }
-    ],
-      is_locked: false
-    },
+  public levels = [];
     
-
-    {
-      id: 2,
-      name: 'delivery',
-      description:'', 
-      project_id: 55,
-      progress: 0.62,
-      Activity :[
-        {
-          name: 'Test Activity three',
-          is_locked: false,
-          progress: 0.47
-        },
-        {
-          name: 'Activity four',
-          is_locked: false,
-          progress:0.98
-        },
-        {
-          name: 'Test Activity five',
-          is_locked: false,
-          progress: 0.47
-        },
-    ],
-      
-      is_locked: false
-
-    },
-    {
-      id: 3,
-      name: 'communication',
-      description: 'there is some dummy text',
-      progress: 1,
-      project_id: 55,
-      Activity :[
-        {
-          name: 'Test Activity six',
-          is_locked: false,
-          progress: 1,
-          hasFeedback: false
-        },
-        {
-          name: 'Activity seven',
-          is_locked: false,
-          progress:0.48,
-          hasFeedback: true
-          
-        }
-    ],
-      is_locked: true
-
-    },
-    {
-      id: 4,
-      name: 'last',
-      description: 'The deliver phase of a project is all about getting the work done. During this phase your team will likely work both independently and collectively to research and ultimately present your findings. Throughout this phase you will also complete a second employability skill assessment to see how you feel your skills are development and reflect on the journey so far in a reflection journal entry.',
-      progress: 0.45,
-      project_id: 65,
-      Activity :[
-        {
-          name: 'Test Activity six',
-          is_locked: false,
-          progress: 1
-        },
-    ],
-      is_locked: false
-
-    }
-  ];
+  ngOnInit() {
+    // this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.milestoneService.getMilestons()
+      .subscribe(levels => this.levels = levels);
+  }
 
 }
