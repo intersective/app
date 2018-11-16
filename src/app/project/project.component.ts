7ad3012ae89b8d8a183ec724dcfd040b75aedaa1
Ronak Amani <ronak@practera.com>
2018-11-16 16:00:21 +1100

    [av2-22]/find the position of DOM elements

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef  } from '@angular/core';
 import { Location } from  '@angular/common';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { MilestoneService } from './project.service';
+import { Content } from '@ionic/angular';
 
 
 @Component({
@@ -12,7 +13,8 @@ import { MilestoneService } from './project.service';
   styleUrls: ['project.component.scss']
 })
 export class ProjectComponent implements OnInit{
-
+  @ViewChild("milestones") milstones: ElementRef<any>;
+  
   constructor(
     location: Location,
     private router: Router,
@@ -28,8 +30,40 @@ export class ProjectComponent implements OnInit{
     // this.id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.milestoneService.getMilestons()
       .subscribe(levels => this.levels = levels);
+      
   }
-  // activityRedirection(id) {
-  //   this.router.navigate(['pages', 'tabs', { outlets: { activity: ['activity', id] } }]);
-  // }
+  activityRedirection(id) {
+    this.router.navigate(['pages', 'tabs', { outlets: { activity: ['activity', id] } }]);
+  }
+
+  scroll(ev) {
+    var scrollTopCurrent = ev.detail.scrollTop;
+    var scrollTopMax = ev.detail.event.path[0].clientHeight;
+    // distance from the bottom to scroll
+    var scrollBottom = scrollTopMax - scrollTopCurrent;
+    //var scrollLength = 286;
+    console.log('scrollTopCurrent', scrollTopCurrent);
+    console.log('scrollTopMax', scrollTopMax);
+    console.log('scrollBottom', scrollBottom);
+    
+   }
+
+   ngAfterViewChecked() {
+    let elementRef = this.milstones;
+    let length = this.milstones.nativeElement.children.length;
+    let milestonesHeigth = [];
+    
+    //console.log('element ref is:',elementRef);
+    
+    //console.log('element offsetHeight is:',elementRef.nativeElement.children[length-1].offsetHeight);
+    //console.log('element relative position is:',elementRef.nativeElement.getBoundingClientRect().top);
+    
+    if (elementRef) {
+      for (let i=0; i< length; i++) {
+        milestonesHeigth[i] = this.milstones.nativeElement.children[i].clientHeight;
+      } 
+    }
+    console.log('milestonesHeigth:', milestonesHeigth);  
+  }
+   
 }
