14f983aeee452c4312102ec0d61107b6d1ede2b2
Ronak Amani <ronak@practera.com>
2018-11-19 15:59:30 +1100

    [av2-53]/topic component

--- src/app/topic/topic.module.ts
+++ src/app/topic/topic.module.ts
@@ -2,15 +2,20 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { TopicComponent } from './topic.component';
+import { TopicComponent} from './topic.component';
 import { TopicService } from './topic.service';
-import { TopicRoutingModule } from './topic-routing.module'
+import { TopicRoutingModule } from './topic-routing.module';
+
+
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TopicRoutingModule
+    TopicRoutingModule,
+   
+
   ],
   declarations: [
     TopicComponent,
--- src/app/topic/topic.service.ts
+++ src/app/topic/topic.service.ts
@@ -8,8 +8,24 @@ export class TopicService {
   topic = { 
     id: 1,
     title: 'Welcome and Warm-up',
-    content: 'Welcome to Practera Phase 2, great to see you on-board!',
+    content: '<div>At the end of this activity you will submit an agreed Project Charter explaining what and how your team will deliver value to your Project Stakeholder. A Project Charter is a great way to ensure team, project stakeholder and consulting mentor are in agreement on the work to be delivered and how it will get achieved. Below is a series of steps and instructions that will guide you and your team through the process of creating a project charter.</div><div><br></div><div><b>STEP 1: Organise a Kick-Off meeting with your team and consulting mentor</b></div>',
     videolink: 'https://vimeo.com/157064711',
+    files: [
+      {
+        "name": "JS2_Program_Overview_2016Aug-1.pdf",
+        "extension": ".pdf",
+        "path": "s3://practera/uploads/b14a7b8059d9c055954c92674ce60032/pHzgZpjQ6eCDPXPWovhT_JS2_Program_Overview_2016Aug-1.pdf",
+        "type": "application/pdf",
+        "size": 1006217,
+        "visibility": "program",
+        " experience_id": 44,
+        "program_id": 176,
+        "slug": "https://www.filepicker.io/api/file/seqhKHMNQnmxeXBxsiKQ",
+        "model": "Story.Topic",
+        "foreign_key": 3460
+      }
+    ]
+
   };
 
   constructor() { }
