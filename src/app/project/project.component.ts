d32c74cae0cc903b7a280c01632ded38dd79cc35
Ronak <ronakamani@gmail.com>
2018-11-20 11:37:35 +1100

    [av2-53]/almost done

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ElementRef,
+  ViewChild
+} from "@angular/core";
+import { Location } from "@angular/common";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { MilestoneService } from "./project.service";
 
 @Component({
-  selector: 'app-project',
-  templateUrl: 'project.component.html',
-  styleUrls: ['project.component.scss']
+  selector: "app-project",
+  templateUrl: "project.component.html",
+  styleUrls: ["project.component.scss"]
 })
-export class ProjectComponent {}
+export class ProjectComponent implements OnInit {
+  
+  constructor(
+    location: Location,
+    private router: Router,
+    private milestoneService: MilestoneService
+  ) {}
+
+  location: Location;
+  public activeMileStoneId = "";
+  public levelPos1: number;
+
+  public levels = [];
+
+  ngOnInit() {
+    // this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.milestoneService
+      .getMilestons()
+      .subscribe(levels => (this.levels = levels));
+  }
+
+  // activityRedirection(id) {
+  //   this.router.navigate(['pages', 'tabs', { outlets: { activity: ['activity', id] } }]);
+  // }
+  // ionViewDidLoad(): void {
+  //   this.levelPos1 = this.level1.nativeElement.getBoundingClientRect().top;
+  // }
+
+  // scrollTo(x: number, y: number, duration: number): void {
+  //   this.content.scrollTo(x, y, duration);
+  // }
+  // scrollToTop(): void {
+  //   this.content.scrollToTop(750);
+  // }
+  // scrollToBottom() {
+  //   this.content.scrollToBottom(750);
+  // }
+}
