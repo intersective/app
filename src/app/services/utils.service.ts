01bab4d62d9d821ac9856b8277a082efc8780f61
Shawn <shawn@practera.com>
2018-11-20 13:24:46 +1100

    pop up refactor

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
+import { ModalController } from '@ionic/angular';
+import { PopUpComponent } from '@components/pop-up/pop-up.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,9 @@ import * as _ from 'lodash';
 export class UtilsService {
   private lodash;
 
-  constructor() {
+  constructor(
+    public modalController: ModalController
+  ) {
   	if (_) {
 	  	this.lodash = _;
   	} else {
@@ -22,5 +26,33 @@ export class UtilsService {
 
   each(collections, callback) {
     return this.lodash.each(collections, callback);
+  }
+
+  unset(object, path) {
+    return this.lodash.unset(object, path);
+  }
+   find(collections, callback) {
+    return this.lodash.find(collections, callback);
+  }
+   indexOf(array, value, fromIndex=0) {
+    return this.lodash.indexOf(array, value, fromIndex);
+  }
+   remove(collections, callback) {
+    return this.lodash.remove(collections, callback);
+  }
+
+  // show pop up message 
+  // this is using pop-up.component.ts as the view
+  // put redirect = false if don't need to redirect
+  async popUp(type, data, redirect) {
+    const modal = await this.modalController.create({
+      component: PopUpComponent,
+      componentProps: { 
+        type: type,
+        data: data,
+        redirect: redirect
+      }
+    });
+    return await modal.present();
   }
 }
