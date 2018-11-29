498a227e7e145e61ddd690f8975a3189ba5a8267
Ronak Amani <ronak@practera.com>
2018-11-29 15:49:42 +1100

    add routing module

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -35,6 +35,11 @@ const routes: Routes = [
         loadChildren: '../activity/activity.module#ActivityModule'
       },
       {
+        path: 'reviews',
+        outlet: 'reviews',
+        loadChildren: '../reviews/reviews.module#ReviewsModule'
+      },
+      {
         path: 'chat',
         outlet: 'chat',
         component: ChatListComponent
