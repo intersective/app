10416bd5be5a1f8ec137d9d65e3c3c2f7c3e3d6c
Ronak Amani <ronak@practera.com>
2018-11-30 09:58:39 +1100

    comment4,5 of review-clean the project component

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProjectService } from './project.service';
 
 @Component({
@@ -11,20 +9,17 @@ import { ProjectService } from './project.service';
 })
 export class ProjectComponent implements OnInit{
 
-  @ViewChild("milestones") milstones: ElementRef<any>;
 
   constructor(
     private router: Router,
     private projectService: ProjectService
    ) {};
 
-  public milestonesHeigth = [];
-  public levels = [];
+  public milestons = [];
     
   ngOnInit() {
-    // this.id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.projectService.getMilestons()
-      .subscribe(levels => this.levels = levels);
+      .subscribe(levels => this.milestons = levels);
   }
 
   goto(id) {
@@ -35,16 +30,6 @@ export class ProjectComponent implements OnInit{
     //do what ever logic you need to come up with the unique identifier of your item in loop, I will just return the object id.
     return level.id ? level.id : undefined;
   }
- 
-  ngAfterViewChecked() {
-    let elementRef = this.milstones;
-    let length = elementRef.nativeElement.children.length;
-    
-    if (elementRef) {
-      for (let i=0; i< length; i++) {
-        this.milestonesHeigth[i] = this.milstones.nativeElement.children[i].clientHeight;
-      } 
-    }
-  }
+  
    
 }
