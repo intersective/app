900e66f5b46bfab0797b60b14c15ce806b914875
Ronak Amani <ronak@practera.com>
2018-11-29 12:19:11 +1100

    ready for code review

--- src/app/topic/topic.service.ts
+++ src/app/topic/topic.service.ts
@@ -8,7 +8,7 @@ export class TopicService {
   topic = { 
     id: 1,
     title: 'Welcome and Warm-up',
-    //content: '<div>At the end of this activity you will submit an agreed Project Charter explaining what and how your team will deliver value to your Project Stakeholder. A Project Charter is a great way to ensure team, project stakeholder and consulting mentor are in agreement on the work to be delivered and how it will get achieved. Below is a series of steps and instructions that will guide you and your team through the process of creating a project charter.</div><div><br></div><div><b>STEP 1: Organise a Kick-Off meeting with your team and consulting mentor</b></div>',
+    content: '<div>At the end of this activity you will submit an agreed Project Charter explaining what and how your team will deliver value to your Project Stakeholder. A Project Charter is a great way to ensure team, project stakeholder and consulting mentor are in agreement on the work to be delivered and how it will get achieved. Below is a series of steps and instructions that will guide you and your team through the process of creating a project charter.</div><div><br></div><div><b>STEP 1: Organise a Kick-Off meeting with your team and consulting mentor</b></div>',
     videolink: 'https://vimeo.com/157064711',
     files: [
       {
@@ -23,7 +23,7 @@ export class TopicService {
         "slug": "https://www.filepicker.io/api/file/seqhKHMNQnmxeXBxsiKQ",
         "model": "Story.Topic",
         "foreign_key": 3460
-      }
+      },
     ]
 
   };
