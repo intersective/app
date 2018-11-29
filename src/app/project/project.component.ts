fb516edc34e3ae6e6facc69f8db37a6c708d6810
Shawn <shawn@practera.com>
2018-11-29 15:37:50 +1100

    [AV2-22]
    indentations

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -4,41 +4,41 @@ import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ProjectService } from './project.service';
 
-
 @Component({
   selector: 'app-project',
   templateUrl: 'project.component.html',
   styleUrls: ['project.component.scss']
 })
 export class ProjectComponent implements OnInit{
+
   @ViewChild("milestones") milstones: ElementRef<any>;
 
   constructor(
     private router: Router,
-    private projectService: ProjectService) {};
+    private projectService: ProjectService
+   ) {};
 
-    public milestonesHeigth = [];
-    public levels = [];
+  public milestonesHeigth = [];
+  public levels = [];
     
   ngOnInit() {
     // this.id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.projectService.getMilestons()
       .subscribe(levels => this.levels = levels);
-      
   }
+
   goto(id) {
     console.log ('activity.id is:', id);
   }
-  trackLevel(index,level){
+
+  trackLevel(index, level){
     //do what ever logic you need to come up with the unique identifier of your item in loop, I will just return the object id.
     return level.id ? level.id : undefined;
-   }
+  }
  
-   
-   ngAfterViewChecked() {
+  ngAfterViewChecked() {
     let elementRef = this.milstones;
     let length = elementRef.nativeElement.children.length;
-    
     
     if (elementRef) {
       for (let i=0; i< length; i++) {
--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -7,127 +7,115 @@ import { Observable, of } from 'rxjs';
 export class ProjectService {
   levels = [
     {
-    id: 1,
-    name: 'fundamental',
-    project_id: 55,
-    progress :1,
-    description: ' You are now part of the learning community. Whether you are a Project Stakeholder, Consulting Mentor or a University Student about to embark on your first consulting project, welcome. This Fundamentals milestone will provide you with an overview, explain your role and provide you with tips and tricks for a successful learning experience ',
-    Activity :[
-      {
-        id: 101,
-        name: 'Test Activity one',
-        is_locked: false,
-        progress: 0.34,
-        hasFeedback: true,
-        is_hidden: false
-      },
-      {
-        id: 102,
-        name: 'Activity two',
-        is_locked: true,
-        progress: 0.74,
-        lead_image: '/src/assets/home-page-img/header-bg.jpg',
-        is_hidden: false
-        
-      },
-      {
-        id: 102,
-        name: 'Activity two',
-        is_locked: true,
-        progress: 0.74,
-        lead_image: '/src/assets/home-page-img/header-bg.jpg',
-        is_hidden: false
-        
-      }
-
-
-
-      
-  ],
-    is_locked: false
-  },
-  
-
-  {
-    id: 2,
-    name: 'delivery',
-    description:'', 
-    project_id: 55,
-    progress: 0.62,
-    Activity :[
-      { 
-        id: 103,
-        name: 'Test Activity three',
-        is_locked: false,
-        progress: 0.47,
-        is_hidden: false
-      },
-      {
-        id: 104,
-        name: 'Activity four',
-        is_locked: false,
-        progress:0.98,
-        is_hidden: false
-      },
-      {
-        id: 105,
-        name: 'Test Activity five',
-        is_locked: false,
-        progress: 0.47,
-        is_hidden: false
-      },
-  ],
-    
-    is_locked: false
-
-  },
-  {
-    id: 3,
-    name: 'communication',
-    description: 'there is some dummy text',
-    progress: 1,
-    project_id: 55,
-    Activity :[
-      {
-        id: 106,
-        name: 'Test Activity six',
-        is_locked: false,
-        progress: 1,
-        hasFeedback: false,
-        is_hidden: false
-      },
-      {
-        id: 107,
-        name: 'Activity seven',
-        is_locked: false,
-        progress:0.48,
-        hasFeedback: true,
-        is_hidden: false
-        
-      }
-  ],
-    is_locked: true
-
-  },
-  {
-    id: 4,
-    name: 'last',
-    description: 'The deliver phase of a project is all about getting the work done. During this phase your team will likely work both independently and collectively to research and ultimately present your findings. Throughout this phase you will also complete a second employability skill assessment to see how you feel your skills are development and reflect on the journey so far in a reflection journal entry.',
-    progress: 0.45,
-    project_id: 65,
-    Activity :[
-      {
-        id: 108,
-        name: 'Test Activity six',
-        is_locked: false,
-        progress: 1,
-        is_hidden: false
-      },
-  ],
-    is_locked: false
-
-  }
-];
+      id: 1,
+      name: 'fundamental',
+      project_id: 55,
+      progress :1,
+      description: ' You are now part of the learning community. Whether you are a Project Stakeholder, Consulting Mentor or a University Student about to embark on your first consulting project, welcome. This Fundamentals milestone will provide you with an overview, explain your role and provide you with tips and tricks for a successful learning experience ',
+      Activity :[
+        {
+          id: 101,
+          name: 'Test Activity one',
+          is_locked: false,
+          progress: 0.34,
+          hasFeedback: true,
+          is_hidden: false
+        },
+        {
+          id: 102,
+          name: 'Activity two',
+          is_locked: true,
+          progress: 0.74,
+          lead_image: '/src/assets/home-page-img/header-bg.jpg',
+          is_hidden: false
+        },
+        {
+          id: 102,
+          name: 'Activity two',
+          is_locked: true,
+          progress: 0.74,
+          lead_image: '/src/assets/home-page-img/header-bg.jpg',
+          is_hidden: false
+        } 
+      ],
+      is_locked: false
+    },
+    {
+      id: 2,
+      name: 'delivery',
+      description:'', 
+      project_id: 55,
+      progress: 0.62,
+      Activity :[
+        { 
+          id: 103,
+          name: 'Test Activity three',
+          is_locked: false,
+          progress: 0.47,
+          is_hidden: false
+        },
+        {
+          id: 104,
+          name: 'Activity four',
+          is_locked: false,
+          progress:0.98,
+          is_hidden: false
+        },
+        {
+          id: 105,
+          name: 'Test Activity five',
+          is_locked: false,
+          progress: 0.47,
+          is_hidden: false
+        },
+      ],
+      is_locked: false
+    },
+    {
+      id: 3,
+      name: 'communication',
+      description: 'there is some dummy text',
+      progress: 1,
+      project_id: 55,
+      Activity :[
+        {
+          id: 106,
+          name: 'Test Activity six',
+          is_locked: false,
+          progress: 1,
+          hasFeedback: false,
+          is_hidden: false
+        },
+        {
+          id: 107,
+          name: 'Activity seven',
+          is_locked: false,
+          progress:0.48,
+          hasFeedback: true,
+          is_hidden: false
+          
+        }
+      ],
+      is_locked: true
+    },
+    {
+      id: 4,
+      name: 'last',
+      description: 'The deliver phase of a project is all about getting the work done. During this phase your team will likely work both independently and collectively to research and ultimately present your findings. Throughout this phase you will also complete a second employability skill assessment to see how you feel your skills are development and reflect on the journey so far in a reflection journal entry.',
+      progress: 0.45,
+      project_id: 65,
+      Activity :[
+        {
+          id: 108,
+          name: 'Test Activity six',
+          is_locked: false,
+          progress: 1,
+          is_hidden: false
+        },
+      ],
+      is_locked: false
+    }
+  ];
 
   constructor() { }
   getMilestons(): Observable<any> {
