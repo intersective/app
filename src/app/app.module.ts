d96396386cce62f67a089ac04a1f146214b2b844
Shawn <shawn@practera.com>
2018-11-30 13:00:32 +1100

    [AV2-97]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
-import { RequestModule } from '@shared/request/request.module'; 
+import { RequestModule } from '@shared/request/request.module';
+import { NotificationModule } from '@shared/notification/notification.module';
 import { AuthModule } from './auth/auth.module';
 import { TabsModule } from './tabs/tabs.module';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PopUpComponent } from '@components/pop-up/pop-up.component';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { UtilsService } from './services/utils.service';
@@ -22,11 +22,7 @@ import { UtilsService } from './services/utils.service';
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    PopUpComponent,
     AppComponent
-  ],
-  entryComponents: [
-    PopUpComponent
   ],
   imports: [
   	BrowserModule, 
@@ -37,6 +33,7 @@ import { UtilsService } from './services/utils.service';
       appkey: 'b11e7c189b',
       prefixUrl: 'https://sandbox.practera.com/',
     }),
+    NotificationModule,
   	AppRoutingModule,
   ],
   providers: [
