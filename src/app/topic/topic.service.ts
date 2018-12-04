c48c2abb1810e6bb2b78a1ea9c66086a950ed01e
Ronak Amani <ronak@practera.com>
2018-12-04 15:09:30 +1100

    code review changes

--- src/app/topic/topic.service.ts
+++ src/app/topic/topic.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HtmlTagDefinition, ArrayType } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TopicService {
-  topic = { 
+  topic: {
+    id: number,
+    title: string,
+    content: string,
+    videolink: string,
+    files: object,
+  } = 
+  { 
     id: 1,
     title: 'Welcome and Warm-up',
     content: '<div>At the end of this activity you will submit an agreed Project Charter explaining what and how your team will deliver value to your Project Stakeholder. A Project Charter is a great way to ensure team, project stakeholder and consulting mentor are in agreement on the work to be delivered and how it will get achieved. Below is a series of steps and instructions that will guide you and your team through the process of creating a project charter.</div><div><br></div><div><b>STEP 1: Organise a Kick-Off meeting with your team and consulting mentor</b></div>',
@@ -20,7 +28,7 @@ export class TopicService {
         "visibility": "program",
         " experience_id": 44,
         "program_id": 176,
-        "slug": "https://www.filepicker.io/api/file/seqhKHMNQnmxeXBxsiKQ",
+        "url": "https://www.filepicker.io/api/file/seqhKHMNQnmxeXBxsiKQ",
         "model": "Story.Topic",
         "foreign_key": 3460
       },
@@ -43,6 +51,4 @@ export class TopicService {
   getTopicIsDone(topicId) {
     return of(this.topicDone[topicId] ? this.topicDone[topicId] : false);
   }
-
-  
 }
