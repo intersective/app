4dc464019d74c51ed15edf0b2db9ec1ed3361b5d
Sau Shen Chaw <cshen_581@hotmail.com>
2018-12-05 13:12:00 +0800

    coreModule ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { RequestModule } from '@shared/request/request.module';
 import { NotificationModule } from '@shared/notification/notification.module';
 import { AuthModule } from './auth/auth.module';
 import { TabsModule } from './tabs/tabs.module';
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { UtilsService } from './services/utils.service';
@@ -16,10 +19,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    
   ],
- 
-
   imports: [
   	BrowserModule, 
     TabsModule,
@@ -28,16 +28,14 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
       appkey: 'b11e7c189b',
       prefixUrl: 'https://sandbox.practera.com/',
     }),
-
     AppRoutingModule,
+    CoreModule,
     NgCircleProgressModule,
     NotificationModule,
-    AuthModule
- 
+    AuthModule,
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-
     // Custom
     UtilsService,
   ],
