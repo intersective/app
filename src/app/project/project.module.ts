213fdcbdb1f5275bd96f7471e86f99b2c014be36
Sau Shen Chaw <cshen_581@hotmail.com>
2018-12-10 10:43:37 +0800

    [AV2-111] removed imports

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,7 +1,5 @@
-import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProjectComponent } from './project.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
