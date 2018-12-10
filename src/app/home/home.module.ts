8f6b11610ac1a808c0aa9a61d816cd3a0f2846b2
Sau Shen Chaw <cshen_581@hotmail.com>
2018-12-10 10:43:37 +0800

    [AV2-111] removed imports

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,4 @@
-import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
 import { HomeComponent } from './home.component';
