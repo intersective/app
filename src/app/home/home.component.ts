b5acc49d9bc86b80c7fe63a678776725244c3293
Ronak <ronakamani@gmail.com>
2018-12-11 18:46:11 +1100

    change the goToreview routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
 import { Router } from '@angular/router';
+import { Activity } from 
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomeComponent implements OnInit {
   progress:number = 80;
   programName:string;
   todoItems = [];
-  activity = {};
+  activity :Activity;
   
   constructor (
     private router: Router,
@@ -29,10 +30,6 @@ export class HomeComponent implements OnInit {
 
   goToActivity(id) {
     this.router.navigateByUrl('app/(project:activity/' + id + ')');
-  }
-
-  goToReview(id) {
-    this.router.navigateByUrl('review/:contextId/:'+ id );
   }
   
 }
