314a5f4829f37fcfc2119e207f1638d2145c88af
Sau Shen Chaw <cshen_581@hotmail.com>
2018-12-12 10:09:53 +0800

    [AV2-58] ionic life cycle & service to pull fastfeedback content

--- src/app/fast-feedback/fast-feedback.module.ts
+++ src/app/fast-feedback/fast-feedback.module.ts
@@ -6,19 +6,24 @@ import { FormsModule } from '@angular/forms';
 import { FastFeedbackRoutingComponent } from './fast-feedback-routing.component';
 import { FastFeedbackComponent } from './fast-feedback.component';
 import { FastFeedbackService } from './fast-feedback.service';
+import { QuestionsModule } from '../questions/questions.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    QuestionsModule,
   ],
   declarations: [
   	FastFeedbackRoutingComponent, 
     FastFeedbackComponent,
   ],
-  entryComponents: [FastFeedbackRoutingComponent],
-  providers:[FastFeedbackService],
-  exports: [FastFeedbackComponent]
+  entryComponents: [ FastFeedbackRoutingComponent ],
+  providers: [ FastFeedbackService ],
+  exports: [
+    FastFeedbackComponent,
+    QuestionsModule,
+  ]
 })
 export class FastFeedbackModule {}
