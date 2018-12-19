0a348e0d78a2a80f33a9fd5efcc102a5ecd87591
Shawn <shawn@practera.com>
2018-12-19 11:57:30 +1100

    [AV2-99]

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TabsService } from './tabs.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.component.html',
   styleUrls: ['tabs.component.scss']
 })
-export class TabsComponent {
-	showTeam = false;
+export class TabsComponent implements OnInit {
 	showReview = false;
   showChat = true;
   noOfTodoItems = 0;
   noOfChats = 0;
+
+  constructor (
+    private tabsService: TabsService,
+    // private utils: UtilsService
+  ) {}
+
+  ngOnInit() {
+    this.tabsService.getNoOfTodoItems()
+      .subscribe(noOfTodoItems => {
+        this.noOfTodoItems = noOfTodoItems;
+      });
+    this.tabsService.getNoOfChats()
+      .subscribe(noOfChats => {
+        this.noOfChats = noOfChats;
+      });
+  }
 }
