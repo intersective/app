3bf37c08fdc5edaf4aa74534655a195a5e4f173d
zawmyohtut <zaw@intersective.com>
2019-01-03 18:12:29 +1100

    [AV2-114] - forgot password service and logic implemented

--- src/app/auth/auth-forgot-password/auth-forgot-password.component.ts
+++ src/app/auth/auth-forgot-password/auth-forgot-password.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UtilsService } from '@services/utils.service';
 import { NotificationService } from '@shared/notification/notification.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth-forgot-password',
@@ -10,21 +11,35 @@ import { NotificationService } from '@shared/notification/notification.service';
 })
 export class AuthForgotPasswordComponent {
   email = '';
+  // variable to control the label of the button
+  isSending = false;
 
   constructor(
     private router: Router,
     private utils: UtilsService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private authService: AuthService
   ) {}
 
-  async send() {
-    // -- todo
+  async send() {   
+    // basic validation
+    if (this.email.length < 0 || !this.email) {
+      return this.notificationService.presentToast('Please enter email.', false);       
+    }
+    this.isSending = true;
     // call API to do forgot password logic
-    
-    // show pop up message for confirmation
-    return this.notificationService.popUp('forgotPasswordConfirmation', {
-      email: this.email
-    }, ['login']);
+    this.authService.forgotPassword(this.email).subscribe(res => {
+      this.isSending = false;
+      if (res) {
+        // show pop up message for confirmation
+        return this.notificationService.popUp('forgotPasswordConfirmation', {
+          email: this.email
+        }, ['login']);
+      } else {
+        return this.notificationService.presentToast('Issue occured. Please try again.', false);       
+      }
+    });
+        
   }
 
 }
