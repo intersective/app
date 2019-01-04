f7e676c75380209c3b0a4c9c97d244682f88311f
Shawn <shawn@practera.com>
2019-01-04 12:10:11 +1100

    [AV2-120]

--- src/app/auth/auth-login/auth-login.component.ts
+++ src/app/auth/auth-login/auth-login.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth.service';
 import { Observable, concat } from 'rxjs';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { NotificationService } from '@shared/notification/notification.service';
+import { UtilsService } from '@services/utils.service';
 
 @Component({
   selector: 'app-auth-login',
@@ -20,10 +21,27 @@ export class AuthLoginComponent {
   constructor(
     private router: Router,
     private authService: AuthService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private utils: UtilsService
   ) {}
 
   login() {
+    if (this.utils.isEmpty(this.loginForm.value.email) || this.utils.isEmpty(this.loginForm.value.password)) {
+      this.notificationService.alert({
+        message: 'Your email or password is empty, please fill them in.',
+        buttons: [
+          {
+            text: 'OK',
+            role: 'cancel',          
+            handler: () => {
+              this.isLoggingIn = false;
+              return;
+            }
+          }
+        ]
+      });
+      return ;
+    }
     this.isLoggingIn = true;
     this.authService.login({
       email: this.loginForm.value.email,
