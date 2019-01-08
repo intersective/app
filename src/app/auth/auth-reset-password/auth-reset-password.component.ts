4e620f86edbbd46b5f351f79b584a01a993c6a6d
zawmyohtut <zaw@intersective.com>
2019-01-08 16:54:31 +1100

    [AV2-114] - reset password UI and service implemented

--- src/app/auth/auth-reset-password/auth-reset-password.component.ts
+++ src/app/auth/auth-reset-password/auth-reset-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { NotificationService } from '../../shared/notification/notification.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth-reset-password',
@@ -7,9 +11,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  key: string;
+
+  verifySuccess = true;
+  isResetting = false;
+
+  resetPasswordForm = new FormGroup({
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl(''),
+  }, { validators: this.checkPasswordMatching });
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private notificationService: NotificationService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.key = this.route.snapshot.paramMap.get('key');
+    this.email = this.route.snapshot.paramMap.get('email');
+
+    if (!this.key || !this.email) {
+      return this._notifyAndRedirect('Invalid reset password link');
+    }   
+    // Call API to verify that key and email parameters from reset password URL are valid 
+    this.authService.verifyUserKeyEmail({key: this.key, email: this.email}).subscribe(res => {      
+      // verification of key and email is successfuly. 
+      this.verifySuccess = true;
+    }, err => {
+      return this._notifyAndRedirect('Invalid reset password link');
+    });
+  }
+
+  resetPassword() {
+    let data = {
+      key: this.key,
+      email: this.email,
+      password: this.resetPasswordForm.controls.password.value,
+      verify_password: this.resetPasswordForm.controls.confirmPassword.value
+    };
+
+    this.authService.resetPassword(data).subscribe(res => {
+      return this._notifyAndRedirect('Password successfully changed!. Please login with the new password.');
+    },err => {
+      return this.notificationService.presentToast('Error updating password.Try again', false);
+    });
+  }
+
+  checkPasswordMatching(resetPasswordForm: FormGroup) { 
+    let password = resetPasswordForm.controls.password.value;
+    let confirmPassword = resetPasswordForm.controls.confirmPassword.value;
+    
+    return password === confirmPassword ? null : { notMatching : true }
+  }
+
+  private _notifyAndRedirect(msg) {
+    this.notificationService.alert({
+      message: msg,
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel',
+          handler: () => {
+            this.router.navigate(['/login']);
+          }
+        }
+      ]
+    });
   }
 
 }
