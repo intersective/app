9f4e7243d84c4edd9fb603a4d35828c69c626142
Shawn <shawn@practera.com>
2019-01-09 17:11:07 +1100

    change api source path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in configurations section of `angular.json`.
 export const environment = {
   production: false,
-  APIEndpoint: 'http://stage.practera.com/',
+  APIEndpoint: 'http://stage-test.practera.com/',
   // APIEndpoint: 'https://sandbox.practera.com/'
   filestack: {
     key: 'AO6F4C72uTPGRywaEijdLz',
