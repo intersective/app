de57ee9607e9f398246ae7ea9d7387e0e43abd2f
Barry Chen <barry@intersective.com>
2019-01-09 12:45:49 +1100

    Update environment.stage.ts
    add filestack configurations.

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,4 +1,17 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'https://stage-test.practera.com/'
+  APIEndpoint: 'https://stage-test.practera.com/',
+  filestack: {
+    key: 'AO6F4C72uTPGRywaEijdLz',
+    s3Config: {
+      location: 's3',
+      container: 'practera-aus',
+      region: 'ap-southeast-2',
+      paths: {
+        any: '/appv2/local/uploads/',
+        image: '/appv2/local/uploads/',
+        video: '/appv2/local/uploads/'
+      }
+    }
+  }
 };
