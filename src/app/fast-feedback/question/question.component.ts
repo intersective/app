894e130b660a6d9c546c218c248e612bce4ddcd1
Shawn <shawn@practera.com>
2019-01-11 15:47:00 +1100

    quick fix

--- src/app/fast-feedback/question/question.component.ts
+++ src/app/fast-feedback/question/question.component.ts
@@ -8,7 +8,7 @@ import { FormGroup }        from '@angular/forms';
 })
 
 export class QuestionComponent {
-  @Input() question: any[];
+  @Input() question: any;
   @Input() form: FormGroup;
 
   constructor() {}
