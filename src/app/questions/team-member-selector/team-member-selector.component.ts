bb9f1f30663da5aa185d7463c914f77b7f03af9c
Shawn <shawn@practera.com>
2019-01-14 11:53:34 +1100

    [AV2-160]

---
+++ src/app/questions/team-member-selector/team-member-selector.component.ts
@@ -0,0 +1,96 @@
+import { Component, Input, forwardRef, ViewChild, ElementRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-team-member-selector',
+  templateUrl: 'team-member-selector.component.html',
+  styleUrls: ['team-member-selector.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => TeamMemberSelectorComponent),
+    }
+  ]
+})
+export class TeamMemberSelectorComponent implements ControlValueAccessor {
+
+  @Input() question;
+  @Input() submission;
+  @Input() review;
+  // this is for doing an assessment or not
+  @Input() doAssessment: Boolean;
+  // this is for doing review or not
+  @Input() doReview: Boolean;
+  // FormControl that is passed in from parent component
+  @Input() control: FormControl;
+  // answer field for submitter & reviewer
+  @ViewChild('answer') answerRef: ElementRef;
+  // comment field for reviewer
+  @ViewChild('comment') commentRef: ElementRef;
+
+  // the value of answer
+  innerValue: any;
+  // validation errors array
+  errors: Array<any> = [];
+
+  constructor() {}
+
+  ngAfterViewInit() {
+  }
+
+  //propagate changes into the form control
+  propagateChange = (_: any) => {}
+
+  // event fired when radio is selected. propagate the change up to the form control using the custom value accessor interface
+  // if 'type' is set, it means it comes from reviewer doing review, otherwise it comes from submitter doing assessment
+  onChange(value, type){
+    //set changed value (answer or comment)
+    if (type) {
+      // initialise innerValue if not set
+      if (!this.innerValue) {
+        this.innerValue = {
+          answer: '',
+          comment: ''
+        };
+      }
+      this.innerValue[type] = value;
+    } else {
+      this.innerValue = value;
+    }
+
+    // propagate value into form control using control value accessor interface
+    this.propagateChange(this.innerValue);
+
+    //reset errors
+    this.errors = [];
+    //setting, resetting error messages into an array (to loop) and adding the validation messages to show below the answer area
+    for (var key in this.control.errors) {
+      if (this.control.errors.hasOwnProperty(key)) {
+        if(key === "required"){
+          this.errors.push("This question is required");
+        }else{
+          this.errors.push(this.control.errors[key]);
+        }
+      }
+    }
+  }
+
+  //From ControlValueAccessor interface
+  writeValue(value: any) {
+    if (value) {
+      this.innerValue = value;
+    }
+  }
+
+  //From ControlValueAccessor interface
+  registerOnChange(fn: any) {
+    this.propagateChange = fn;
+  }
+
+  //From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+
+  }
+
+}
