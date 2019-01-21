278e33efa8ce3849d0606f472378f608635ec63f
Shawn <shawn@practera.com>
2019-01-21 15:14:19 +1100

    [AV2-115]

--- src/app/reviews/reviews.service.ts
+++ src/app/reviews/reviews.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RequestService } from '@shared/request/request.service';
+import { UtilsService } from '@services/utils.service';
+
+const api = {
+  reviews: 'api/reviews.json',
+};
 
 export interface Review {
-  id:number,
+  assessmentId: number,
   isDone: boolean,
   name: string,
   submitterName: string,
-  submittedAssessmnetDate?: string,
-  submittedReviewDate?: string,
+  date?: string,
   teamName?: string,
-  isInTeam: boolean,
   contextId: number
 }
 
@@ -17,42 +22,50 @@ export interface Review {
   providedIn: 'root',
 })
 export class ReviewsService {
-  
-  reviews:Array<Review> = [
-    {
-      id:1,
-      isDone: true,
-      name: 'moderated for mentors',
-      submitterName: 'Ronak',
-      submittedAssessmnetDate: 'Nov 19',
-      teamName: 'one',
-      isInTeam: true,
-      contextId: 3
-    },
-    { id:2,
-      isDone: false,
-      name: 'video test',
-      submitterName: 'Sara',
-      submittedReviewDate: '20 Jun',
-      teamName: 'one',
-      isInTeam: true,
-      contextId: 1
-    },
-    { id:33,
-      isDone: true,
-      name: 'Demo assessment',
-      submitterName: 'Ronak',
-      submittedAssessmnetDate: '15 Nov',
-      teamName: 'two',
-      isInTeam: true,
-      contextId: 2
-    }
-  ]
 
-  constructor() { }
+  constructor(
+    private request: RequestService,
+    private utils: UtilsService,
+  ) { }
 
   getReviews(): Observable<any> {
-    return of(this.reviews);
+    return this.request.get(api.reviews)
+      .pipe(map(response => {
+        if (response.success && response.data) {
+          return this._normaliseReviews(response.data);
+        } else {
+          return [];
+        }
+      })
+    );
   }
-  
+
+  private _normaliseReviews(data) {
+    if (!Array.isArray(data)) {
+      return this.request.apiResponseFormatError('Reviews format error');
+    }
+    let reviews = [];
+    data.forEach(review => {
+      if (!this.utils.has(review, 'Assessment.id') ||
+          !this.utils.has(review, 'Assessment.name') ||
+          !this.utils.has(review, 'AssessmentReview.is_done') ||
+          !this.utils.has(review, 'AssessmentSubmission.Submitter.name') ||
+          !this.utils.has(review, 'AssessmentSubmission.context_id') ||
+          !this.utils.has(review, 'AssessmentReview.created') ||
+          !this.utils.has(review, 'AssessmentReview.modified')) {
+        return this.request.apiResponseFormatError('Reviews object format error');
+      }
+      reviews.push({
+        assessmentId: review.Assessment.id,
+        isDone: review.AssessmentReview.is_done,
+        name: review.Assessment.name,
+        submitterName: review.AssessmentSubmission.Submitter.name,
+        date: review.AssessmentReview.is_done ? review.AssessmentReview.modified : review.AssessmentReview.created,
+        teamName: this.utils.has(review, 'AssessmentSubmission.Team.name') ? review.AssessmentSubmission.Team.name : '',
+        contextId: review.AssessmentSubmission.context_id
+      });
+    });
+    return reviews;
+  }
+
 }
