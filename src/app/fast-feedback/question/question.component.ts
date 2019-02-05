333d8bcff97a3c36b784c822fbb074e222e8f67a
sasangachathumal <chathumal.sasanga02@gmail.com>
2019-02-05 12:09:23 +0530

    [AV2-220] change choice white space to normal. create a new scss file for question component.

--- src/app/fast-feedback/question/question.component.ts
+++ src/app/fast-feedback/question/question.component.ts
@@ -5,6 +5,7 @@ import { FormGroup }        from '@angular/forms';
 @Component({
   selector: 'question',
   templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
 })
 
 export class QuestionComponent {
