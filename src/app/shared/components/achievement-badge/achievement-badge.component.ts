36d27d367c756b631691bff96f9708c577891e2c
Shawn <shawn@practera.com>
2019-02-14 10:20:55 +1100

    [AV2-243]

---
+++ src/app/shared/components/achievement-badge/achievement-badge.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-achievement-badge',
+  templateUrl: './achievement-badge.component.html',
+  styleUrls: ['./achievement-badge.component.scss']
+})
+export class AchievementBadgeComponent {
+
+}
+
