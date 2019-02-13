3f40718ee562e4671518b7af126a8abf17537ccd
Shawn <shawn@practera.com>
2019-02-14 10:20:55 +1100

    [AV2-243]

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { AchievementsService, Achievements } from './achievements.service';
+import { AchievementsService, Achievement } from './achievements.service';
 import { UtilsService } from '../services/utils.service';
 import { RouterEnter } from '@services/router-enter.service';
 
