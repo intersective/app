f148e82c06fcaed22c93ced91a013f8158a6a86e
Shawn <shawn@practera.com>
2019-02-19 11:13:25 +1100

    [AV2-239]

--- src/app/shared/notification/notification.module.ts
+++ src/app/shared/notification/notification.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PopUpComponent } from './pop-up/pop-up.component';
-import { AchievementPopUpComponent } from './achievement/achievement-pop-up.component';
+import { AchievementPopUpComponent } from './achievement-pop-up/achievement-pop-up.component';
 import { NotificationService } from './notification.service';
 
 
