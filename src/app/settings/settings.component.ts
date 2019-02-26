f8768fdff9debe20e1a2492e21e395384c891614
Ronak <ronakamani@gmail.com>
2019-02-26 21:41:08 +1100

    [AV2-228]/fix the setting page issue

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -231,4 +231,13 @@ export class SettingsComponent extends RouterEnter {
     return this.authService.logout();
   }
 
+  setCursorPosition(event) {
+    event = (event) ? event : window.event;
+    var charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+  }
+
 }
