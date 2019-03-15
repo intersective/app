5ed98ae3bbc3569afbcafb04fa2ba94e84699d25
Shawn <shawn@practera.com>
2019-03-15 17:33:10 +1100

    [AV2-278]

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -20,40 +20,72 @@ export class EventDetailComponent {
   ) {}
 
   confirmed() {
-    if (this.event.isBooked) {
-      this.eventDetailService.cancelEvent(this.event).subscribe(response => {
-        if (response.success) {
-          this.notificationService.alert({
-            message: 'Booking canceled Successfully!',
-            buttons: [
-              {
-                text: 'OK',
-                role: 'cancel'
-              }
-            ]
-          });
-          // update the event list & activity detail page
-          this.utils.broadcastEvent('update-event', null);
-        }
-      });
-    } else {
-      this.eventDetailService.bookEvent(this.event).subscribe(response => {
-        if (response.success) {
-          this.notificationService.alert({
-            message: 'Booked Successfully!',
-            buttons: [
-              {
-                text: 'OK',
-                role: 'cancel'
-              }
-            ]
-          });
-          // update the event list & activity detail page
-          this.utils.broadcastEvent('update-event', null);
-        }
-      });
+    switch (this.buttonText()) {
+      case 'Book':
+        this.eventDetailService.bookEvent(this.event).subscribe(response => {
+          if (response.success) {
+            this.notificationService.alert({
+              message: 'Booked Successfully!',
+              buttons: [
+                {
+                  text: 'OK',
+                  role: 'cancel'
+                }
+              ]
+            });
+            // update the event list & activity detail page
+            this.utils.broadcastEvent('update-event', null);
+          }
+        });
+        break;
+
+      case 'Cancel Booking':
+        this.eventDetailService.cancelEvent(this.event).subscribe(response => {
+          if (response.success) {
+            this.notificationService.alert({
+              message: 'Booking canceled Successfully!',
+              buttons: [
+                {
+                  text: 'OK',
+                  role: 'cancel'
+                }
+              ]
+            });
+            // update the event list & activity detail page
+            this.utils.broadcastEvent('update-event', null);
+          }
+        });
+        break;
+
+      case 'Check In':
+      case 'View Check In':
+        this.router.navigate(['assessment', 'event', this.event.assessment.contextId, this.event.assessment.id]);
+        break;
     }
     this.modalController.dismiss();
+  }
+
+  buttonText() {
+    // for not booked event
+    if (!this.event.isBooked) {
+      if (!this.event.isPast && this.event.remainingCapacity > 0) {
+        return 'Book';
+      }
+      return false;
+    }
+    // can only cancel booking before event start
+    if (!this.event.isPast) {
+      return 'Cancel Booking';
+    }
+    // for event that doesn't have check in
+    if (!this.utils.has(event, 'assessment.id')) {
+      return false;
+    }
+    // for event that have check in
+    if (this.event.assessment.isDone) {
+      return 'View Check In';
+    }
+    return 'Check In';
   }
 
   close() {
