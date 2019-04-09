5b6ae3a9bed80c5b26ba73258f8c4543bc32b462
Sau Shen Chaw <cshen_581@hotmail.com>
2019-04-09 15:57:25 +0800

    remove code

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,35 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { UtilsService } from '@services/utils.service';
-import { BrowserStorageService } from '@services/storage.service';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   template: '<ion-router-outlet></ion-router-outlet>'
 })
-export class AuthComponent implements OnInit {
-  constructor(
-    private route: ActivatedRoute,
-    private storage: BrowserStorageService,
-    private utils: UtilsService
-  ) {}
-
-  ngOnInit() {
-    this.route.data
-      .subscribe((response: any) => {
-        try {
-          const expConfig = (response.config || {}).data;
-          if (expConfig.length > 0) {
-            this.storage.setUser({
-              'logo': expConfig[0].logo,
-              'themeColor': expConfig[0].config.theme_color
-            });
-          }
-        } catch (err) {
-          console.log('Inconsistent Experince config.');
-          throw err;
-        }
-      });
-  }
-}
+export class AuthComponent {}
