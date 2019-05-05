a3a5e291d292ecbb0791ba611f9f85d7e5761f33
Wes Sonnenreich <wes@practera.com>
2019-05-06 09:51:30 +1000

    temp disable events loading

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -26,7 +26,7 @@ export class ActivityComponent extends RouterEnter {
   };
   loadingActivity = true;
   events: Array<Event>;
-  loadingEvents = true;
+  loadingEvents = false;
 
   constructor(
     public router: Router,
