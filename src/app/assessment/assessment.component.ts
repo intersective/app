1bb4be08a754b8c2ddad20901110c64cdf18321e
sasangachathumal <chathumal.sasanga02@gmail.com>
2019-05-17 08:56:03 +0530

    [AV2-69] fixed issues found in code review. change date format in due date. moved date format method to shared service.

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -6,6 +6,7 @@ import { NotificationService } from '@shared/notification/notification.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { BrowserStorageService } from '@services/storage.service';
 import { RouterEnter } from '@services/router-enter.service';
+import { SharedService } from '@services/shared.service';
 
 const SAVE_PROGRESS_TIMEOUT = 10000;
 
@@ -69,6 +70,7 @@ export class AssessmentComponent extends RouterEnter {
     public utils: UtilsService,
     private notificationService: NotificationService,
     public storage: BrowserStorageService,
+    public sharedService: SharedService
   ) {
     super(router);
   }
