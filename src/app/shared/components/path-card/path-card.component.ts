484a8b4eaf52cf81ff42de4477401cb2689769ef
Wes Sonnenreich <wes@practera.com>
2019-06-26 03:31:51 +1000

    first push

--- src/app/shared/components/path-card/path-card.component.ts
+++ src/app/shared/components/path-card/path-card.component.ts
@@ -21,6 +21,7 @@ export class PathCardComponent implements OnInit {
   backgroundImageStyle: string = '';
   backgroundImageStyle1: string = '';
   backgroundImageStyle2: string = '';
+  backgroundImageStyle3: string = '';
   pathLoading: boolean = true;
   constructor ( @Inject(DOCUMENT) private document: Document ) { }
 
@@ -30,8 +31,10 @@ export class PathCardComponent implements OnInit {
     }
     console.log(this.path);
     this.backgroundImageStyle = '';
-    this.backgroundImageStyle1 = 'url(https://egyptinnovate.com/sites/default/files/styles/details_page_image_width_654/public/Project-Management.jpg?itok=SIynbALN)';
-    this.backgroundImageStyle2 = 'url(https://smallbiztrends.com/wp-content/uploads/2016/05/shutterstock_311472353.jpg)';
+    this.backgroundImageStyle1 = 'url(https://www.conferenceboard.ca/images/default-source/cboc-images-public/esp2000_banner.jpg?sfvrsn=b503719b_0), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
+    this.backgroundImageStyle2 = 'url(https://smallbiztrends.com/wp-content/uploads/2016/05/shutterstock_311472353.jpg), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
+    this.backgroundImageStyle3 = 'url(https://dailytimes.com.pk/assets/uploads/2018/02/01/third-story-photo-1.jpg), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
+    
     if (this.path.leadImage) {
       this.backgroundImageStyle = 'url(' + this.path.leadImage + '), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
     }
---
+++ src/environments/environment.live.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  appkey: 'b11e7c189b',
+  pusherKey: '255f010d210933ca7675',
+  env: 'live',
+  APIEndpoint: 'https://api.practera.com/',
+  intercomAppId: '',
+  filestack: {
+    key: 'AO6F4C72uTPGRywaEijdLz',
+    s3Config: {
+      location: 's3',
+      container: 'practera-aus',
+      region: 'ap-southeast-2',
+      paths: {
+        any: '/appv2/live/uploads/',
+        image: '/appv2/live/uploads/',
+        video: '/media/fpvideo/upload/'
+      }
+    }
+  },
+  intercom: true,
+  goMobile: false,
+};
