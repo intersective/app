905e8efd48a2482f7f7929588e35085e47bf466e
Wes Sonnenreich <wes@practera.com>
2019-06-26 11:08:28 +1000

    better images/colors

--- src/app/shared/components/path-card/path-card.component.ts
+++ src/app/shared/components/path-card/path-card.component.ts
@@ -22,6 +22,7 @@ export class PathCardComponent implements OnInit {
   backgroundImageStyle1: string = '';
   backgroundImageStyle2: string = '';
   backgroundImageStyle3: string = '';
+  backgroundImageStyle4: string = '';
   pathLoading: boolean = true;
   constructor ( @Inject(DOCUMENT) private document: Document ) { }
 
@@ -31,9 +32,10 @@ export class PathCardComponent implements OnInit {
     }
     console.log(this.path);
     this.backgroundImageStyle = '';
-    this.backgroundImageStyle1 = 'url(https://www.conferenceboard.ca/images/default-source/cboc-images-public/esp2000_banner.jpg?sfvrsn=b503719b_0), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
+    this.backgroundImageStyle1 = 'url(/assets/cards/employability.png), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
     this.backgroundImageStyle2 = 'url(https://smallbiztrends.com/wp-content/uploads/2016/05/shutterstock_311472353.jpg), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
-    this.backgroundImageStyle3 = 'url(https://dailytimes.com.pk/assets/uploads/2018/02/01/third-story-photo-1.jpg), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
+    this.backgroundImageStyle3 = 'url(/assets/cards/map.jpg), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
+    this.backgroundImageStyle4 = 'url(/assets/cards/credentials.png), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
     
     if (this.path.leadImage) {
       this.backgroundImageStyle = 'url(' + this.path.leadImage + '), linear-gradient( rgba(0, 0, 0, .4), rgba(0, 0, 0, 0.2) )';
