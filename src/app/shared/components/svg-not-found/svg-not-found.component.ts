7366bae772a55f11f8c5b9e8d2a4a6f29296a7f5
Shawn <shawn@practera.com>
2019-07-11 10:48:44 +0800

    [AV2-347]

---
+++ src/app/shared/components/svg-not-found/svg-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'svg-not-found',
+  templateUrl: './svg-not-found.component.html',
+  styleUrls: ['./svg-not-found.component.scss']
+})
+export class SvgNotFoundComponent {
+
+  constructor(
+  ) {}
+
+}
