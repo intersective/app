062e1fdfc31029799eef07174676b5df1d2b2956
chaw <cshen_581@hotmail.com>
2019-07-26 00:29:19 +0800

    [AV2-309] multi fixes

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -5,17 +5,24 @@ import { SettingsComponent } from './settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { TextMaskModule } from 'angular2-text-mask';
 import { FilestackModule } from '@shared/filestack/filestack.module';
+import { FastFeedbackModule } from '../fast-feedback/fast-feedback.module';
 
 @NgModule({
   imports: [
     SharedModule,
     SettingsRoutingModule,
     TextMaskModule,
-    FilestackModule
+    FilestackModule,
+    FastFeedbackModule
   ],
   declarations: [
     SettingsRoutingComponent,
     SettingsComponent
   ],
+  exports: [
+    SharedModule,
+    FilestackModule,
+    FastFeedbackModule
+  ]
 })
 export class SettingsModule {}
