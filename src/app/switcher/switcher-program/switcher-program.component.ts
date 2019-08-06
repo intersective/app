bef7eb967a35aa03210f5b6d8447d471d7cecd6e
Shawn <shawn@practera.com>
2019-08-06 14:43:13 +0800

    [AV2-367]

--- src/app/switcher/switcher-program/switcher-program.component.ts
+++ src/app/switcher/switcher-program/switcher-program.component.ts
@@ -45,7 +45,7 @@ export class SwitcherProgramComponent implements OnInit {
     return this.switcherService.switchProgram(this.programs[index]).subscribe(() => {
       loading.dismiss().then(() => {
         // reset pusher (upon new timelineId)
-        this.pusherService.initantiate({ unsubscribe: true });
+        this.pusherService.initialise({ unsubscribe: true });
 
         if ((typeof environment.goMobile !== 'undefined' && environment.goMobile === false)
           || /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
