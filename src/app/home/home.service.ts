3a568053ff69827793222e71d569e84f33b5c17e
Shawn <shawn@practera.com>
2019-08-07 17:10:13 +0800

    [AV2-367]

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -88,7 +88,6 @@ export class HomeService {
       this.request.apiResponseFormatError('TodoItem array format error');
       return [];
     }
-console.log('normalise todo item');
     data.forEach(todoItem => {
       if (!this.utils.has(todoItem, 'identifier') ||
           !this.utils.has(todoItem, 'is_done') ||
