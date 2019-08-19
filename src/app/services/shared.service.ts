5eaa5e660bb6a77583953ae735c3eb1f7ad1a4a2
sasangachathumal <chathumal.sasanga02@gmail.com>
2019-08-19 20:51:33 +0530

    [AV2-348] added stop video playing to tabs. remove leave event from activity component. fixed lint issue in shared service.

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -84,12 +84,12 @@ export class SharedService {
     return 'Due ' + this.utils.utcToLocal(dueDate);
   }
 
+  /**
+   * This method get all iframe and videos from documents and stop playing videos.
+   */
   stopPlayingViodes() {
-    console.log('stopPlayingViodes');
-    const iframes = document.querySelectorAll( 'iframe');
-    const videos = document.querySelectorAll( 'video' );
-    console.log('stopPlayingViodes - iframes', iframes);
-    console.log('stopPlayingViodes - videos', videos);
+    const iframes = Array.from(document.querySelectorAll( 'iframe'));
+    const videos = Array.from(document.querySelectorAll( 'video' ));
     if ( iframes ) {
       iframes.forEach(frame => {
         frame.src = frame.src;
