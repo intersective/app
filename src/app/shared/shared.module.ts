de3e7a31ae9a11563e7300f4c55a8df9684c78db
sasangachathumal <chathumal.sasanga02@gmail.com>
2019-08-20 19:47:06 +0530

    [AV2-199] create new directive to resize textarea.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,6 +11,7 @@ import { CircleProgressComponent } from '@shared/components/circle-progress/circ
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { BrandingLogoComponent } from '@shared/components/branding-logo/branding-logo.component';
 import { ContactNumberFormComponent } from '@shared/components/contact-number-form/contact-number-form.component';
+import { AutoresizeDirective } from './directives/autoresize/autoresize.directive';
 
 const largeCircleDefaultConfig = {
   backgroundColor: 'var(--ion-color-light)',
@@ -43,7 +44,8 @@ const largeCircleDefaultConfig = {
     ClickableItemComponent,
     CircleProgressComponent,
     BrandingLogoComponent,
-    ContactNumberFormComponent
+    ContactNumberFormComponent,
+    AutoresizeDirective
   ],
   exports: [
     ActivityCardComponent,
