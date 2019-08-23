a7f9d2caa34810742d79192a757db3979940c14a
Shawn <shawn@practera.com>
2019-08-23 11:42:17 +0800

    [AV2-391]

--- src/app/assessment/assessment.component.spec.ts
+++ src/app/assessment/assessment.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
@@ -11,6 +11,7 @@ import { NotificationService } from '@shared/notification/notification.service';
 import { ActivityService } from '@app/activity/activity.service';
 import { FastFeedbackService } from '@app/fast-feedback/fast-feedback.service';
 import { BrowserStorageService } from '@services/storage.service';
+import { SharedService } from '@services/shared.service';
 import { of } from 'rxjs';
 
 fdescribe('AssessmentComponent', () => {
@@ -23,14 +24,49 @@ fdescribe('AssessmentComponent', () => {
   let fastFeedbackSpy: jasmine.SpyObj<FastFeedbackService>;
   let routerSpy: jasmine.SpyObj<Router>;
   let routeStub: Partial<ActivatedRoute>;
+  let shared: SharedService;
+  const mockAssessment = {
+    name: 'test',
+    description: 'test',
+    isForTeam: false,
+    dueDate: '2029-02-02',
+    isOverdue: false,
+    groups: [{
+      name: 'test groups',
+      description: 'test groups description',
+      questions: [
+        {
+          id: 123,
+          name: 'test',
+          description: 'test',
+          canAnswer: true,
+          canComment: false,
+          type: 'text',
+          isRequired: true,
+          audience: ['participant', 'mentor']
+        },
+        {
+          id: 124,
+          name: 'test',
+          description: 'test',
+          canAnswer: true,
+          canComment: false,
+          type: 'text',
+          isRequired: true,
+          audience: ['participant', 'mentor']
+        }
+      ],
+    }],
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, HttpClientModule],
+      imports: [ReactiveFormsModule, FormsModule, HttpClientModule],
       declarations: [AssessmentComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         UtilsService,
+        SharedService,
         {
           provide: AssessmentService,
           useValue: jasmine.createSpyObj('AssessmentService', ['getAssessment', 'getSubmission', 'getFeedbackReviewed', 'saveAnswers', 'saveFeedbackReviewed', 'popUpReviewRating'])
@@ -95,6 +131,13 @@ fdescribe('AssessmentComponent', () => {
     activitySpy = TestBed.get(ActivityService);
     fastFeedbackSpy = TestBed.get(FastFeedbackService);
     routeStub = TestBed.get(ActivatedRoute);
+    shared = TestBed.get(SharedService);
+    // initialise service calls
+    assessmentSpy.getAssessment.and.returnValue(of(mockAssessment));
+    assessmentSpy.getSubmission.and.returnValue(of({
+      submission: {},
+      review: {}
+    }));
   });
 
   it('should create', () => {
@@ -105,42 +148,26 @@ fdescribe('AssessmentComponent', () => {
     fixture.detectChanges();
     expect(component.id).toEqual(1);
     expect(component.activityId).toEqual(2);
+    expect(component.contextId).toEqual(3);
+    expect(component.submissionId).toEqual(4);
+    expect(component.action).toEqual('assessment');
+  });
+
+  it('should get correct assessment and display correct info in html', () => {
+    fixture.detectChanges();
+    expect(component.assessment).toEqual(mockAssessment);
+    expect(component.loadingAssessment).toEqual(false);
+    expect(element.querySelector('ion-title.subTitle')).toBeTruthy();
+    expect(element.querySelector('h1').innerHTML).toEqual(mockAssessment.name);
+    expect(element.querySelector('app-description')).toBeTruthy();
+    expect(element.querySelector('p.over')).toBeFalsy();
+    expect(element.querySelector('p.due-date').innerHTML).toEqual(shared.dueDateFormatter(mockAssessment.dueDate));
   });
 
   it('should list unanswered question compulsoryQuestionsAnswered', () => {
     expect(component.compulsoryQuestionsAnswered).toBeDefined();
 
-    component.assessment = {
-      name: 'test',
-      description: 'test',
-      isForTeam: false,
-      groups: [{
-        name: 'test groups',
-        description: 'test groups description',
-        questions: [
-          {
-            id: 123,
-            name: 'test',
-            description: 'test',
-            canAnswer: true,
-            canComment: false,
-            type: 'moderated',
-            isRequired: true,
-            audience: ['participant', 'mentor']
-          },
-          {
-            id: 124,
-            name: 'test',
-            description: 'test',
-            canAnswer: true,
-            canComment: false,
-            type: 'moderated',
-            isRequired: true,
-            audience: ['participant', 'mentor']
-          }
-        ],
-      }],
-    };
+    component.assessment = mockAssessment;
 
     const answers = {
       'Assessment': {
