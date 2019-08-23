aa6df4a86b483c61f8d304bb60c20e266b5d5deb
sasangachathumal <chathumal.sasanga02@gmail.com>
2019-08-23 13:27:12 +0530

    [AV2-199] fixed issue text box not shrink after send message.

---
+++ src/app/shared/directives/autoresize/autoresize.directive.ts
@@ -0,0 +1,46 @@
+import { Directive, ElementRef, Input, HostListener, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appAutoresize]'
+})
+export class AutoresizeDirective implements OnInit {
+
+  // tslint:disable-next-line:no-input-rename
+  @Input('appAutoresize') maxHeight: number;
+
+  @HostListener('input', ['$event.target'])
+  onInput(textArea: HTMLTextAreaElement): void {
+    this.adjust();
+  }
+
+  constructor(public element: ElementRef) { }
+
+  ngOnInit(): void {
+    this.adjust();
+  }
+
+  /**
+   * in this method we resize textarea releted to text in side it.
+   * when user type on textarea it will expand untill scroll height lower than max height.
+   *  if scroll height biger than max height textare will not expand and it will scroll content.
+   * - select textare.
+   * - set 'aoto' to height and overflow properties.
+   * - check min value from scrollheight and maxheight and get it as the newheight.
+   * - set that new height to textarea height.
+   */
+  adjust(): void {
+    const ta = this.element.nativeElement.querySelector('textarea');
+    let newHeight;
+    if (ta) {
+      ta.style.overflow = 'auto';
+      ta.style.height = 'auto';
+      if (this.maxHeight) {
+      newHeight = Math.min(ta.scrollHeight, this.maxHeight);
+      } else {
+        newHeight = ta.scrollHeight;
+      }
+      ta.style.height = newHeight + 'px';
+    }
+  }
+
+}
