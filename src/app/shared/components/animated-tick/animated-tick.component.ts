b7be76f90a01c33c82d59b2df1bb3d5a7bed5ece
chaw <cshen_581@hotmail.com>
2019-08-26 20:14:10 +0800

    [AV2-425] compiled svg

---
+++ src/app/shared/components/animated-tick/animated-tick.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-tick',
+  templateUrl: './animated-tick.component.html',
+  styleUrls: ['./animated-tick.component.scss']
+})
+
+export class AnimatedTickComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit() {
+    console.log('created tick');
+  }
+
+  ngOnDestroy() {
+    console.log('destroyed tick');
+  }
+}
