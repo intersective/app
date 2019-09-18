e3879f41c6141ed465a3a0d490900b8f629779de
Shawn <shawn@practera.com>
2019-09-18 11:13:01 +0800

    [AV2-407]

--- src/app/fast-feedback/fast-feedback.component.spec.ts
+++ src/app/fast-feedback/fast-feedback.component.spec.ts
@@ -1,25 +1,76 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Observable, of, pipe } from 'rxjs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UtilsService } from '@services/utils.service';
+import { FastFeedbackSubmitterService } from './fast-feedback-submitter.service';
+import { BrowserStorageService } from '@services/storage.service';
+import { NotificationService } from '@shared/notification/notification.service';
+import { ModalController } from '@ionic/angular';
 import { FastFeedbackComponent } from './fast-feedback.component';
+import { QuestionComponent } from './question/question.component';
 
-describe('FastFeedbackComponent', () => {
-  // let component: FastFeedbackComponent;
-  // let fixture: ComponentFixture<FastFeedbackComponent>;
+class Page {
+  get questions() {
+    return this.queryAll<HTMLElement>('question');
+  }
+  fixture: ComponentFixture<FastFeedbackComponent>;
+
+  constructor(fixture: ComponentFixture<FastFeedbackComponent>) {
+    this.fixture = fixture;
+  }
+  private query<T>(selector: string): T {
+    return this.fixture.nativeElement.querySelector(selector);
+  }
+  private queryAll<T>(selector: string): T[] {
+    return this.fixture.nativeElement.querySelectorAll(selector);
+  }
+}
+
+fdescribe('FastFeedbackComponent', () => {
+  let component: FastFeedbackComponent;
+  let fixture: ComponentFixture<FastFeedbackComponent>;
+  let page: Page;
+  let fastfeedbackSpy: jasmine.SpyObj<FastFeedbackSubmitterService>;
 
   beforeEach(async(() => {
-    // TestBed.configureTestingModule({
-    //   declarations: [ FastFeedbackComponent ]
-    // })
-    // .compileComponents();
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      declarations: [ FastFeedbackComponent, QuestionComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        {
+          provide: FastFeedbackSubmitterService,
+          useValue: jasmine.createSpyObj('FastFeedbackSubmitterService', ['submit'])
+        },
+        UtilsService,
+        {
+          provide: NotificationService,
+          useValue: jasmine.createSpyObj('NotificationService', ['alert'])
+        },
+        {
+          provide: ModalController,
+          useValue: {
+            dismiss: jasmine.createSpy('dismiss')
+          }
+        },
+        {
+          provide: BrowserStorageService,
+          useValue: jasmine.createSpyObj('BrowserStorageService', ['set'])
+        }
+      ],
+    })
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    // fixture = TestBed.createComponent(FastFeedbackComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture = TestBed.createComponent(FastFeedbackComponent);
+    component = fixture.componentInstance;
+    page = new Page(fixture);
+    fastfeedbackSpy = TestBed.get(FastFeedbackSubmitterService);
   });
 
   it('should create', () => {
-    // expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
