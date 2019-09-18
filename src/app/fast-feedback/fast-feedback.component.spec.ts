6e4be296bae07ab68311a0ed83cd70c3b6f75a45
Shawn <shawn@practera.com>
2019-09-18 12:50:24 +0800

    [AV2-407]

--- src/app/fast-feedback/fast-feedback.component.spec.ts
+++ src/app/fast-feedback/fast-feedback.component.spec.ts
@@ -9,6 +9,7 @@ import { NotificationService } from '@shared/notification/notification.service';
 import { ModalController } from '@ionic/angular';
 import { FastFeedbackComponent } from './fast-feedback.component';
 import { QuestionComponent } from './question/question.component';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 class Page {
   get questions() {
@@ -27,11 +28,13 @@ class Page {
   }
 }
 
-fdescribe('FastFeedbackComponent', () => {
+describe('FastFeedbackComponent', () => {
   let component: FastFeedbackComponent;
   let fixture: ComponentFixture<FastFeedbackComponent>;
   let page: Page;
   let fastfeedbackSpy: jasmine.SpyObj<FastFeedbackSubmitterService>;
+  let notificationSpy: jasmine.SpyObj<NotificationService>;
+  let modalSpy: jasmine.SpyObj<ModalController>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -68,9 +71,81 @@ fdescribe('FastFeedbackComponent', () => {
     component = fixture.componentInstance;
     page = new Page(fixture);
     fastfeedbackSpy = TestBed.get(FastFeedbackSubmitterService);
+    notificationSpy = TestBed.get(NotificationService);
+    modalSpy = TestBed.get(ModalController);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('when testing ngOnInit(), it should get the correct data', () => {
+    component.questions = Array.from({length: 5}, (x, i) => {
+      return {
+        id: i + 1
+      };
+    });
+    component.ngOnInit();
+    expect(Object.keys(component.fastFeedbackForm.controls).length).toBe(5);
+  });
+
+  it('when testing dismiss(), it should dismiss', () => {
+    component.dismiss();
+    expect(modalSpy.dismiss.calls.count()).toBe(1);
+  })
+
+  describe('when testing submit()', () => {
+    beforeEach(() => {
+      component.fastFeedbackForm = new FormGroup({
+        0: new FormControl(''),
+        1: new FormControl(''),
+        2: new FormControl(''),
+        3: new FormControl(''),
+        4: new FormControl('')
+      });
+      component.fastFeedbackForm.setValue(Array.from({length: 5}, (x, i) => {
+        return {
+          answer: i + 2,
+          questionId: i + 1
+        };
+      }));
+      component.meta = {
+        context_id: 1,
+        team_id: 2,
+        target_user_id: 3,
+        team_name: 'team',
+        assessment_name: 'asmt'
+      }
+      fastfeedbackSpy.submit.and.returnValue(of({}));
+    });
+    afterEach(() => {
+      expect(fastfeedbackSpy.submit.calls.count()).toBe(1);
+      expect(notificationSpy.alert.calls.count()).toBe(1);
+      notificationSpy.alert.calls.first().args[0].buttons[0].handler();
+      expect(modalSpy.dismiss.calls.count()).toBe(1);
+    });
+    it('should submit correct data #1', () => {
+      component.submit();
+      expect(fastfeedbackSpy.submit.calls.first().args[1]).toEqual({
+        context_id: 1,
+        team_id: 2
+      });
+    });
+    it('should submit correct data #2', () => {
+      component.meta.team_id = null;
+      component.submit();
+      expect(fastfeedbackSpy.submit.calls.first().args[1]).toEqual({
+        context_id: 1,
+        target_user_id: 3
+      });
+    });
+    it('should submit correct data #3', () => {
+      component.meta.team_id = null;
+      component.meta.target_user_id = null;
+      component.submit();
+      expect(fastfeedbackSpy.submit.calls.first().args[1]).toEqual({
+        context_id: 1
+      });
+    });
   });
 });
