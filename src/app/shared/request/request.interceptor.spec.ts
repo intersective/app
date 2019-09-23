6e14bd5e67638bef117544746ca507c8d8360581
chaw <cshen_581@hotmail.com>
2019-09-23 18:45:18 +0800

    [AV2-434] interceptor test

--- src/app/shared/request/request.interceptor.spec.ts
+++ src/app/shared/request/request.interceptor.spec.ts
@@ -1,26 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { RequestModule } from './request.module';
 import { RequestService } from './request.service';
 import { RequestInterceptor } from './request.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { asyncData } from '@testing/async-observable-helpers';
+
+import { Router } from '@angular/router';
+import { TestUtils } from '@testing/utils';
 
 describe('RequestInterceptor', () => {
+  const APPKEY = 'TEST';
+  const routerSpy = TestUtils.createRouterSpy();
+  let service: RequestService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RequestModule.forRoot({
+          appkey: APPKEY,
+          prefixUrl: 'test.com'
+        })
+      ],
       providers: [
         RequestService,
+        {
+          provide: Router,
+          useValue: routerSpy
+        },
         {
           provide: HTTP_INTERCEPTORS,
           useClass: RequestInterceptor,
           multi: true,
         }
       ]
-    })
+    });
+
+    service = TestBed.get(RequestService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should has Authentication injected into HTTP header', () => {
+  beforeEach(fakeAsync(() => {
+    service.get('/test').subscribe(_res => {
+      expect(_res).toBeTruthy();
+    });
+    tick();
+  }));
 
+  it('should has Authentication injected into HTTP header', () => {
+    const req = httpMock.expectOne({ method: 'GET' });
+    expect(req.request.headers.has('appkey')).toEqual(true);
+  });
+
+  it('should return APPKEY as provided in RequestConfig', () => {
+    const req = httpMock.expectOne({ method: 'GET' });
+    expect(req.request.headers.get('appkey')).toBe(APPKEY);
   });
 });
