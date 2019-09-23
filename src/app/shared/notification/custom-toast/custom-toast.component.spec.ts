f842370bebc4627310c8ac4ea809629174fc21e9
chaw <cshen_581@hotmail.com>
2019-09-23 18:50:39 +0800

    [AV2-434] customToast unit-test fix

--- src/app/shared/notification/custom-toast/custom-toast.component.spec.ts
+++ src/app/shared/notification/custom-toast/custom-toast.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomToastComponent } from './custom-toast.component';
@@ -8,7 +9,8 @@ describe('CustomToastComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CustomToastComponent ]
+      declarations: [ CustomToastComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
