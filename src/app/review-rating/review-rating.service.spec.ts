7052ae14359092664776fa0b9e5a62e9c83cbb36
Shawn <shawn@practera.com>
2019-09-24 14:04:57 +0800

    [AV2-412]

---
+++ src/app/review-rating/review-rating.service.spec.ts
@@ -0,0 +1,39 @@
+import { ReviewRatingService } from './review-rating.service';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { RequestService } from '@shared/request/request.service';
+
+describe('ReviewRatingService', () => {
+  let service: ReviewRatingService;
+  let requestSpy: jasmine.SpyObj<RequestService>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ReviewRatingService,
+        {
+          provide: RequestService,
+          useValue: jasmine.createSpyObj('RequestService', ['delete', 'post', 'get'])
+        },
+      ]
+    });
+    service = TestBed.get(ReviewRatingService);
+    requestSpy = TestBed.get(RequestService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('when testing submitRating()', () => {
+    requestSpy.post.and.returnValue(of(''));
+    service.submitRating({
+      assessment_review_id: 1,
+      rating: 0.4,
+      comment: '',
+      tags: []
+    }).subscribe();
+    expect(requestSpy.post.calls.count()).toBe(1);
+  });
+});
+
