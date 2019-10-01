a331e0ca81ee758db5552aef952032b44cef46d3
chaw <cshen_581@hotmail.com>
2019-10-01 14:02:53 +0800

    [AV2-464] added few more SPA tracking

--- src/app/auth/auth-forgot-password/auth-forgot-password.component.ts
+++ src/app/auth/auth-forgot-password/auth-forgot-password.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '@shared/notification/notification.service';
 import { UtilsService } from '@services/utils.service';
 import { AuthService } from '../auth.service';
+import { NewRelicService } from '@shared/new-relic/new-relic.service';
 
 @Component({
   selector: 'app-auth-forgot-password',
   templateUrl: 'auth-forgot-password.component.html',
   styleUrls: ['auth-forgot-password.component.scss']
 })
-export class AuthForgotPasswordComponent {
+export class AuthForgotPasswordComponent implements OnInit {
   email = '';
   // variable to control the label of the button
   isSending = false;
@@ -16,8 +17,13 @@ export class AuthForgotPasswordComponent {
   constructor(
     private notificationService: NotificationService,
     private authService: AuthService,
-    private utils: UtilsService
+    private utils: UtilsService,
+    private newRelic: NewRelicService
   ) {}
+
+  ngOnInit() {
+    this.newRelic.setPageViewName('forgotpassword');
+  }
 
   async send() {
     // basic validation
@@ -28,6 +34,7 @@ export class AuthForgotPasswordComponent {
     // call API to do forgot password logic
     this.authService.forgotPassword(this.email).subscribe(
       res => {
+        this.newRelic.actionText('forgot password request sent');
         this.isSending = false;
         // show pop up message for confirmation
         return this.notificationService.popUp(
@@ -38,6 +45,7 @@ export class AuthForgotPasswordComponent {
         );
       },
       err => {
+        this.newRelic.noticeError(`Password Reset Error: ${JSON.stringify(err)}`);
         this.isSending = false;
         if (this.utils.has(err, 'data.type')) {
           // pop up if trying to reset password too frequently
