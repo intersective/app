1890c25979b6aff96a6aae1d3f593a783ddc4a4d
Shawn <shawn@practera.com>
2019-10-04 11:12:03 +0800

    [AV2-415]

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,64 +1,57 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AuthService } from '../auth/auth.service';
 import { SettingsComponent } from './settings.component';
-import { of } from 'rxjs';
+import { SettingService } from './setting.service';
+import { Observable, of, pipe } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '@shared/shared.module';
+import { UtilsService } from '@services/utils.service';
+import { FilestackService } from '@shared/filestack/filestack.service';
 
-describe('SettingsComponent', () => {
-  // let component: SettingsComponent;
-  // let fixture: ComponentFixture<SettingsComponent>;
+fdescribe('SettingsComponent', () => {
+  let component: SettingsComponent;
+  let fixture: ComponentFixture<SettingsComponent>;
+  let settingsSpy: jasmine.SpyObj<SettingService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let utils: UtilsService;
 
   beforeEach(async(() => {
-    // TestBed.configureTestingModule({
-    //   imports: [RouterTestingModule],
-    //   declarations: [SettingsComponent],
-    //   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    //   providers: [
-    //     {
-    //       provide: AuthService, useValue: {
-    //         logout: () => of(true),
-    //       }
-    //     }
-    //   ]
-    // }).compileComponents();
+    TestBed.configureTestingModule({
+      imports: [ SharedModule, HttpClientModule ],
+      declarations: [ SettingsComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        UtilsService,
+        FilestackService,
+        {
+          provide: SettingService,
+          useValue: jasmine.createSpyObj('SettingService', ['updateProfileImage'])
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate'),
+            settings: of()
+          }
+        },
+      ],
+    })
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    // fixture = TestBed.createComponent(SettingsComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture = TestBed.createComponent(SettingsComponent);
+    component = fixture.componentInstance;
+    settingsSpy = TestBed.get(SettingService);
+    routerSpy = TestBed.get(Router);
+    utils = TestBed.get(UtilsService);
   });
 
   it('should create', () => {
-    // expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 
-  /*it('should initiated with default values', () => {
-    component.ngOnInit();
-    expect(component.email.length).toBeGreaterThan(0);
-    expect(component.contact_number.length).toBeGreaterThan(0);
-  });*/
 
-  it('should navigate to switcher', () => {
-    // expect(component.switchProgram).toBeDefined();
-    // component.switchProgram();
-    // @TODO: check routing
-  });
-
-  /*it('should logout', () => {
-    expect(component.logout).toBeDefined();
-    component.logout();
-    expect(AuthService.logout.subscribe).toHaveBeenCalled();
-  });*/
-
-  /*it('should validate contact_number with verifyContactNumber', () => {
-    expect(component.verifyContactNumber).toBeDefined();
-  });*/
-
-  it('should update profile', () => {
-    // expect(component.updateProfile).toBeDefined();
-    const testContactNumber = '0123456789';
-    // expect(component.updateProfile(testContactNumber)).toBeTruthy();
-  });
 });
+
