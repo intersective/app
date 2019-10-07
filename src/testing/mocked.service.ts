dcf461534f96c99a39183507b26a2497b912f5ea
chaw <cshen_581@hotmail.com>
2019-10-07 16:51:11 +0800

    added test for modal

--- src/testing/mocked.service.ts
+++ src/testing/mocked.service.ts
@@ -1,8 +1,24 @@
+import { of, Observable } from 'rxjs';
+
 export class MockNewRelicService {
   noticeError() {
     return true;
   }
   actionText() {
     return true;
+  }
+}
+
+export class FastFeedbackServiceMock {
+  pullFastFeedback(): Observable<any> {
+    return of({
+      present: () => {
+        return new Promise<any>(resolve => resolve(true));
+      },
+      onDidDismiss: (data) => {
+        console.log(data);
+        return;
+      }
+    });
   }
 }
