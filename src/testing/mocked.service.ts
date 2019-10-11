4ee82f6b688183b2b4aee5a892f1ecad16104ba6
Sau Shen Chaw <cshen_581@hotmail.com>
2019-10-11 15:27:08 +0800

    unit test fix

--- src/testing/mocked.service.ts
+++ src/testing/mocked.service.ts
@@ -1,5 +1,6 @@
 import { of, Observable } from 'rxjs';
 import { SpyObject } from './utils';
+import { BrowserStorageService } from '@services/storage.service';
 
 export class MockNewRelicService {
   noticeError() {
@@ -28,5 +29,36 @@ export class FastFeedbackServiceMock {
 
   getFastFeedback() {
     return;
+  }
+}
+
+export class BrowserStorageServiceMock extends SpyObject {
+  getUser;
+  get;
+  getConfig;
+  set;
+  append;
+  remove;
+  clear;
+  setUser;
+  memoryCache;
+  storage;
+  setConfig;
+
+  constructor() {
+    super(BrowserStorageService);
+    const USER = {
+      userHash: 'testuserhash',
+      enrolment: {
+        id: 1,
+      }
+    };
+    this.getUser = this.spy('getUser').and.returnValue(USER);
+    this.get = this.spy('get').and.returnValue(true);
+    this.getConfig = this.spy('getConfig').and.returnValue(true);
+  }
+
+  getProviders(): Array<any> {
+    return [{ provide: BrowserStorageService, useValue: this }];
   }
 }
