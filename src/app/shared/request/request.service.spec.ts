bb99eb57dc520e00e22a1f6214cd1cb0409de925
Sau Shen Chaw <cshen_581@hotmail.com>
2019-10-16 15:42:14 +0800

    [AV2-475] more coverage

--- src/app/shared/request/request.service.spec.ts
+++ src/app/shared/request/request.service.spec.ts
@@ -1,10 +1,13 @@
+import { isDevMode, enableProdMode } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import {
   inject,
   fakeAsync,
   tick,
   TestBed,
-  async
+  async,
 } from '@angular/core/testing';
+
 import {
   MockBackend,
 } from '@angular/http/testing';
@@ -28,9 +31,17 @@ import {
   ResponseOptions
 } from '@angular/http';
 
-import { RequestService, RequestConfig } from './request.service';
+import { RequestService, RequestConfig, DevModeService } from './request.service';
 import { Router } from '@angular/router';
+import { BrowserStorageService } from '@services/storage.service';
 import { TestUtils } from '@testing/utils';
+import { BrowserStorageServiceMock } from '@testing/mocked.service';
+
+describe('DevModeService', () => {
+  it('isDevMode() should return true', () => {
+    expect(new DevModeService().isDevMode()).toBeTruthy();
+  });
+});
 
 describe('RequestConfig', () => {
   let requestConfig: RequestConfig;
@@ -52,12 +63,15 @@ describe('RequestService', () => {
   let service: RequestService;
   let mockBackend: HttpTestingController;
   let requestConfigSpy: RequestConfig;
+  let devModeServiceSpy: DevModeService;
+  let storageSpy: BrowserStorageService;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
         RequestService,
+        DevModeService,
         {
           provide: RequestConfig,
           useValue: {
@@ -69,12 +83,18 @@ describe('RequestService', () => {
           provide: Router,
           useValue: routerSpy
         },
+        {
+          provide: BrowserStorageService,
+          useClass: BrowserStorageServiceMock
+        }
       ]
     });
 
     service = TestBed.get(RequestService);
     mockBackend = TestBed.get(HttpTestingController);
     requestConfigSpy = TestBed.get(RequestConfig);
+    devModeServiceSpy = TestBed.get(DevModeService);
+    storageSpy = TestBed.get(BrowserStorageService);
   });
 
   it('should be created', () => {
@@ -82,10 +102,11 @@ describe('RequestService', () => {
   });
 
   describe('get()', () => {
+    const testURL = 'https://www.test.com';
+
     it('should perform a GET request based on provided URL', fakeAsync(() => {
       let res = { body: true };
 
-      const testURL = 'https://www.test.com';
       /*httpMock.connections.subscribe(con => {
         expect(con.request.url).toBe(testURL);
         const response = new ResponseOptions(res);
@@ -106,39 +127,80 @@ describe('RequestService', () => {
 
       mockBackend.verify();
     }));
+
+    it('should perform error handling when fail', fakeAsync(() => {
+      spyOn(devModeServiceSpy, 'isDevMode').and.returnValue(false);
+
+      const ERR_MESSAGE = 'Invalid request parameter';
+      const err = { success: false, status: 400, statusText: 'Bad Request' };
+      let res: any;
+      let errRes: any;
+      service.get(testURL).subscribe(
+        _res => {
+          res = _res;
+        },
+        _err => {
+          errRes = _err;
+        }
+      );
+      const req = mockBackend.expectOne({ url: testURL, method: 'GET'}).flush(ERR_MESSAGE, err);
+
+      expect(res).toBeUndefined();
+      expect(errRes).toEqual(ERR_MESSAGE);
+    }));
   });
 
   describe('post()', () => {
-    it('should perform a GET request based on provided URL', fakeAsync(() => {
-        let res = { body: true };
+    const testURL = 'https://www.post-test.com';
+    const sampleData = {
+      sample: 'data'
+    };
 
-        const testURL = 'https://www.post-test.com';
-        const sampleData = {
-          sample: 'data'
-        };
-
-        service.post(testURL, sampleData).subscribe(_res => {
-          res = _res;
-        });
-        const req = mockBackend.expectOne({ method: 'POST' });
-        req.flush(res);
-
-        tick();
-
-        const { body } = res;
-        expect(req.request.url).toBe(testURL);
-        expect(body).toBe(true);
-
-        mockBackend.verify();
-      })
-    );
-  });
-
-  describe('delete()', () => {
     it('should perform a GET request based on provided URL', fakeAsync(() => {
       let res = { body: true };
 
-      const testURL = 'https://www.delete-test.com';
+      service.post(testURL, sampleData).subscribe(_res => {
+        res = _res;
+      });
+      const req = mockBackend.expectOne({ method: 'POST' });
+      req.flush(res);
+
+      tick();
+
+      const { body } = res;
+      expect(req.request.url).toBe(testURL);
+      expect(body).toBe(true);
+
+      mockBackend.verify();
+    }));
+
+    it('should perform error handling when fail', fakeAsync(() => {
+      spyOn(devModeServiceSpy, 'isDevMode').and.returnValue(false);
+
+      const ERR_MESSAGE = 'Invalid POST Request';
+      const err = { success: false, status: 400, statusText: 'Bad Request' };
+      let res: any;
+      let errRes: any;
+      service.post(testURL, sampleData).subscribe(
+        _res => {
+          res = _res;
+        },
+        _err => {
+          errRes = _err;
+        }
+      );
+      const req = mockBackend.expectOne({ url: testURL, method: 'POST'}).flush(ERR_MESSAGE, err);
+
+      expect(res).toBeUndefined();
+      expect(errRes).toEqual(ERR_MESSAGE);
+    }));
+  });
+
+  describe('delete()', () => {
+    const testURL = 'https://www.delete-test.com';
+
+    it('should perform a GET request based on provided URL', fakeAsync(() => {
+      let res = { body: true };
 
       service.delete(testURL).subscribe(_res => {
         res = _res;
@@ -153,6 +215,27 @@ describe('RequestService', () => {
       expect(body).toBe(true);
 
       mockBackend.verify();
+    }));
+
+    it('should perform error handling when fail', fakeAsync(() => {
+      spyOn(devModeServiceSpy, 'isDevMode').and.returnValue(false);
+
+      const ERR_MESSAGE = 'Invalid DELETE Request';
+      const err = { success: false, status: 400, statusText: 'Bad Request' };
+      let res: any;
+      let errRes: any;
+      service.delete(testURL).subscribe(
+        _res => {
+          res = _res;
+        },
+        _err => {
+          errRes = _err;
+        }
+      );
+      const req = mockBackend.expectOne({ url: testURL, method: 'DELETE'}).flush(ERR_MESSAGE, err);
+
+      expect(res).toBeUndefined();
+      expect(errRes).toEqual(ERR_MESSAGE);
     }));
   });
 
@@ -189,5 +272,48 @@ describe('RequestService', () => {
       service.apiResponseFormatError('testing error');
       expect(console.error).toHaveBeenCalledWith('API response format error.\ntesting error');
     });
+  });
+  describe('handleError()', () => {
+    const ERR_MESSAGE = 'Invalid request';
+    const err = { success: false, status: 400, statusText: 'Bad Request' };
+    let errRes: any;
+    let request: any;
+    beforeEach(fakeAsync(() => {
+      request = service.get('test.com').subscribe(
+        _res => _res,
+        _err => {
+          errRes = _err;
+        }
+      );
+    }));
+
+    it('should only run console.error on dev mode', () => {
+      spyOn(devModeServiceSpy, 'isDevMode').and.returnValue(true);
+      spyOn(console, 'error');
+
+      mockBackend.expectOne({ method: 'GET'}).flush(ERR_MESSAGE, err);
+      expect(devModeServiceSpy.isDevMode).toHaveBeenCalled();
+      expect(console.error).toHaveBeenCalled();
+      expect(devModeServiceSpy.isDevMode()).toBeTruthy();
+      expect(errRes).toEqual(ERR_MESSAGE);
+    });
+
+    it('should logout user on bad apikey', fakeAsync(() => {
+      const badKey = 'Expired apikey';
+      mockBackend.expectOne({ method: 'GET'}).flush({message: badKey}, err);
+      expect(service['loggedOut']).toBeTruthy();
+      tick(2000);
+      expect(service['loggedOut']).toEqual(false);
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['logout']);
+    }));
+
+    it('should throw error if static file retrival fail', fakeAsync(() => {
+      const badFile = 'static file pull error';
+      mockBackend.expectOne({ method: 'GET'}).flush({
+        error: '<!DOCTYPE html>',
+        message: badFile
+      }, err);
+      expect(errRes).toBeTruthy();
+    }));
   });
 });
