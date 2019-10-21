3f67d018f809946bdfe384c7b4c67895f8be897f
Shawn <shawn@practera.com>
2019-10-21 15:50:00 +0800

    [AV2-476]

--- src/app/services/router-enter.service.ts
+++ src/app/services/router-enter.service.ts
@@ -11,7 +11,6 @@ export class RouterEnter implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    // this.onEnter();
     this.subscription = this.router.events.subscribe(event => {
       // invoke the onEnter() function of the component if the routing match
       if (event instanceof NavigationEnd && event.url.includes(this.routeUrl)) {
