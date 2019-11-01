a8aac08d99c8f2f014bf94401e4e48ed00132b4f
Shawn <shawn@practera.com>
2019-11-01 10:15:21 +0800

    [AV2-494]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   pusherKey: '255f010d210933ca7675',
   env: 'sandbox',
   APIEndpoint: 'https://sandbox.practera.com/',
+  graphQL: 'https://bfj1v3jxd5.execute-api.ap-southeast-2.amazonaws.com/Prod/',
   intercomAppId: '',
   filestack: {
     key: 'AO6F4C72uTPGRywaEijdLz',
