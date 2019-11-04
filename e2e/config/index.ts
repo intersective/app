15e3b75877d6617e077f98af001de028c96da262
Sau Shen Chaw <cshen_581@hotmail.com>
2019-11-04 20:37:22 +0800

    added mentor

--- e2e/config/index.ts
+++ e2e/config/index.ts
@@ -4,6 +4,12 @@ export const USER = {
   name: 'protractor',
 };
 
+export const MENTOR = {
+  email: 'protractor@test.com',
+  password: 'kW96dLJHrQDaaLM',
+  name: 'protractor_mentor',
+};
+
 export const PROGRAM = {
   name: 'Zaw Slider Test Program'
 };
