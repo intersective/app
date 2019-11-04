99790dc4be048f5c80c3d01fdd5eeba152005bad
Shawn <shawn@practera.com>
2019-11-04 10:29:07 +0800

    [AV2-494]unit test

--- src/app/project/project.service.spec.ts
+++ src/app/project/project.service.spec.ts
@@ -41,24 +41,26 @@ describe('ProjectService', () => {
   it('when testing getProject(), should return correct data', () => {
     const response = {
       success: true,
-      data: Array.from({length: 5}, (x, i) => {
-        return {
-          id: i + 1,
-          name: 'm' + i,
-          description: 'des' + i,
-          is_locked: false,
-          progress: (i + 1) / 10,
-          Activity: Array.from({length: 3}, (y, j) => {
-            return {
-              id: i * 10 + j + 1,
-              name: 'activity name' + j,
-              is_locked: false,
-              lead_image: '',
-              progress: (i * 10 + j + 1) / 100,
-            };
-          })
-        };
-      })
+      data: {
+        milestones: Array.from({length: 5}, (x, i) => {
+          return {
+            id: i + 1,
+            name: 'm' + i,
+            description: 'des' + i,
+            is_locked: false,
+            progress: (i + 1) / 10,
+            activities: Array.from({length: 3}, (y, j) => {
+              return {
+                id: i * 10 + j + 1,
+                name: 'activity name' + j,
+                is_locked: false,
+                lead_image: '',
+                progress: (i * 10 + j + 1) / 100,
+              };
+            })
+          };
+        })
+      }
     };
     const expected = Array.from({length: 5}, (x, i) => {
       return {
@@ -83,7 +85,9 @@ describe('ProjectService', () => {
   });
 
   it('when testing getOverview(), it should return correct data', () => {
-    const response = {id: 2};
+    const response = {
+      id: 2
+    };
     requestSpy.get.and.returnValue(of(response));
     service.getOverview().subscribe(res => expect(res).toEqual(response));
   });
