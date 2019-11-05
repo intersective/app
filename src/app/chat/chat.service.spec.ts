a363e529d8c80f3c4d08785d078b4f8839fd948f
sasangachathumal <chathumal.sasanga02@gmail.com>
2019-11-05 10:38:22 +0530

    [AV2-401] setup test bed, providers.

--- src/app/chat/chat.service.spec.ts
+++ src/app/chat/chat.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { RequestService } from '@shared/request/request.service';
+import { UtilsService } from '@services/utils.service';
+import { PusherService } from '@shared/pusher/pusher.service';
 import { ChatService } from './chat.service';
 
 describe('ChatService', () => {
-  // let service: ChatService;
+  let service: ChatService;
+  let requestSpy: jasmine.SpyObj<RequestService>;
+  let utils: UtilsService;
 
   beforeEach(() => {
-    // TestBed.configureTestingModule({});
-    // service = TestBed.get(ChatService);
+    TestBed.configureTestingModule({
+      providers: [
+        ChatService,
+        UtilsService,
+        {
+          provide: RequestService,
+          useValue: jasmine.createSpyObj('RequestService', ['get', 'post', 'apiResponseFormatError'])
+        }
+      ]
+    });
+    service = TestBed.get(ChatService);
+    requestSpy = TestBed.get(RequestService);
+    utils = TestBed.get(UtilsService);
   });
 
   it('should be created', () => {
-    // expect(service).toBeTruthy();
+    expect(service).toBeTruthy();
   });
 
   // it('should instantiated with variables', () => {
