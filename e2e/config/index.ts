019e4c2687300fd1b811f24189d4859e2acc37be
Sau Shen Chaw <cshen_581@hotmail.com>
2019-11-05 15:40:03 +0800

    [AV2-483] changes based on new switcher workflow & added reviewer

--- e2e/config/index.ts
+++ e2e/config/index.ts
@@ -1,3 +1,10 @@
+// added to test single program workflow (shouldn't stop user at program switcher page)
+export const SINGLE_PROGRAM_USER = {
+  email: 'protractor_single_program@test.com',
+  password: 'kW96dLJHrQDaaLM',
+  name: 'protractor_single_program',
+};
+
 export const USER = {
   email: 'protractor@test.com',
   password: 'kW96dLJHrQDaaLM',
@@ -5,11 +12,11 @@ export const USER = {
 };
 
 export const MENTOR = {
-  email: 'protractor@test.com',
+  email: 'protractor_mentor@test.com',
   password: 'kW96dLJHrQDaaLM',
   name: 'protractor_mentor',
 };
 
 export const PROGRAM = {
-  name: 'Zaw Slider Test Program'
+  name: 'AppV2 E2E Tests'
 };
