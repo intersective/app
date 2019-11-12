90bbb3c45f78d00175a40dd4b8b23968b12d211d
Shawn <shawn@practera.com>
2019-11-12 15:27:31 +0800

    [Av2-482]

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { UtilsService } from '@services/utils.service';
+import { RouterEnter } from '@services/router-enter.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent extends RouterEnter {
+  routeUrl = '/app/activity';
   activityId: number;
   topicId: number;
   assessmentId: number;
@@ -19,19 +21,23 @@ export class TasksComponent implements OnInit {
     public router: Router,
     private route: ActivatedRoute,
     public utils: UtilsService
-  ) { }
-
-  ngOnInit() {
-    this.activityId = +this.route.snapshot.paramMap.get('id');
+  ) {
+    super(router);
   }
-  ngAfterViewInit() {
-    this.activity.onEnter();
+
+  onEnter() {
+    this.activityId = +this.route.snapshot.paramMap.get('id');
+    // trigger onEnter after the element get generated
+    setTimeout(() => {
+      this.activity.onEnter();
+    });
   }
 
   goto(event) {
     switch (event.type) {
       case 'topic':
         this.topicId = event.topicId;
+        // hide the assessment component
         this.assessmentId = null;
         // trigger onEnter after the element get generated
         setTimeout(() => {
@@ -41,6 +47,7 @@ export class TasksComponent implements OnInit {
       case 'assessment':
         this.assessmentId = event.assessmentId;
         this.contextId = event.contextId;
+        // hide the topic component
         this.topicId = null;
         // trigger onEnter after the element get generated
         setTimeout(() => {
