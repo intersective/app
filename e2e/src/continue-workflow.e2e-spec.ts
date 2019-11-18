10d3c65f41559f34b86b0416437fb533604669f6
Sau Shen Chaw <cshen_581@hotmail.com>
2019-11-18 23:35:56 +0800

    [AV2-498] additional test for unfinished task

--- e2e/src/continue-workflow.e2e-spec.ts
+++ e2e/src/continue-workflow.e2e-spec.ts
@@ -16,7 +16,9 @@ describe('AppV2 - Continue workflow', () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
-  it('should login and select a program', () => {
+  it('should login and redirect to home screen', () => {
+    page.navigateTo();
+    browser.executeScript('window.localStorage.clear();');
     page.navigateTo('/');
     browser.sleep(2000);
     browser.ignoreSynchronization = true;
@@ -33,13 +35,6 @@ describe('AppV2 - Continue workflow', () => {
     browser.sleep(8000);
     const homeComponentContent = page.getHomeScreen();
     expect(homeComponentContent.element(by.css('h1')).getText()).toEqual(PROGRAM.name);
-
-    // // available program should be more than 0
-    // const programs = element.all(by.css('ion-col'));
-    // expect(programs.count()).toBeGreaterThan(0);
-
-    // const firstProgram = programs.first();
-    // expect(firstProgram.getText()).toEqual(PROGRAM.name);
   });
 
   it('should browse into project tab', () => {
@@ -55,7 +50,7 @@ describe('AppV2 - Continue workflow', () => {
     });
   });
 
-  it('should navigate to next task by the sequence of API', () => {
+  it('should navigate to project if all task is completed in an activity', () => {
     page.navigateTo('/app/project');
     browser.sleep(3000);
 
@@ -63,8 +58,10 @@ describe('AppV2 - Continue workflow', () => {
     const projectScreen = element(by.css('app-project'));
     const milestoneList = projectScreen.element(by.css('ion-content')).element(by.css('ion-list'));
     milestoneList.all(by.css('ion-item')).then(milestones => {
-      const [first, seconds] = milestones;
+      const [first, second] = milestones;
 
+      // first milestone: 1
+      expect(first.element(by.css('ion-text')).getText()).toEqual('1');
       first.element(by.css('ion-list')).all(by.css('ion-item')).then(activities => {
         const [ firstActivity, secondActivity ] = activities;
 
@@ -77,19 +74,76 @@ describe('AppV2 - Continue workflow', () => {
         activity.element(by.css('ion-content')).element(by.css('ion-card')).all(by.css('ion-item')).then(tasks => {
           const [firstTask] = tasks;
           firstTask.click();
-          browser.sleep(3000);
+          browser.sleep(5000);
         });
 
         // task screen
         const assessment = element(by.css('app-assessment'));
         expect(assessment.isDisplayed()).toBeTruthy();
         expect(assessment.element(by.css('h1')).getText()).toEqual('Assessment 1');
-        const continueBtn = assessment.element(by.tagName('ion-footer')).element(by.css('ion-button'));
+        const continueBtn = assessment.element(by.tagName('ion-footer')).element(by.tagName('ion-toolbar')).element(by.tagName('ion-button'));
         continueBtn.click();
         browser.sleep(5000);
 
         // back to project
         expect(projectScreen.isDisplayed()).toBeTruthy();
+      });
+    });
+  });
+
+  it('should navigate to next unfinished task by the order of task', () => {
+    page.navigateTo('/app/project');
+    browser.sleep(5000);
+
+    // project screen
+    const projectScreen = element(by.css('app-project'));
+    projectScreen.all(by.css('ion-content > ion-list > ion-item')).then(milestones => {
+      const [first, second] = milestones;
+
+    // second milestone: 2
+      expect(second.element(by.css('.project-title')).getText()).toEqual('2');
+      second.all(by.css('ion-list > ion-item')).then(activities => {
+        const [firstActivity] = activities;
+
+        firstActivity.click();
+        browser.sleep(5000);
+
+        const activity = element(by.css('app-activity'));
+        expect(activity.isDisplayed()).toBeTruthy();
+
+    // finishing up first task
+        activity.element(by.css('ion-content')).element(by.css('ion-card')).all(by.css('ion-item')).then(tasks => {
+          const [firstTask] = tasks;
+          firstTask.click();
+          browser.sleep(5000);
+        });
+
+    // 1st task screen
+        const assessment = element(by.css('app-assessment'));
+        expect(assessment.isDisplayed()).toBeTruthy();
+        expect(assessment.element(by.css('h1')).getText()).toEqual('Milestone 2: Activity 1');
+        const continueBtn = assessment.element(by.tagName('ion-footer')).element(by.tagName('ion-toolbar')).element(by.tagName('ion-button'));
+        continueBtn.click();
+        // page.submitAssessment(assessment);
+        browser.sleep(8000);
+
+    // 2nd task screen
+        /*element.all(by.css('app-assessment')).then(asmts => {
+          expect(asmts.length).toBeGreaterThan(1);
+
+          const [last, latest] = asmts;
+          // const latest = element(by.css('app-assessment'));
+          expect(latest.isDisplayed()).toBeTruthy();
+          expect(latest.element(by.css('h1')).getText()).toEqual('Milestone 2: Activity 2');
+          page.submitAssessment(latest);
+          browser.sleep(8000);
+        });*/
+
+    // 3rd task screen
+        const topic = element(by.css('app-topic'));
+        expect(topic.isDisplayed()).toBeTruthy();
+        expect(topic.element(by.css('h3')).getText()).toEqual('Milestone 2: Topic 3');
+      });
 
     // for undone
     /**
@@ -118,11 +172,13 @@ describe('AppV2 - Continue workflow', () => {
         browser.sleep(5000);
     **/
 
-      });
     });
   });
 
   it('should redirect user back to project page for last task in an activity', () => {
+    page.navigateTo('/app/project');
+    browser.sleep(3000);
+
     const projectScreen = element(by.css('app-project'));
     const milestoneList = projectScreen.element(by.css('ion-content')).element(by.css('ion-list'));
 
@@ -139,16 +195,16 @@ describe('AppV2 - Continue workflow', () => {
         expect(activity.isDisplayed()).toBeTruthy();
 
         activity.element(by.css('ion-content')).element(by.css('ion-card')).all(by.css('ion-item')).then(tasks => {
-          const [firstTask, secondTask, lastTask] = tasks;
-          lastTask.click();
-          browser.sleep(3000);
+          const [firstTask, secondTask, thirdTask] = tasks;
+          thirdTask.click();
+          browser.sleep(6000);
         });
 
         // last task screen
         const assessment = element(by.css('app-assessment'));
         expect(assessment.isDisplayed()).toBeTruthy();
-        expect(assessment.element(by.css('h1')).getText()).toEqual('Assessment 1');
-        const continueBtn = assessment.element(by.tagName('ion-footer')).element(by.css('ion-button'));
+        expect(assessment.element(by.css('h1')).getText()).toEqual('Assessment 3'); // last task
+        const continueBtn = assessment.element(by.tagName('ion-footer')).element(by.tagName('ion-button'));
         continueBtn.click();
         browser.sleep(5000);
 
