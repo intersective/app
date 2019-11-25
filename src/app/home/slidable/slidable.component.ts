852d549a32fe3cb27fe873abfbd14794c35f17f1
Sau Shen Chaw <cshen_581@hotmail.com>
2019-11-25 20:19:05 +0800

    [AV2-491] slidable ui now

--- src/app/home/slidable/slidable.component.ts
+++ src/app/home/slidable/slidable.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-slidable',
   templateUrl: './slidable.component.html',
   styleUrls: ['./slidable.component.scss']
 })
-export class SlidableComponent implements OnInit {
+export class SlidableComponent implements OnInit, OnChanges {
   @Input() notifications;
+  // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
 
   constructor() { }
 
@@ -14,4 +19,7 @@ export class SlidableComponent implements OnInit {
     console.log('NOTI::', this.notifications);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+  }
 }
