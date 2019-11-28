f28217ab8b588db375e88217e847471a2f787883
Shawn <shawn@practera.com>
2019-11-28 12:51:41 +0800

    [AV2-500]unit test

--- src/app/events/events.component.spec.ts
+++ src/app/events/events.component.spec.ts
@@ -1,57 +1,97 @@
 import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { EventsComponent } from './events.component';
-import { ActivityModule } from '../activity/activity.module';
-import { TopicModule } from '../topic/topic.module';
+import { EventListModule } from '../event-list/event-list.module';
+import { EventDetailModule } from '../event-detail/event-detail.module';
 import { AssessmentModule } from '../assessment/assessment.module';
 import { Observable, of, pipe } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ActivatedRouteStub } from '@testing/activated-route-stub';
 import { MockRouter } from '@testing/mocked.service';
-import { BrowserStorageService } from '@services/storage.service';
 
 describe('EventsComponent', () => {
   let component: EventsComponent;
   let fixture: ComponentFixture<EventsComponent>;
   let routeStub: ActivatedRouteStub;
-  let storageSpy: jasmine.SpyObj<BrowserStorageService>;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     imports: [ ActivityModule, TopicModule, AssessmentModule ],
-  //     declarations: [ EventsComponent ],
-  //     providers: [
-  //       {
-  //         provide: Router,
-  //         useClass: MockRouter
-  //       },
-  //       {
-  //         provide: ActivatedRoute,
-  //         useValue: new ActivatedRouteStub({ id: 1 })
-  //       },
-  //       {
-  //         provide: BrowserStorageService,
-  //         useValue: jasmine.createSpyObj('BrowserStorageService', ['getUser'])
-  //       },
-  //     ]
-  //   })
-  //   .compileComponents();
-  // }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ EventListModule, EventDetailModule, AssessmentModule ],
+      declarations: [ EventsComponent ],
+      providers: [
+        {
+          provide: Router,
+          useClass: MockRouter
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: new ActivatedRouteStub({ activity_id: 1, event_id: 2 })
+        },
+      ]
+    })
+    .compileComponents();
+  }));
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(EventsComponent);
-  //   component = fixture.componentInstance;
-  //   routeStub = TestBed.get(ActivatedRoute);
-  //   storageSpy = TestBed.get(BrowserStorageService);
-  //   // mock the activity object
-  //   component.eventList = { onEnter() {} };
-  //   // mock the event detail object
-  //   component.eventDetail = { onEnter() {} };
-  //   // mock the assessment object
-  //   component.assessment = { onEnter() {} };
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EventsComponent);
+    component = fixture.componentInstance;
+    routeStub = TestBed.get(ActivatedRoute);
+    // mock the activity object
+    component.eventList = { onEnter() {} };
+    // spy on the onEnter function
+    spyOn(component.eventList, 'onEnter');
+    // mock the assessment object
+    component.assessment = { onEnter() {} };
+    // spy on the onEnter function
+    spyOn(component.assessment, 'onEnter');
+  });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('onEnter() should get correct data', fakeAsync(() => {
+    component.onEnter();
+    expect(component.activityId).toEqual(1);
+    expect(component.eventId).toEqual(2);
+    expect(component.assessmentId).toBeNull();
+    expect(component.currentEvent).toBeNull();
+    tick();
+    expect(component.eventList.onEnter).toHaveBeenCalled();
+  }));
+
+  it('goto() should get correct data', () => {
+    const event = {
+      id: 11,
+      name: 'event',
+      description: 'des',
+      location: 'location',
+      activityId: 1,
+      activityName: 'activity',
+      startTime: '',
+      endTime: '',
+      capacity: 10,
+      remainingCapacity: 1,
+      isBooked: true,
+      singleBooking: true,
+      canBook: true,
+    };
+    component.goto(event);
+    expect(component.currentEvent).toEqual(event);
+    expect(component.eventId).toEqual(11);
+    expect(component.assessmentId).toBeNull();
+    expect(component.contextId).toBeNull();
+  });
+
+  it('checkin() should get correct data', fakeAsync(() => {
+    const params = {
+      assessmentId: 1,
+      contextId: 2
+    };
+    component.checkin(params);
+    expect(component.assessmentId).toEqual(1);
+    expect(component.contextId).toEqual(2);
+    tick();
+    expect(component.assessment.onEnter).toHaveBeenCalled();
+  }));
 
 });
