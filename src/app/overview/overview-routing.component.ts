88b5dbb9bddacec1b1fe04bd1bf98e39751e7837
Sau Shen Chaw <cshen_581@hotmail.com>
2019-11-30 13:37:43 +0800

    [AV2-491] added centerized title at overview

--- src/app/overview/overview-routing.component.ts
+++ src/app/overview/overview-routing.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserStorageService } from '@services/storage.service';
 
 @Component({
   selector: 'app-overview-routing',
   templateUrl: './overview-routing.component.html'
 })
-export class OverviewRoutingComponent {}
+export class OverviewRoutingComponent implements OnInit {
+
+  programName: string;
+  constructor(private storage: BrowserStorageService) { }
+
+  ngOnInit() {
+    this.programName = this.storage.getUser().programName;
+  }
+}
