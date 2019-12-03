557d2d4419772b815a95bb71c7830577753212aa
Sau Shen Chaw <cshen_581@hotmail.com>
2019-12-03 10:03:01 +0800

    [AV2-491] corrected ngCircle usage & adjusting detailed dimension

--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -9,7 +9,6 @@ import { HomeService } from './home/home.service';
 import { TodoCardComponent } from './home/todo-card/todo-card.component';
 import { SlidableComponent } from './home/slidable/slidable.component';
 import { ProjectComponent } from './project/project.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FastFeedbackModule } from '../fast-feedback/fast-feedback.module';
 import { ProjectService } from './project/project.service';
 
@@ -40,23 +39,6 @@ import { ProjectService } from './project/project.service';
       },
     ]),
 
-    NgCircleProgressModule.forRoot({
-      'outerStrokeLinecap': 'butt',
-      'toFixed': 0,
-      'outerStrokeColor': 'var(--ion-color-primary)',
-      'backgroundColor': 'var(--ion-color-light)',
-      'backgroundStroke': 'var(--ion-color-primary)',
-      'showTitle': false,
-      'showSubtitle': false,
-      'startFromZero': false,
-      'showInnerStroke': false,
-      'showUnits': false,
-      'backgroundStrokeWidth': 2,
-      'maxPercent': 100,
-      'outerStrokeWidth': 8,
-      'radius': 4,
-      'space': -20
-    }),
     FastFeedbackModule,
   ],
   declarations: [
