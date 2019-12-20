e390433beef55e7213043652a8ef7643c44a7939
Mihai Petrescu <mihai.petrescu@gmail.com>
2019-12-20 15:32:53 +1100

    working the the custom environments file

---
+++ src/environments/environment.custom.ts
@@ -0,0 +1,25 @@
+export const environment = {
+    production: true,
+    appkey: 'b11e7c189b',
+    pusherKey: '255f010d210933ca7675',
+    env: 'stage',
+    APIEndpoint: '<CUSTOM_API_ENDPOINT>',
+    intercomAppId: 'pef1lmo8',
+    filestack: {
+      key: 'AO6F4C72uTPGRywaEijdLz',
+      s3Config: {
+        location: 's3',
+        container: 'practera-aus',
+        region: '<CUSTOM_AWS_REGION>',
+        paths: {
+          any: '/appv2/stage/uploads/',
+          image: '/appv2/stage/uploads/',
+          video: '/appv2/stage/video/upload/'
+        }
+      }
+    },
+    defaultCountryModel: 'AUS',
+    intercom: false,
+    goMobile: false,
+  };
+  
