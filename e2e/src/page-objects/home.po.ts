d1e97843648d265bab14873a0620e80a48cd2d6d
Shawn <shawn@practera.com>
2019-12-20 11:48:38 +0800

    [AV2-551]

--- e2e/src/page-objects/home.po.ts
+++ e2e/src/page-objects/home.po.ts
@@ -3,6 +3,20 @@ import { AppPage } from './app.po';
 
 export class HomePage extends AppPage {
   parent = $('app-overview-routing');
+  home = this.parent.$('app-home');
+  textProgramName = this.parent.$('ion-title');
+  textProjectProgress = this.home.$('app-cicle-progress').$$('tspan').get(0);
+  btnAchievementAll = this.home.$('.achievement').$('a');
 
+  getProgramName() {
+    return this.textProgramName.getText();
+  }
 
+  getProjectProgress() {
+    return this.textProjectProgress.getText();
+  }
+
+  clickViewAll() {
+    return this.btnAchievementAll.click();
+  }
 }
