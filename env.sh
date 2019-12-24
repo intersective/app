d5ec379554e4bdb010497fc0067188924de0b6a0
Mihai Petrescu <mihai.petrescu@gmail.com>
2019-12-24 13:59:56 +1100

    putting a runtime version in

--- env.sh
+++ env.sh
@@ -2,6 +2,7 @@
 while IFS='=' read -r name value ; do
   if [[ $name == 'CUSTOM_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
+        sed -i "s#<$name>#${!name}#g" angular.json
   fi
   if [[ $name == 'FILESTACK_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/filestack.ts
