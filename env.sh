841faf09fea3615f95e1504c7bf56423055b9621
Mihai Petrescu <mihai.petrescu@gmail.com>
2019-12-24 15:07:27 +1100

    fixing the build

--- env.sh
+++ env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 while IFS='=' read -r name value ; do
-  if [[ $name == 'CUSTOM_'* ]]; then
+    if [[ $name == 'CUSTOM_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
         sed -i "s#<$name>#${!name}#g" angular.json
-  fi
-  if [[ $name == 'FILESTACK_'* ]]; then
+    fi
+    if [[ $name == 'FILESTACK_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/filestack.ts
-  fi
+    fi
 done < <(env)
 
-cat src/environments/environment.custom.ts
-cat angular.json
-cat src/environments/filestack.ts
+# cat src/environments/environment.custom.ts
+# cat angular.json
+# cat src/environments/filestack.ts
