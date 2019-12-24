753d5adae4ab49119718b8dc8d987a0bae8286f9
Mihai Petrescu <mihai.petrescu@gmail.com>
2019-12-24 12:32:53 +1100

    putting a runtime version in

--- env.sh
+++ env.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+while IFS='=' read -r name value ; do
+  if [[ $name == 'CUSTOM_'* ]]; then
+        sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
+  fi
+  if [[ $name == 'FILESTACK_'* ]]; then
+        sed -i "s#<$name>#${!name}#g" src/environments/filestack.ts
+  fi
+done < <(env)
 
-while IFS='=' read -r name value ; do echo $name; done < <(env)
-while IFS='=' read -r name value ; do  if [[ $name == 'CUSTOM_'* ]]; then sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts; fi; done < <(env)
-cat src/environment/environment.custom.ts
-cat src/environment/filestack.ts
+
