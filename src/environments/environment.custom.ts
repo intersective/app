42bcab834183f9dca277a6599bc76e03aec5020d
Mihai Petrescu <mihai.petrescu@gmail.com>
2020-01-03 10:11:33 +1100

    config file

--- src/environments/environment.custom.ts
+++ src/environments/environment.custom.ts
@@ -1,25 +1,24 @@
 export const environment = {
-    production: true,
-    appkey: 'b11e7c189b',
-    pusherKey: '255f010d210933ca7675',
-    env: 'stage',
-    APIEndpoint: 'https://stage.practera.com/',
-    intercomAppId: 'pef1lmo8',
-    filestack: {
-      key: 'AO6F4C72uTPGRywaEijdLz',
-      s3Config: {
-        location: 's3',
-        container: 'practera-aus',
-        region: 'ap-southeast-2',
-        paths: {
-          any: '/appv2/nu/uploads/',
-          image: '/appv2/nu/uploads/',
-          video: '/appv2/nu/video/upload/'
-        }
+  production: true,
+  appkey: 'b11e7c189b',
+  pusherKey: '255f010d210933ca7675',
+  env: '<CUSTOM_ENVIRONMENT>',
+  APIEndpoint: '<CUSTOM_API_ENDPOINT>',
+  intercomAppId: 'pef1lmo8',
+  filestack: {
+    key: 'AO6F4C72uTPGRywaEijdLz',
+    s3Config: {
+      location: 's3',
+      container: '<CUSTOM_S3_BUCKET>',
+      region: '<CUSTOM_AWS_REGION>',
+      paths: {
+        any: '/appv2/<CUSTOM_APP>/uploads/',
+        image: '/appv2/<CUSTOM_APP>/uploads/',
+        video: '/appv2/<CUSTOM_APP>/video/upload/'
       }
-    },
-    defaultCountryModel: 'AUS',
-    intercom: false,
-    goMobile: false,
-  };
-  
+    }
+  },
+  defaultCountryModel: '<CUSTOM_COUNTRY>',
+  intercom: false,
+  goMobile: false,
+};
