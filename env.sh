eac7c8cbfb00b95c37077115c605a8374b41a23b
Mihai Petrescu <mihai.petrescu@gmail.com>
2020-01-03 12:05:55 +1100

    documentation

--- env.sh
+++ env.sh
@@ -8,7 +8,3 @@ while IFS='=' read -r name value ; do
         sed -i "s#<$name>#${!name}#g" src/environments/filestack.ts
     fi
 done < <(env)
-
-cat src/environments/environment.custom.ts
-cat angular.json
-cat src/environments/filestack.ts
