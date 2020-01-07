d9bc1319a9381e399f78d21ae795d1911d84526d
Shawn <shawn@practera.com>
2020-01-07 16:24:43 +0800

    [AV2-552]

--- e2e/src/page-objects/program-switcher.po.ts
+++ e2e/src/page-objects/program-switcher.po.ts
@@ -4,11 +4,17 @@ import { AppPage } from './app.po';
 export class ProgramSwitcherPage extends AppPage {
   parent = $('app-switcher-program');
   row = this.parent.$(`ion-row`);
+  programCards = this.row.$$('ion-col')
   firstCard = this.row.$$('ion-col').get(0).$('ion-card');
 
   clickFirstCard() {
-    return this.firstCard.click();
+    return this.clickCard(0);
   }
+
+  clickCard(i) {
+    return this.programCards.get(i).$('ion-card').click();
+  }
+
   waitUntilFirstCardClickable() {
     return this.wait(ExpectedConditions.elementToBeClickable(this.firstCard));
   }
