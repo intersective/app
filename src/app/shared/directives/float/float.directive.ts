6b7e8c96327c5ddbc08fbdbfa3022d136dfc8cbd
chaw <cshen_581@hotmail.com>
2020-02-05 12:30:10 +0800

    [AV2-587] track parents with cards

--- src/app/shared/directives/float/float.directive.ts
+++ src/app/shared/directives/float/float.directive.ts
@@ -1,17 +1,22 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { UtilsService } from '@services/utils.service';
 
 @Directive({
   selector: '[appFloat]'
 })
 export class FloatDirective {
-  constructor(private el: ElementRef) { }
-
-  @HostListener('mouseenter') onMouseEnter() {
-    this.float('yellow');
-  }
+  constructor(private el: ElementRef, private utils: UtilsService) { }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.float(null);
+  }
+
+  @HostListener('wheel') onScroll() {
+    this.float('yellow');
+  }
+
+  @HostListener('mousewheel') onMouseScroll() {
+    console.log('~mousewheel~');
   }
 
   private float(color: string) {
@@ -19,10 +24,28 @@ export class FloatDirective {
     this.isDisplaying(this.el.nativeElement);
   }
 
+  getBoundaryRect(element) {
+    const child = element.getBoundingClientRect();
+    const el = this.el.nativeElement.getBoundingClientRect();
+
+    // const parent = element.parentElement.getBoundingClientRect();
+    console.log(el, child);
+    if (el.top < parent.top) {
+      element.style.border = '1px solid';
+    } else {
+      element.style.border = 'none';
+    }
+  }
+
   // preliminary test to calculate appearance in screen
   private isDisplaying(element) {
-    const el = element.getBoundingClientRect();
-    const parent = element.parentElement.getBoundingClientRect();
-    console.log(el, parent);
+    const cards = element.getElementsByTagName('ion-card');
+    console.log(cards);
+
+    if (cards.length > 0) {
+      this.utils.each(cards, card => {
+        this.getBoundaryRect(card);
+      });
+    }
   }
 }
