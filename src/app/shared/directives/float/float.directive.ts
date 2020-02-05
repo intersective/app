2df139c1e56615a43cf6859ac7345b1e4093cc26
chaw <cshen_581@hotmail.com>
2020-02-05 14:59:37 +0800

    [AV2-587] reduced gap between ion-cards inside

--- src/app/shared/directives/float/float.directive.ts
+++ src/app/shared/directives/float/float.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 import { UtilsService } from '@services/utils.service';
 
+const ION_DEFAULT_CARD_SHADOW = '0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12)';
+
 @Directive({
   selector: '[appFloat]'
 })
@@ -20,11 +22,14 @@ export class FloatDirective {
     this.isDisplaying(this.el.nativeElement);
   }
 
-  renderStyle(element): void {
-    element.style = {
-      border: '1px solid',
-      boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
-    };
+  renderStyle(element, options = {
+    activate: true
+  }): void {
+    if (options.activate === true) {
+      element.style.boxShadow = '0 4px 12px rgba(0,0,0,0.2)';
+    } else {
+      element.style.boxShadow = ION_DEFAULT_CARD_SHADOW;
+    }
   }
 
   getBoundaryRect(element) {
@@ -32,16 +37,14 @@ export class FloatDirective {
     const el = this.el.nativeElement.getBoundingClientRect();
     element.style.transition = 'all 0.1s ease';
 
-    if (el.top < (child.top - 10)) { // top
+    if (el.top < (child.top - 5)) { // top
       this.renderStyle(element);
     } else {
-      element.style.border = 'none';
-      element.style.boxShadow = 'none';
+      this.renderStyle(element, { activate: false});
     }
 
-    if (el.bottom < (child.bottom + 10)) { // bottom
-      element.style.border = 'none';
-      element.style.boxShadow = 'none';
+    if (el.bottom < (child.bottom + 5)) { // bottom
+      this.renderStyle(element, { activate: false});
     }
   }
 
