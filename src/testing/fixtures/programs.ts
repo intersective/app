cfcdd1b1b603c2d85934d5b2adc03b3843a3ee47
chaw <cshen_581@hotmail.com>
2020-02-06 20:53:09 +0800

    [AV2-418] added fixture

---
+++ src/testing/fixtures/programs.ts
@@ -0,0 +1,19 @@
+import { ProgramObj } from '@app/switcher/switcher.service';
+export const ProgramFixture: ProgramObj[] = [
+  {
+    program: {
+      id: 1,
+      experience_id: 1,
+      name: 'test program',
+    },
+    project: {
+      id: 1,
+    },
+    timeline: {
+      id: 1,
+    },
+    enrolment: {
+      contact_number: '0123456789'
+    },
+  }
+];
