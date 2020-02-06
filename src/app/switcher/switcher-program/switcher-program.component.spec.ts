24b91e63404d694983dd72ba4a69c026b28543df
chaw <cshen_581@hotmail.com>
2020-02-07 00:15:45 +0800

    [AV2-418] done switcher-program.component

--- src/app/switcher/switcher-program/switcher-program.component.spec.ts
+++ src/app/switcher/switcher-program/switcher-program.component.spec.ts
@@ -1,23 +1,45 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, flushMicrotasks, tick } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { NewRelicService } from '@shared/new-relic/new-relic.service';
 import { SwitcherProgramComponent } from './switcher-program.component';
 import { SwitcherService } from '../switcher.service';
-import { MockSwitcherService } from '@testing/mocked.service';
+import { MockSwitcherService, MockRouter, MockNewRelicService } from '@testing/mocked.service';
 import { ProgramFixture } from '@testing/fixtures/programs';
+import { Router } from '@angular/router';
+import { PusherService } from '@shared/pusher/pusher.service';
+import { NotificationService } from '@shared/notification/notification.service';
+import { UtilsService } from '@services/utils.service';
+import { SharedModule } from '@shared/shared.module';
+import { LoadingController } from '@ionic/angular';
 
 describe('SwitcherProgramComponent', () => {
   let component: SwitcherProgramComponent;
   let fixture: ComponentFixture<SwitcherProgramComponent>;
   let newrelicSpy: jasmine.SpyObj<NewRelicService>;
-  let swicherSpy: jasmine.SpyObj<SwitcherService>;
+  let switcherSpy: jasmine.SpyObj<SwitcherService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let loadingSpy: jasmine.SpyObj<LoadingController>;
+  let notifySpy: jasmine.SpyObj<NotificationService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [SharedModule, HttpClientTestingModule],
       declarations: [SwitcherProgramComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        NewRelicService,
+        PusherService,
+        NotificationService,
+        UtilsService,
+        LoadingController,
+        {
+          provide: NewRelicService,
+          useClass: MockNewRelicService
+        },
+        {
+          provide: Router,
+          useClass: MockRouter,
+        },
         {
           provide: SwitcherService,
           useClass: MockSwitcherService,
@@ -28,7 +50,10 @@ describe('SwitcherProgramComponent', () => {
     fixture = TestBed.createComponent(SwitcherProgramComponent);
     component = fixture.componentInstance;
     newrelicSpy = TestBed.get(NewRelicService);
-    swicherSpy = TestBed.get(SwitcherService);
+    switcherSpy = TestBed.get(SwitcherService);
+    routerSpy = TestBed.get(Router);
+    loadingSpy = TestBed.get(LoadingController);
+    notifySpy = TestBed.get(NotificationService);
   }));
 
   beforeEach(() => {
@@ -43,16 +68,81 @@ describe('SwitcherProgramComponent', () => {
 
   describe('ngOnInit()', () => {
     it('should instantiate with API program list', () => {
+      spyOn(switcherSpy.getPrograms(), 'subscribe');
       component.ngOnInit();
-      expect(newrelicSpy.setPageViewName).toHaveBeenCalled();
-      expect(swicherSpy.getPrograms().subscribe).toHaveBeenCalled();
+
+      expect(newrelicSpy.setPageViewName).toHaveBeenCalledWith('program switcher');
+      expect(switcherSpy.getPrograms().subscribe).toHaveBeenCalled();
       expect(component.programs).toEqual(ProgramFixture);
     });
   });
 
   describe('switch()', () => {
-    it('should switch to selected program based on provided programmatic index', () => {
+    const testRoute = ['test', 'path'];
+    const programIndex = 0;
 
+    beforeEach(() => {
+      component.programs = ProgramFixture; // load fixture
+      spyOn(loadingSpy, 'create').and.returnValue({
+        present: () => new Promise(res => res('test')),
+        dismiss: () => new Promise(res => res(true))
+      });
     });
+
+    it('should switch to selected program based on provided programmatic index', fakeAsync(() => {
+      switcherSpy.switchProgramAndNavigate = jasmine.createSpy('switchProgramAndNavigate').and.returnValue(new Promise(res => res(testRoute)));
+
+      component.switch(programIndex);
+      flushMicrotasks();
+
+      expect(newrelicSpy.createTracer).toHaveBeenCalled();
+      expect(newrelicSpy.actionText).toHaveBeenCalled();
+      expect(switcherSpy.switchProgramAndNavigate).toHaveBeenCalledWith(ProgramFixture[programIndex]);
+      expect(routerSpy.navigate).toHaveBeenCalledWith(testRoute);
+    }));
+
+    it('should popup error at failed program switching', fakeAsync(() => {
+      const error = {
+        msg: 'test error msg',
+      };
+      switcherSpy.switchProgramAndNavigate = jasmine.createSpy('switchProgramAndNavigate').and.returnValue(new Promise((res, reject) => reject(error)
+      ));
+      spyOn(notifySpy, 'alert');
+
+      component.switch(programIndex);
+      flushMicrotasks();
+
+      expect(newrelicSpy.createTracer).toHaveBeenCalled();
+      expect(newrelicSpy.actionText).toHaveBeenCalled();
+      expect(switcherSpy.switchProgramAndNavigate).toHaveBeenCalledWith(ProgramFixture[programIndex]);
+      expect(routerSpy.navigate).not.toHaveBeenCalled();
+      expect(notifySpy.alert).toHaveBeenCalledWith({
+        header: 'Error switching program',
+        message: error.msg,
+      });
+      expect(newrelicSpy.noticeError).toHaveBeenCalled();
+    }));
+
+    it('should popup error at failed program switching (without error message)', fakeAsync(() => {
+      const error = {
+        different: 'kind of format'
+      };
+      switcherSpy.switchProgramAndNavigate = jasmine.createSpy('switchProgramAndNavigate').and.returnValue(new Promise((res, reject) => reject(error)
+      ));
+      spyOn(notifySpy, 'alert');
+
+      component.switch(programIndex);
+      flushMicrotasks();
+
+      expect(newrelicSpy.createTracer).toHaveBeenCalled();
+      expect(newrelicSpy.actionText).toHaveBeenCalled();
+      expect(switcherSpy.switchProgramAndNavigate).toHaveBeenCalledWith(ProgramFixture[programIndex]);
+      expect(routerSpy.navigate).not.toHaveBeenCalled();
+      expect(notifySpy.alert).toHaveBeenCalledWith({
+        header: 'Error switching program',
+        message: JSON.stringify(error)
+      });
+      expect(newrelicSpy.noticeError).toHaveBeenCalled();
+    }));
   });
 });
