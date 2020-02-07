a0609527966f06718d9a0b10a41fcf25c8c95963
chaw <cshen_581@hotmail.com>
2020-02-07 09:40:57 +0800

    [AV2-418] also added switcherService unit test

--- src/test.ts
+++ src/test.ts
@@ -15,6 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /switcher\-program\.component\.spec\.ts$/);
+const context = require.context('./', true, /switcher\.service\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
