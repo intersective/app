b7b5a2ccc70f1451baca7eea56c18c85c9509f2b
Barry Chen <barry@intersective.com>
2020-02-07 11:53:23 +1100

    Update protractor-ci.conf.js

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -3,8 +3,35 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
-  }
+    args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
+  },
+  multiCapabilities: [
+    {
+      browserName: 'chrome',
+      shardTestFiles: true,
+      maxInstances: 5,
+      specs: [
+        './src/features/desktop/*.feature'
+      ],
+      chromeOptions: {
+        args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+      }
+    },
+    {
+      browserName: 'chrome',
+      shardTestFiles: true,
+      maxInstances: 5,
+      specs: [
+        './src/features/mobile/*.feature'
+      ],
+      chromeOptions: {
+        args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+        mobileEmulation : {
+          deviceName: 'Galaxy S5'
+        }
+      }
+    }
+  ],
 };
 
 exports.config = config;
