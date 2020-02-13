c00da4fbb3deb194d438781e5050172f84d2e869
sasangachathumal <chathumal.sasanga02@gmail.com>
2020-02-13 17:39:36 +0530

    [AV2-586] create skeleton elements for achievement badges. added styles for them. create skeleton elements for progress circle. added some styles for it. added loging logic to achievement badges.

--- src/app/shared/components/achievement-badge/achievement-badge.component.ts
+++ src/app/shared/components/achievement-badge/achievement-badge.component.ts
@@ -12,6 +12,7 @@ export class AchievementBadgeComponent {
 
   @Input() achievement: Achievement;
   @Input() showName: Boolean = false;
+  @Input() loading: Boolean;
 
   constructor(
     private notificationService: NotificationService,
