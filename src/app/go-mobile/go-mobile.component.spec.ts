5225c18a0ad47b9d0945641345d2047598d8b3e0
chaw <cshen_581@hotmail.com>
2020-02-20 11:53:10 +0800

    [AV2-409] component test is done

--- src/app/go-mobile/go-mobile.component.spec.ts
+++ src/app/go-mobile/go-mobile.component.spec.ts
@@ -1,6 +1,6 @@
 import { IonicModule } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import {
   HttpTestingController,
   HttpClientTestingModule
@@ -18,11 +18,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TextMaskModule } from 'angular2-text-mask';
 import { Router } from '@angular/router';
 import { environment } from 'environments/environment';
+import { of, throwError } from 'rxjs';
 
 describe('GoMobileComponent', () => {
   let component: GoMobileComponent;
   let fixture: ComponentFixture<GoMobileComponent>;
   let newRelicSpy: NewRelicService;
+  let goMobileSpy: GoMobileService;
+  let notificationSpy: NotificationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -37,7 +40,10 @@ describe('GoMobileComponent', () => {
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       providers: [
         GoMobileService,
-        NotificationService,
+        {
+          provide: NotificationService,
+          useValue: jasmine.createSpyObj(['alert', 'presentToast']),
+        },
         {
           provide: NewRelicService,
           useClass: MockNewRelicService
@@ -59,6 +65,8 @@ describe('GoMobileComponent', () => {
     fixture = TestBed.createComponent(GoMobileComponent);
     component = fixture.debugElement.componentInstance;
     newRelicSpy = TestBed.get(NewRelicService);
+    goMobileSpy = TestBed.get(GoMobileService);
+    notificationSpy = TestBed.get(NotificationService);
   });
 
   it('should created', () => {
@@ -86,4 +94,95 @@ describe('GoMobileComponent', () => {
     });
   });
 
+  describe('submit()', () => {
+    beforeEach(() => {
+      component.countryModel = 'AUS';
+      spyOn(goMobileSpy, 'submit').and.returnValue(of(true));
+      expect(component.sendingSMS).toBeFalsy();
+    });
+
+    it('should prevent bad contact number format', () => {
+      component.profile.contactNumber = '0123456789'; // mock bad format
+      component.submit();
+      expect(notificationSpy.presentToast).toHaveBeenCalledWith('Invalid contact number', false);
+    });
+
+    it('should submit to update profile', () => {
+      component.profile.contactNumber = '012345678912'; // mock correct format
+      component.submit();
+
+      expect(newRelicSpy.addPageAction).toHaveBeenCalled();
+      expect(goMobileSpy.submit).toHaveBeenCalled();
+      expect(notificationSpy.alert).toHaveBeenCalled();
+    });
+
+    it('should fail gracefully (notify user) if API return error', () => {
+      goMobileSpy.submit = jasmine.createSpy('submit').and.returnValue(throwError(false));
+      component.profile.contactNumber = '012345678912'; // mock correct format
+      component.submit();
+
+      expect(newRelicSpy.addPageAction).toHaveBeenCalled();
+      expect(goMobileSpy.submit).toHaveBeenCalled();
+      expect(notificationSpy.alert).toHaveBeenCalledWith({ header: 'Error submitting contact info', message: 'false' });
+    });
+  });
+
+  describe('validateContactNumber()', () => {
+    beforeEach(() => {
+      component.profile.contactNumber = '012345678912'; // 12 digit contact numbers
+    });
+
+    it('should validate AUS format of contact number', () => {
+      component.countryModel = 'AUS';
+      const result = component.validateContactNumber();
+      expect(result).toBeTruthy();
+    });
+
+    it('should validate US format of contact number', () => {
+      component.countryModel = 'US';
+      const result = component.validateContactNumber();
+      expect(result).toBeTruthy();
+    });
+
+    it('should invalidate contact number (it not belong to US/AUS)', () => {
+      component.countryModel = 'ANY_WRONG_NAME';
+      const result = component.validateContactNumber();
+      expect(result).toBeFalsy();
+    });
+
+    it('should return false when contact number is not 12 digits number', () => {
+      component.profile.contactNumber = '0123456789';
+      const result = component.validateContactNumber();
+      expect(result).toBeFalsy();
+    });
+
+    it('should return false and reset contactNumber to NULL when contact number is 3 digits number for AUS', () => {
+      component.countryModel = 'AUS';
+      component.profile.contactNumber = '012';
+      const result = component.validateContactNumber();
+      expect(result).toBeFalsy();
+      expect(component.invalidNumber).toBeTruthy();
+      expect(component.profile.contactNumber).toBeNull();
+    });
+
+    it('should return false and reset contactNumber to NULL when contact number is 2 digits number for US', () => {
+      component.countryModel = 'US';
+      component.profile.contactNumber = '01';
+      const result = component.validateContactNumber();
+      expect(result).toBeFalsy();
+      expect(component.invalidNumber).toBeTruthy();
+      expect(component.profile.contactNumber).toBeNull();
+    });
+  });
+
+  describe('updateCountry()', () => {
+    it('should be able to update locale', () => {
+      const testNumber = '0000000000';
+
+      // @TODO: verify with team why is this only update number rather than locale
+      component.profile.contactNumber = '0123456789';
+      component.updateCountry(testNumber);
+      expect(component.profile.contactNumber).toEqual(testNumber);
+    });
+  });
 });
