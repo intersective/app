35324502d40d4a942a9c5cbd06a53bf59152c37b
Sau Shen Chaw <cshen_581@hotmail.com>
2020-02-24 21:38:07 +0800

    [AV2-614] more activity service test

--- src/testing/fixtures/overview.ts
+++ src/testing/fixtures/overview.ts
@@ -1,34 +1,38 @@
 import { Overview, OverviewMilestone, OverviewActivity, OverviewTask } from '../../app/activity/activity.service';
 
+export const Task1 = {
+  is_locked: false,
+  type: 'assessment',
+  id: 1,
+  name: 'task 1',
+  context_id: 1,
+  status: 'done',
+  is_team: false,
+  progress: 1,
+  deadline: null,
+  Submitter: {
+    id: 1,
+    name: 'Submitter 1',
+    email: 'submitter1@test.com',
+    image: null,
+  },
+};
+
+export const Activity1 = {
+  id: 1,
+  name: 'Activity 1',
+  is_locked: false,
+  Tasks: [
+    Task1
+  ],
+};
+
 export const Milestones1: OverviewMilestone = {
   id: 1,
   name: 'Milestone 1',
   is_locked: false,
   Activities: [
-    {
-      id: 1,
-      name: 'Activity 1',
-      is_locked: false,
-      Tasks: [
-        {
-          is_locked: false,
-          type: 'assessment',
-          id: 1,
-          name: 'task 1',
-          context_id: 1,
-          status: 'done',
-          is_team: false,
-          progress: 1,
-          deadline: null,
-          Submitter: {
-            id: 1,
-            name: 'Submitter 1',
-            email: 'submitter1@test.com',
-            image: null,
-          },
-        }
-      ],
-    }
+    Activity1
   ],
 };
 
