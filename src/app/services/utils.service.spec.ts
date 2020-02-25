d4168d4b4f9596766bbe87abe2610a70098b3099
Sau Shen Chaw <cshen_581@hotmail.com>
2020-02-25 13:17:50 +0800

    [AV2-414] start adding unit test for utilservice

--- src/app/services/utils.service.spec.ts
+++ src/app/services/utils.service.spec.ts
@@ -1,12 +1,22 @@
+import { TestBed,  } from '@angular/core/testing';
 import { UtilsService } from './utils.service';
 
 describe('UtilsService', () => {
-  // let service: UtilsService;
-  // const modalController = null;
-  // beforeEach(() => {
-  //   service = new UtilsService(modalController);
-  // });
+  let service: UtilsService;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        UtilsService,
+      ]
+    });
+
+    service = TestBed.get(UtilsService);
+  });
+
+  it('should created', () => {
+    expect(service).toBeTruthy();
+  });
   // it('has lodash instantiated', () => {
   //   // expect(service).toBeDefined();
   // });
