66e5f3fe2a8e3e7dbb0eecbff272be14a8f865ce
chaw <cshen_581@hotmail.com>
2020-03-09 21:29:30 +0800

    ts config for phaser

--- src/app/test/phaser/phaser.component.ts
+++ src/app/test/phaser/phaser.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Game } from 'phaser';
 
 @Component({
   selector: 'app-phaser',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class PhaserComponent {
-
+  constructor() {
+    console.log(Game);
+  }
 }
