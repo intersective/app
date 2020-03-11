ee18e214798fb9ae5b75aa39dbb9141b49a70961
chaw <cshen_581@hotmail.com>
2020-03-11 08:06:46 +0800

    phaser

--- src/app/test/phaser/phaser.component.ts
+++ src/app/test/phaser/phaser.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Phaser from 'phaser';
 // import { Phaser } from '@ion-phaser/core';
 
 @Component({
@@ -11,14 +12,14 @@ export class PhaserComponent {
   public initialize: boolean;
 
   constructor() {
-    // console.log(Game);
-    this.game = {
+    console.log(Phaser);
+    /*this.game = {
       width: "100%",
       height: "100%",
       type: Phaser.AUTO,
       scene: {}
     }
-    this.initialize = true;
+    this.initialize = true;*/
   }
 
 
