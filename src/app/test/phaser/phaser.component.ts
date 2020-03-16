6fee942c468d5e34fbd2a5e8384124dba8eb3e2d
chaw <cshen_581@hotmail.com>
2020-03-16 23:23:16 +0800

    until now, animation still not working, let's try different version of phaser

--- src/app/test/phaser/phaser.component.ts
+++ src/app/test/phaser/phaser.component.ts
@@ -1,36 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+// import { Phaser } from 'Phaser';
 declare var Phaser;
+// import { load } from 'Phaser';
 
 @Component({
   selector: 'app-phaser',
   templateUrl: './phaser.component.html',
 })
 
-export class PhaserComponent implements OnInit {
-  game;
-  back;
-  mummy;
-  anim;
-  loopText;
+export class PhaserComponent {
+  game;//: Phaser.Game;
 
   public initialize: boolean;
 
   constructor() {
+    // super('test');
     console.log(Phaser);
     this.game = new Phaser.Game({
+      parent: '#phaser-game',
+      // domContainer: 'phaser-game',
+      // gameTitle: 'Practera Game',
       type: Phaser.AUTO,
       width: 800,
       height: 600,
-      physics: {
-          default: 'arcade',
-          arcade: {
-              gravity: { y: 200 }
-          }
-      },
+      // physics: {
+      //     default: 'arcade',
+      //     arcade: {
+      //         gravity: { y: 200 }
+      //     }
+      // },
       scene: {
-          preload: this.preload,
-          create: this.create
-      }
+        preload: this.preload,
+        create: this.create
+      },
     });
     /*this.game = {
       width: "100%",
@@ -42,23 +44,60 @@ export class PhaserComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log(this);
+    console.log(this.game);
     // this.game.add.image(0, 0, 'practera');
   }
 
   preload() {
-    this.game.anims.load('practera', 'assets/icon/favicon.ico');
-    // this.game.load.image('gem', 'assets/logo.svg');
+    // console.log(this.load.image('gem', 'assets/logo.svg'));
+    // this.game.load.setBaseURL('http://localhost:4200');
+    // this.game.load.image('gem', 'assets/geometric-light.png');
+
+    // this.load.setBaseURL('http://labs.phaser.io');
+
+    // this.load.image('sky', 'assets/skies/space3.png');
+    // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    // this.load.image('red', 'assets/particles/red.png');
   }
+
   /*preload() {
     this.game.load.image('lazur', 'assets/pics/thorn_lazur.png');
     this.game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
   }*/
   create() {
-    this.game.anims.create({ key: 'diamond', frames: this.game.anims.generateFrameNames('gems', { prefix: 'diamond_', end: 15, zeroPad: 4 }), repeat: 4 });
+    /*this.anims.create({ key: 'diamond',
+      frames: this.game.anims.generateFrameNames('gems',
+      { prefix: 'diamond_',
+        end: 15,
+        zeroPad: 4
+      }),
+      repeat: 4
+    });*/
     // var text = this.game.add.text(350, 250, '', { font: '16px Courier', fill: '#00ff00'
-    var gem = this.game.add.sprite(400, 300, 'gems').setScale(4);
-     gem.play('diamond');
+    // var gem = this.add.sprite(400, 300, 'gems').setScale(4);
+     // gem.play('diamond');
+     // this.add.image(0, 0, 'gem');
+
+     // this.add.image(400, 300, 'sky');
+
+     //  let particles = this.add.particles('red');
+
+     //  let emitter = particles.createEmitter({
+     //      speed: 100,
+     //      scale: { start: 1, end: 0 },
+     //      blendMode: 'ADD'
+     //  });
+
+     //  let logo = this.physics.add.image(400, 100, 'logo');
+
+     //  logo.setVelocity(100, 200);
+     //  logo.setBounce(1, 1);
+     //  logo.setCollideWorldBounds(true);
+
+     //  emitter.startFollow(logo);
   }
+
   /*create() {
     this.back = this.game.add.image(0, -400, 'lazur');
     this.back.scale.set(2);
@@ -77,13 +116,13 @@ export class PhaserComponent implements OnInit {
   }*/
 
 
-  animationStarted(sprite, animation) {
-    this.game.add.text(32, 32, 'Animation started', { fill: 'white' });
+  /*animationStarted(sprite, animation) {
+    this.add.text(32, 32, 'Animation started', { fill: 'white' });
   }
 
   animationLooped(sprite, animation) {
     if (animation.loopCount === 1) {
-      this.loopText = this.game.add.text(32, 64, 'Animation looped', { fill: 'white' });
+      this.loopText = this.add.text(32, 64, 'Animation looped', { fill: 'white' });
     }
     else
     {
@@ -94,12 +133,12 @@ export class PhaserComponent implements OnInit {
   }
 
   animationStopped(sprite, animation) {
-    this.game.add.text(32, 64+32, 'Animation stopped', { fill: 'white' });
+    this.add.text(32, 64+32, 'Animation stopped', { fill: 'white' });
   }
 
   update() {
     if (this.anim.isPlaying) {
       this.back.x -= 1;
     }
-  }
+  }*/
 }
