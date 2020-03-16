b045630ed4a5a05674480fe45e6fcaf5b338b48b
Ronak <ronakamani@gmail.com>
2020-03-16 14:04:58 +1100

    clean the code

--- src/app/questions/file/file-display/file-display.component.ts
+++ src/app/questions/file/file-display/file-display.component.ts
@@ -2,33 +2,6 @@ import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/cor
 import { FilestackService } from '@shared/filestack/filestack.service';
 import { UtilsService } from '@services/utils.service';
 import { FixOrientationService } from '@services/fix-orientation.service';
-// import * as exif from 'exif-js';
-
-// const getImageClassToFixOrientation = (orientation) => {
-//   switch (orientation) {
-//     case 2:
-//       return ('flip');
-//     case 3:
-//       return ('rotate-180');
-//     case 4:
-//       return ('flip-and-rotate-180');
-//     case 5:
-//       return ('flip-and-rotate-270');
-//     case 6:
-//       return ('rotate-90');
-//     case 7:
-//       return ('flip-and-rotate-90');
-//     case 8:
-//       return ('rotate-270');
-//   }
-// }
-
-// const swapWidthAndHeight = img => {
-//   const currentHeight = img.height;
-//   const currentWidth = img.width;
-//   img.height = currentWidth;
-//   img.width = currentHeight;
-// }
 
 @Component({
   selector: 'app-file-display',
@@ -93,17 +66,7 @@ export class FileDisplayComponent implements OnInit, OnChanges {
     }
   }
 
-  // imageLoaded(e) {
-  //   exif.getData(e.target, function () {
-  //     var allMetaData = exif.getAllTags(this);
-  //     const orientationClassFix = getImageClassToFixOrientation(allMetaData.Orientation);
-  //     this.classList.add(orientationClassFix);
-  //     if(allMetaData.Orientation >= 5) {
-  //       swapWidthAndHeight(this);
-  //     }
 
-  //   });
-  // }
   loaded(e) {
     this.fixOrientation.imageLoaded(e);
 
