3418ac9e78b978ad8996a2e39741dde8b754a05d
chaw <cshen_581@hotmail.com>
2020-04-13 17:44:10 +0800

    [AV2-625] FCM not working
    cordova-plugin-fcm doesnt supported in capacitor

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -5,6 +5,8 @@ import { UtilsService } from '@services/utils.service';
 import { combineLatest, Observable, of } from 'rxjs';
 import { FastFeedbackService } from '../fast-feedback/fast-feedback.service';
 
+import { FCM } from '@ionic-native/fcm/ngx';
+
 import {
   Plugins,
   PushNotification,
@@ -28,6 +30,7 @@ export class OverviewComponent implements OnInit {
     private utils: UtilsService,
     private route: ActivatedRoute,
     private fastFeedbackService: FastFeedbackService,
+    private fcm: FCM
   ) {
     this.isMobile = this.utils.isMobile();
   }
@@ -37,6 +40,27 @@ export class OverviewComponent implements OnInit {
       this.programName = this.storage.getUser().programName;
       this.fastFeedbackService.pullFastFeedback().subscribe();
     });
+
+    this.fcm.subscribeToTopic('marketing');
+
+    this.fcm.getToken().then(token => {
+      console.log('token::', token);
+    });
+
+    this.fcm.onNotification().subscribe(data => {
+      if(data.wasTapped){
+        console.log("Received in background");
+      } else {
+        console.log("Received in foreground");
+      };
+    });
+
+    this.fcm.onTokenRefresh().subscribe(token => {
+      // backend.registerToken(token);
+      console.log('refresh token::', token);
+    });
+
+    this.fcm.unsubscribeFromTopic('marketing');
 
     console.log('Initializing HomePage');
 
