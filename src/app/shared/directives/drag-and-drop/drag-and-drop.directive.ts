9d2f8c54ae4caedc6f38fce876244eb1b3e7c2a8
sasangachathumal <chathumal.sasanga02@gmail.com>
2020-05-01 12:54:03 +0530

    [AV2-668] update styles for drop locations. updated drag and drop location. update directive to add validations to files and emit files to component added style to show when file drag over the location.

--- src/app/shared/directives/drag-and-drop/drag-and-drop.directive.ts
+++ src/app/shared/directives/drag-and-drop/drag-and-drop.directive.ts
@@ -1,20 +1,24 @@
-import { Directive, HostListener, Output, EventEmitter, Input } from '@angular/core';
+import { Directive, HostListener, HostBinding, Output, EventEmitter, Input } from '@angular/core';
 
 @Directive({
   selector: '[appDragAndDrop]'
 })
 export class DragAndDropDirective {
   @Output() fileDropped = new EventEmitter<any>();
-  @Input() acceptFileType: string;
+  @Input() acceptFileType?: string;
+
+  @HostBinding('class.fileover') fileOver: boolean;
 
   @HostListener('dragover', ['$event']) ondragover(evt) {
     evt.preventDefault();
     evt.stopPropagation();
+    this.fileOver = true;
   }
 
   @HostListener('dragleave', ['$event']) ondragleave(evt) {
     evt.preventDefault();
     evt.stopPropagation();
+    this.fileOver = false;
   }
 
   @HostListener('drop', ['$event']) ondrop(evt) {
@@ -22,9 +26,40 @@ export class DragAndDropDirective {
     evt.stopPropagation();
     console.log('drop');
     console.log('file', evt.dataTransfer.files);
+    this.fileOver = false;
     const files = evt.dataTransfer.files;
-    if (files.length > 0) {
-      this.fileDropped.emit(files);
+    // error - no file droped
+    if (files.length < 0) {
+      console.log('error - no file droped');
+      this.fileDropped.emit({
+        success: false,
+        message: 'no file droped'
+      });
+      return;
+    }
+    // error - more than one file droped
+    if (files.length > 1) {
+      console.log('error - more than one file droped');
+      this.fileDropped.emit({
+        success: false,
+        message: 'more than one file droped'
+      });
+      return;
+    }
+    const file = files[0];
+    if (this.acceptFileType && this.acceptFileType !== 'any' && file.type.includes(this.acceptFileType)) {
+      this.fileDropped.emit({
+        success: true,
+        data: file
+      });
+    } else {
+      // error - not maching file type
+      console.log('error - not maching file type');
+      this.fileDropped.emit({
+        success: false,
+        message: 'not maching file type'
+      });
+      return;
     }
   }
 
