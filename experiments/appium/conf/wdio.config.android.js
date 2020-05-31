7ae19a28eb5ebc383532d85584640176065a693d
chaw <cshen_581@hotmail.com>
2020-06-01 01:26:24 +0800

    [AV2-680] webdriverio + cucumber unsuccessful

--- experiments/appium/conf/wdio.config.android.js
+++ experiments/appium/conf/wdio.config.android.js
@@ -3,28 +3,32 @@ const { config } = require('./wdio.shared.conf');
 
 config.capabilities = [{
   platformName: "Android",
-  platformVersion: "8",
   maxInstances: 1,
-  app: join(
-    process.cwd(), "./experiments/appium/appV2-debug.apk"
+  'appium:app': join(
+    process.cwd(), "./experiments/appium/Android-NativeDemoApp-0.2.1.apk"
+    // process.cwd(), "./experiments/appium/appV2-debug.apk"
   ),
+  'appium:deviceName': 'emulator-5554',
   // app: "/Users/chaw/Workspaces/www/intersective/practera-app-v2/experiments/appium/ApiDemos-debug.apk",
-  'appium:appPackage': "com.practera.appv2",
-  'appium:appActivity': ".MainActivity",
+  // 'appium:appPackage': "com.practera.appv2",
+  // 'appium:appActivity': ".MainActivity",
   // 'appium:chromedriverExecutableDir': '<PATH TO CHROME DRIVER>',
   'appium:platformVersion': '8.0',
   'appium:orientation': 'PORTRAIT',
   // `automationName` will be mandatory, see
   // https://github.com/appium/appium/releases/tag/v1.13.0
   'appium:automationName': 'UiAutomator2',
-  // The path to the app
-  'appium:app': join(
-    process.cwd(), './apps/Android-NativeDemoApp-0.2.1.apk',
-  )
+  'appium:noReset': true,
+  'appium:fullReset': false,
+  'appium:dontStopAppOnReset': true,
+  'appium:newCommandTimeout': 60,
 }];
 
-config.specs = [
-  './experiments/tests/steps/*.spec.ts',
-];
+/*config.specs = [
+  './experiments/tests/steps/*.steps.js',
+  // './experiments/tests/steps/*.spec.js',
+];*/
+
+config.cucumberOpts.require = ['./experiments/tests/steps/**/app*.steps.js'];
 
 exports.config = config;
