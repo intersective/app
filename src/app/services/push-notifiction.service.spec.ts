428fb8530eff9b5b82a72a9176f4fea7220cf828
chaw <cshen_581@hotmail.com>
2020-08-14 15:29:05 +0800

    [AV2-730] inited test

---
+++ src/app/services/push-notifiction.service.spec.ts
@@ -0,0 +1,7 @@
+import { TestBed } from '@angular/core/testing';
+import { PusherService } from './push-notifiction.service';
+
+describe('StorageService', () => {
+  let service: PusherService;
+
+});
