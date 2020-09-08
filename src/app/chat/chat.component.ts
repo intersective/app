58df1e0296b6aed5494dd966706c7ab6bbf828b8
chaw <cshen_581@hotmail.com>
2020-09-08 12:23:08 +0800

    [AV2-744] chat and (any) assessment submission

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
+import { PushNotificationService } from '@services/push-notification.service';
+import { NotificationService } from '@shared/notification/notification.service';
 
 @Component({
   template: '<ion-router-outlet></ion-router-outlet>',
 })
-export class ChatComponent {}
+export class ChatComponent {
+  constructor(
+    private pushNotificationService: PushNotificationService,
+    private notificationService: NotificationService
+  ) {}
+
+  async ionViewDidEnter() {
+    const hasPNPermission = await this.pushNotificationService.checkPermission('isFirstVisit', '/app/chat');
+    if (!hasPNPermission) {
+      this.notificationService.popUp('shortMessage', {message: 'To do this assessment, you have to be in a team.'});
+    }
+  }
+}
