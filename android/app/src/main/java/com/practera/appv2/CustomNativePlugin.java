d1739f3736afe8a71f6f790257efbc83e3d51f44
ronak <ronak@practera.com>
2020-10-21 14:23:57 +1100

    update the goToSetting function

--- android/app/src/main/java/com/practera/appv2/CustomNativePlugin.java
+++ android/app/src/main/java/com/practera/appv2/CustomNativePlugin.java
@@ -13,17 +13,32 @@ import com.getcapacitor.PluginMethod;
 
 @NativePlugin()
 public class CustomNativePlugin extends Plugin {
+  String packageName;
     @PluginMethod()
     public void goToAppSetting(PluginCall call) {
-        String value = call.getString("value");
+      String value = call.getString("value");
 
-        JSObject ret = new JSObject();
-        ret.put("value", value + " redirecting...");
+      JSObject ret = new JSObject();
+      ret.put("value", value + " redirecting...");
 
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        startActivityForResult(call, intent, 1);
-        call.success();
+      Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+       Uri uri = Uri.fromParts("package", getPackageName(), null);
+       intent.setData(uri);
+       startActivityForResult(call, intent, 1);
+       call.success();
     }
 
+  @PluginMethod()
+  private void setPackageName(PluginCall call) {
+    this.packageName = call.getString("packageName");
+    call.success();
+  }
+  private String getPackageName() {
+    if (this.packageName != null && !this.packageName.isEmpty()) {
+      return this.packageName;
+    }
+
+    return "com.practera.appv2";
+  }
 
 }
