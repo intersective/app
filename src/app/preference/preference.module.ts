3e5a73fd5d4eab3329d41f39764132daa39ca7b9
chaw <cshen_581@hotmail.com>
2020-11-02 23:58:12 +0800

    [AV2-810] fixed repeated calles to same api
    `strict-origin-when-cross-origin` is being used atm

--- src/app/preference/preference.module.ts
+++ src/app/preference/preference.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreferenceComponent } from './preference.component';
 import { PreferenceRoutingModule } from './preference-routing.module';
+import { OptionsComponent } from './options/options.component';
 
 @NgModule({
-  declarations: [PreferenceComponent],
+  declarations: [PreferenceComponent, OptionsComponent],
   imports: [
     CommonModule,
     PreferenceRoutingModule
--- src/app/services/preference.service.ts
+++ src/app/services/preference.service.ts
@@ -1,10 +1,94 @@
 import { Injectable } from '@angular/core';
 import { RequestService } from '@shared/request/request.service';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs';
 
 const APIs = {
   preference: 'https://4d052q3ph6.execute-api.ap-southeast-2.amazonaws.com/notify',
-}
+};
+
+const DUMMY_DATA = {
+  "categories": [
+    {
+      "name": "Chat Notifications",
+      "order": 1,
+      "preferences": [
+        {
+          "key": "best key",
+          "name": "Chat messages",
+          "description": "When I receive chat messages",
+          "remarks": "Chat messages can be muted from within individual chat channels",
+          "options": [
+            {
+              "name": "Email",
+              "medium": "email",
+              "value": true,
+              "locked": false,
+              "locked_name": ""
+            },
+            {
+              "name": "SMS",
+              "medium": "sms",
+              "value": false,
+              "locked": false,
+              "locked_name": ""
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Team Changes Notifications",
+      "order": 1,
+      "preferences": [
+        {
+          "key": "new_member_added",
+          "name": "When a member has been added to the team",
+          "description": "How do you want to be told when a member was added to the team",
+          "remarks": "",
+          "options": [
+            {
+              "name": "Email",
+              "medium": "email",
+              "value": true,
+              "locked": false,
+              "locked_name": ""
+            },
+            {
+              "name": "SMS",
+              "medium": "sms",
+              "value": false,
+              "locked": false,
+              "locked_name": ""
+            }
+          ]
+        },
+        {
+          "key": "member_removed",
+          "name": "When a member has been removed",
+          "description": "How do you want to be told when a member was removed from the team",
+          "remarks": "",
+          "options": [
+            {
+              "name": "Email",
+              "medium": "email",
+              "value": true,
+              "locked": false,
+              "locked_name": ""
+            },
+            {
+              "name": "SMS",
+              "medium": "sms",
+              "value": false,
+              "locked": false,
+              "locked_name": ""
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +98,7 @@ export class PreferenceService {
   constructor(private request: RequestService) { }
 
   getPreference(): Observable<any> {
-    return this.request.post(APIs.preference, {});
+    return of(DUMMY_DATA);
+    // return this.request.post(APIs.preference, {});
   }
 }
