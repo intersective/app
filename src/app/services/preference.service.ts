e3307fa8d692059434cf9ea8a38191afd9821473
chaw <cshen_581@hotmail.com>
2020-11-03 11:59:23 +0800

    [AV2-810] no dummy data need

--- src/app/services/preference.service.ts
+++ src/app/services/preference.service.ts
@@ -7,89 +7,6 @@ const APIs = {
   preference: 'https://4d052q3ph6.execute-api.ap-southeast-2.amazonaws.com/notify',
 };
 
-const DUMMY_DATA = {
-  "categories": [
-    {
-      "name": "Chat Notifications",
-      "order": 1,
-      "preferences": [
-        {
-          "key": "best key",
-          "name": "Chat messages",
-          "description": "When I receive chat messages",
-          "remarks": "Chat messages can be muted from within individual chat channels",
-          "options": [
-            {
-              "name": "Email",
-              "medium": "email",
-              "value": true,
-              "locked": false,
-              "locked_name": ""
-            },
-            {
-              "name": "SMS",
-              "medium": "sms",
-              "value": false,
-              "locked": false,
-              "locked_name": ""
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "name": "Team Changes Notifications",
-      "order": 1,
-      "preferences": [
-        {
-          "key": "new_member_added",
-          "name": "When a member has been added to the team",
-          "description": "How do you want to be told when a member was added to the team",
-          "remarks": "",
-          "options": [
-            {
-              "name": "Email",
-              "medium": "email",
-              "value": true,
-              "locked": false,
-              "locked_name": ""
-            },
-            {
-              "name": "SMS",
-              "medium": "sms",
-              "value": false,
-              "locked": false,
-              "locked_name": ""
-            }
-          ]
-        },
-        {
-          "key": "member_removed",
-          "name": "When a member has been removed",
-          "description": "How do you want to be told when a member was removed from the team",
-          "remarks": "",
-          "options": [
-            {
-              "name": "Email",
-              "medium": "email",
-              "value": true,
-              "locked": false,
-              "locked_name": ""
-            },
-            {
-              "name": "SMS",
-              "medium": "sms",
-              "value": false,
-              "locked": false,
-              "locked_name": ""
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -98,7 +15,6 @@ export class PreferenceService {
   constructor(private request: RequestService) { }
 
   getPreference(): Observable<any> {
-    // return of(DUMMY_DATA);
     return this.request.post(APIs.preference, {});
   }
 }
