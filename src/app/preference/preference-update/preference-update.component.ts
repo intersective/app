4a7a695141aa6995405628c453899025fcb2baa7
chaw <cshen_581@hotmail.com>
2020-11-03 17:28:41 +0800

    [AV2-810] boardcasting to subscriptions

--- src/app/preference/preference-update/preference-update.component.ts
+++ src/app/preference/preference-update/preference-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PreferenceService, Category } from '@services/preference.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { UtilsService } from '@services/utils.service';
 
 @Component({
   selector: 'app-preference-update',
@@ -6,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preference-update.component.scss']
 })
 export class PreferenceUpdateComponent implements OnInit {
+  noHistoryStack = true;
+  preferences: {
+    categories: any;
+  };
+  preferenceSubject$: Subscription;
+  currentPreference: object;
 
-  constructor() { }
+  constructor(
+    private preferenceService: PreferenceService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private utilsService: UtilsService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: { key: string }) => {
+      const { key } = params;
+
+      this.preferenceSubject$ = this.preferenceService.preference$.subscribe(res => {
+        if (this.utilsService.isEmpty(res) && this.noHistoryStack === true) {
+          this.noHistoryStack = false;
+          this.preferenceService.getPreference();
+        } else {
+          this.preferences = res;
+          this.currentPreference = this.filterPreferences(this.preferences, key);
+        }
+      });
+    });
   }
 
+  ngOnDestroy() {
+    if (this.preferenceSubject$ && this.preferenceSubject$ instanceof Subscription) {
+      this.preferenceSubject$.unsubscribe();
+    }
+  }
+
+  /**
+   * @name filterPreferences
+   * @param {Category[] }} preferences   entire preferences object
+   * @param {string}        key  url parameter
+   */
+  filterPreferences(preferences: { categories?: Category[] }, key: string) {
+    let result;
+    if (preferences && preferences.categories) {
+      preferences.categories.find(cat => {
+        result = cat.preferences.find(pref => {
+          return pref.key === key;
+        });
+      });
+    }
+
+    return result;
+  }
 }
