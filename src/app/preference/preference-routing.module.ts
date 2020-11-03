a12ab1e68a768284f3eada2c9869262a21cace00
chaw <cshen_581@hotmail.com>
2020-11-03 15:37:19 +0800

    [AV2-810] fixed settings test + audit warning + pref subpage

--- src/app/preference/preference-routing.module.ts
+++ src/app/preference/preference-routing.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PreferenceComponent } from './preference.component';
 import { PreferenceUpdateComponent } from './preference-update/preference-update.component';
+import { PreferenceRoutingComponent } from './preference-routing.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PreferenceComponent,
-    children: [
+    /*children: [
+      {
+        path: '',
+        component: PreferenceComponent,
+      },
       {
         path: 'update',
         component: PreferenceUpdateComponent,
       }
-    ]
+    ]*/
   },
+  {
+    path: 'update',
+    component: PreferenceUpdateComponent,
+  }
 ];
 
 @NgModule({
--- src/app/services/preference.service.ts
+++ src/app/services/preference.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { RequestService } from '@shared/request/request.service';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs';
+import { of, BehaviorSubject } from 'rxjs';
 
 const APIs = {
   preference: 'https://4d052q3ph6.execute-api.ap-southeast-2.amazonaws.com/notify',
@@ -11,6 +11,11 @@ const APIs = {
   providedIn: 'root'
 })
 export class PreferenceService {
+  private _preferences$ = new BehaviorSubject<any>({
+    catergory: {}
+  });
+
+  preference$ = this._preferences$.asObservable();
 
   constructor(private request: RequestService) { }
 
