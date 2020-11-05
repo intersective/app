c7b6c463da79e4101f117af41203bd8ec5ab301f
ronak <ronak@practera.com>
2020-11-05 13:00:07 +1100

    update test.ts

--- src/test.ts
+++ src/test.ts
@@ -15,6 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /push\-notification\.service\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
