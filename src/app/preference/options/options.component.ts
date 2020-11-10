6e55cb5ab783f9e366b7977b08af02121d554171
ronak <ronak@practera.com>
2020-11-10 16:01:25 +1100

    Notification page

--- src/app/preference/options/options.component.ts
+++ src/app/preference/options/options.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-options',
-  template: `<p>{{ result }}</p>`,
+  template: `<p class="color-medium">{{ result }}</p>`,
   styleUrls: ['./options.component.scss']
 })
 export class OptionsComponent implements OnInit {
