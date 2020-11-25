4248223e6c51fe1d2365a766e06de5e407d52647
chaw <cshen_581@hotmail.com>
2020-11-25 20:04:45 +0800

    [AV2-793] 50%++

--- src/app/services/push-notification.service.spec.ts
+++ src/app/services/push-notification.service.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed, flush, fakeAsync } from '@angular/core/testing';
-import { PushNotificationService } from './push-notification.service';
+import { PushNotificationService, PermissionTypes } from './push-notification.service';
 import { RequestService } from '@shared/request/request.service';
+import { Router, RouterStateSnapshot } from '@angular/router';
 import { BrowserStorageService } from '@services/storage.service';
+import { environment } from '@environments/environment';
 
 import {
   Plugins,
@@ -104,6 +106,93 @@ describe('PushNotificationService', () => {
 
       service.hasPermission().then(hasPermission => {
         expect(hasPermission).toBeFalsy();
+      });
+      flush();
+    }));
+  });
+
+  describe('requestPermission()', () => {
+    it('should register to push notification plugin when permission granted', fakeAsync(() => {
+
+      PushNotifications.requestPermission = jasmine.createSpy('requestPermission').and.returnValue(new Promise(resolve => resolve({
+        granted: true
+      })));
+      PushNotifications.register = jasmine.createSpy();
+
+      service = new PushNotificationService(storageSpy);
+      service['pushNotificationPlugin'] = PushNotifications;
+      service.requestPermission().then(() => {
+        expect(PushNotifications.requestPermission).toHaveBeenCalled();
+        expect(storageSpy.set).toHaveBeenCalled();
+        expect(PushNotifications.register).toHaveBeenCalled();
+      });
+      flush();
+    }));
+  });
+
+  describe('associateDeviceToUser()', () => {
+    it('should associate user to device', fakeAsync(() => {
+      const APPKEY = 'testAppkey';
+      const ID = 'testID';
+      const TOKEN = 'testToken';
+
+      PusherBeams.setUserID = jasmine.createSpy('setUserID').and.returnValue(new Promise(resolve => resolve(true)));
+
+      environment.appkey = APPKEY;
+      const service = new PushNotificationService(storageSpy);
+      service['pusherBeams'] = PusherBeams;
+
+      service.associateDeviceToUser(ID, TOKEN).then(res => {
+        expect(PusherBeams.setUserID).toHaveBeenCalledWith(jasmine.objectContaining({
+            userID: ID,
+            headers: {
+              appkey: APPKEY,
+              apikey: TOKEN,
+            },
+            beamsAuthURL: 'https://wchpiwp904.execute-api.us-east-2.amazonaws.com/beams'
+        }));
+        expect(res).toBeTruthy();
+      });
+    }));
+  });
+
+  describe('promptForPermission()', () => {
+    it('should return false if plugin not available', fakeAsync(() => {
+      Capacitor.isPluginAvailable = () => false;
+      const snapshot: Partial<RouterStateSnapshot> = {
+        url: 'doesNotMatter'
+      };
+      service.promptForPermission(PermissionTypes.firstVisit, snapshot).then(res => {
+        expect(res).toBeFalsy();
+      });
+      flush();
+    }));
+
+    it('should prompt user for push notification permission', fakeAsync(() => {
+      Capacitor.isPluginAvailable = () => true;
+      const visited = [
+        'url1',
+        'url2',
+      ];
+
+      storageSpy.get = jasmine.createSpy('get').and.returnValue(visited);
+
+      Permissions.query = () => new Promise(resolve => resolve({
+        state: 'granted'
+      }));
+
+      PushNotifications.requestPermission = () => new Promise(resolve => resolve({
+        granted: true
+      }));
+
+      const snapshot: Partial<RouterStateSnapshot> = {
+        url: 'url3'
+      };
+
+      const service = new PushNotificationService(storageSpy);
+      service['pushNotificationPlugin'] = PushNotifications;
+      service.promptForPermission(PermissionTypes.firstVisit, snapshot).then(() => {
+        expect(storageSpy.set).toHaveBeenCalledWith('visited', [...visited, 'url3']);
       });
       flush();
     }));
