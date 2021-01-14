d7fdd8b6d440e869ae72dad11eed19b2137d9c26
ronak <ronak@practera.com>
2021-01-14 11:53:32 +1100

    update yes/sure button

--- src/app/shared/components/pn-permission-modal/pn-permission-modal.component.ts
+++ src/app/shared/components/pn-permission-modal/pn-permission-modal.component.ts
@@ -4,6 +4,7 @@ import { UtilsService } from '@services/utils.service';
 import { ModalController } from '@ionic/angular';
 import { PushNotificationService } from '@services/push-notification.service';
 
+
 @Component({
   selector: 'pn-permission-modal',
   templateUrl: './pn-permission-modal.component.html',
@@ -17,17 +18,22 @@ export class PNPermissionModalComponent {
     public modalController: ModalController,
     public utils: UtilsService,
     private pushNotificationService: PushNotificationService,
+    private notificationService: NotificationService,
   ) {}
 
-  dismiss() {
+  dismissPermissionModal() {
     
     this.modalController.dismiss();
   }
 
   // go to Native App settings, so user can manually allow Push Notification there
   async goToSystemSetting() {
-    const goToSetting = await this.utils.goToSystemSetting();
-    this.dismiss();
+     const goToSetting = await this.utils.goToSystemSetting();
+    const stateChange =  this.pushNotificationService.listenToStateChangeToActive();
+    const permissionGranted = this.pushNotificationService.hasPermission();
+    if (stateChange && permissionGranted ) {
+      this.notificationService.dismiss();
+    }
     return goToSetting;
   }
 }
