bf8c7bafaa8dbb5561bb07b9b0f69a7fd1fd37a0
Sunil Badyankal <sunilsbcloud@gmail.com>
2021-01-29 17:40:01 +1100

    testing-sandbox-3

--- env.sh
+++ env.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+
 export CUSTOMPLAIN_SKIPGLOBALLOGINFLAG=${CUSTOMPLAIN_SKIPGLOBALLOGINFLAG:-false}
 export CUSTOMPLAIN_PRDMODEFLAG=${CUSTOMPLAIN_PRDMODEFLAG:-true}
+sed -i "s/<CUSTOM_PIWIK_ENVIRONEMENT>/$CUSTOM_PIWIK_ENVIRONEMENT/" src/index.html
+sed -i "s/<SHA256_KEYSTORE_CERT_FINGERPRINTS>/$SHA256_KEYSTORE_CERT_FINGERPRINTS/" src/well-known/assetlinks.json
+
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOMPLAIN_'* ]]; then
         sed -i "s#'<$name>'#${!name}#g" src/environments/environment.custom.ts
