5e809d210a06b939495647dda703de16a53ab547
Sunil Badyankal <sunilsbcloud@gmail.com>
2021-02-04 12:12:58 +1100

    update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export CUSTOMPLAIN_SKIPGLOBALLOGINFLAG=${CUSTOMPLAIN_SKIPGLOBALLOGINFLAG:-false}
+export CUSTOMPLAIN_PRDMODEFLAG=${CUSTOMPLAIN_PRDMODEFLAG:-true}
+
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOMPLAIN_'* ]]; then
         sed -i "s#'<$name>'#${!name}#g" src/environments/environment.custom.ts
