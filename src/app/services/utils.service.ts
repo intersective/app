3316cffce1ecc57ad66cd98e22b3a3e85a321462
ronak <ronak@practera.com>
2021-04-08 20:42:05 +1000

    fix the lint error

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -384,7 +384,7 @@ export class UtilsService {
   }
 
   clickableKeys(event: KeyboardEvent): boolean {
-    if (['Enter','Space'].indexOf(event.code) !== -1) {
+    if (['Enter', 'Space'].indexOf(event.code) !== -1) {
       return true;
     }
   }
