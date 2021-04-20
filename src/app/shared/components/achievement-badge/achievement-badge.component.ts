16756f2606bedf8415aae97eb7690f34dc8c8dc3
ronak <ronak@practera.com>
2021-04-20 15:20:05 +1000

    remove extra code

--- src/app/shared/components/achievement-badge/achievement-badge.component.ts
+++ src/app/shared/components/achievement-badge/achievement-badge.component.ts
@@ -15,8 +15,7 @@ export class AchievementBadgeComponent {
 
   @Input() achievement: Achievement;
   @Input() showName: Boolean = false;
-  allBackgroundElements = document.querySelectorAll('.backgroundElement, [tabindex="0"]');
-
+  
   /**
     * @param {NotificationService} notificationService  the notification object
     * @param {UtilsService} utils  the utils object
@@ -33,9 +32,6 @@ export class AchievementBadgeComponent {
     */
   showAchievementDetails() {
     this.notificationService.achievementPopUp('', this.achievement);
-    this.allBackgroundElements.forEach(element => {
-      element.setAttribute('tabindex', '-1');
-    });
   }
 
 }
