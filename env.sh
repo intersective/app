1cc77a8d01f5f7c8ee3be72f824cf7865dff2bed
Sunil Badyankal <sunilsbcloud@gmail.com>
2021-04-28 15:15:07 +1000

    new way aws deploy test to beta

--- env.sh
+++ env.sh
@@ -6,13 +6,11 @@ export CUSTOMPLAIN_PRDMODEFLAG=${CUSTOMPLAIN_PRDMODEFLAG:-true}
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOMPLAIN_'* ]]; then
         sed -i "s#'<$name>'#${!name}#g" src/environments/environment.custom.ts
-        echo "replacing <$name> to ${!name}"
     fi
 done < <(env)
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOM_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
         sed -i "s#<$name>#${!name}#g" angular.json
-        echo "replacing <$name> to ${!name}"
     fi
 done < <(env)
