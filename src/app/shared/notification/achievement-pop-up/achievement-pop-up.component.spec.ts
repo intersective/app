80474730a346256da94ae2e615d24bf22836876e
chaw <cshen_581@hotmail.com>
2021-05-24 23:11:30 +0800

    [AV2-901] added 2 tests

--- src/app/shared/notification/achievement-pop-up/achievement-pop-up.component.spec.ts
+++ src/app/shared/notification/achievement-pop-up/achievement-pop-up.component.spec.ts
@@ -2,33 +2,45 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AchievementPopUpComponent } from './achievement-pop-up.component';
 import { Observable, of, pipe } from 'rxjs';
-import { ModalController } from '@ionic/angular';
-import { Apollo } from 'apollo-angular';
+import { ModalController, IonicModule } from '@ionic/angular';
+import { UtilsService } from '@services/utils.service';
+
+class AchievementModalPage {
+  fixture: ComponentFixture<AchievementPopUpComponent>;
+
+  constructor(fixture: ComponentFixture<AchievementPopUpComponent>) {
+    this.fixture = fixture;
+  }
+}
 
 describe('AchievementPopUpComponent', () => {
   let component: AchievementPopUpComponent;
   let fixture: ComponentFixture<AchievementPopUpComponent>;
+  let page: AchievementModalPage;
   const modalCtrlSpy = jasmine.createSpyObj('ModalController', ['dismiss', 'create']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ IonicModule ],
       declarations: [ AchievementPopUpComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       providers: [
-        Apollo,
         {
           provide: ModalController,
           useValue: modalCtrlSpy
-        }
+        },
+        {
+          provide: UtilsService,
+          useValue: jasmine.createSpyObj('UtilsService', [ 'isMobile' ])
+        },
       ],
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AchievementPopUpComponent);
     component = fixture.componentInstance;
-  });
+    page = new AchievementModalPage(fixture);
+  }));
 
   it('should create', () => {
     expect(component).toBeDefined();
@@ -43,6 +55,20 @@ describe('AchievementPopUpComponent', () => {
     fixture.detectChanges();
     component.confirmed('Enter');
     expect(modalCtrlSpy.dismiss.calls.count()).toBe(1);
+  });
+
+  xdescribe('ionViewDidEnter()', () => {
+    it('should prepare accessibility controls', () => {
+      component.ionViewDidEnter();
+
+    });
+  });
+
+  describe('confirm()', () => {
+    it('should dismiss', () => {
+      component.confirmed(null);
+      expect(modalCtrlSpy.dismiss).toHaveBeenCalled();
+    });
   });
 });
 
