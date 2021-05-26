403be003c212c55e2f8552df395abc70b572c391
Wes Sonnenreich <wes@intersective.com>
2021-05-26 15:08:27 -0400

    Add timezone information to events in app

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -281,6 +281,27 @@ export class UtilsService {
     }).format(dateToFormat.toDate());
   }
 
+  localTimezone() {
+    const today = new Date();
+    const short = today.toLocaleDateString(undefined);
+    const full = today.toLocaleDateString(undefined, { timeZoneName: 'long' });
+
+    // Trying to remove date from the string in a locale-agnostic way
+    const shortIndex = full.indexOf(short);
+    if (shortIndex >= 0) {
+      const trimmed = full.substring(0, shortIndex) + full.substring(shortIndex + short.length);
+      
+      // by this time `trimmed` should be the timezone's name with some punctuation -
+      // trim it from both sides
+      return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, '');
+
+    } else {
+      // in some magic case when short representation of date is not present in the long one, just return the long one as a fallback, since it should contain the timezone's name
+      return Intl.DateTimeFormat().resolvedOptions().timeZone;
+    }
+      
+  }
+
   /**
    * @description dates comparison (between today/provided date)
    * @param {Date    | string} timeString [description]
