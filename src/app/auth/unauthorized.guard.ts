3b0e4d5c786b4a08f3df02e369d6e9b96c8fc5c5
sasangachathumal <chathumal.sasanga02@gmail.com>
2021-08-24 11:43:14 +0530

    [AV2-946] move redirect logic to logout method before clear stroage.

--- src/app/auth/unauthorized.guard.ts
+++ src/app/auth/unauthorized.guard.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UtilsService } from '@services/utils.service';
-import { environment } from '@environments/environment';
-import { BrowserStorageService } from '@services/storage.service';
 
 @Injectable()
 export class UnauthorizedGuard implements CanActivate {
@@ -13,19 +11,12 @@ export class UnauthorizedGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private utils: UtilsService,
-    public storage: BrowserStorageService
+    private utils: UtilsService
   ) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const userIsAuthenticated = this.authService.isAuthenticated();
     if (userIsAuthenticated !== true) {
-      const fromGlobalLogin = this.storage.get('fromGlobalLogin');
-      // redirect user to global login if user came from it.
-      if (fromGlobalLogin) {
-        this.utils.openUrl(`${ environment.globalLoginUrl }?referrer=${ window.location.hostname }&stackUuid=${ environment.stackUuid }`);
-        return false;
-      }
       return true;
     }
 
