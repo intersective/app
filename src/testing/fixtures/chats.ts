0ab7cbe5a4c763dcd4293ea923d547e83673c7c7
chaw <3895281+trtshen@users.noreply.github.com>
2021-09-01 18:33:57 +0800

    [AV2-950]

---
+++ src/testing/fixtures/chats.ts
@@ -0,0 +1,60 @@
+const chats = [
+  {
+    uuid: '1',
+    name: 'Team 1',
+    avatar: 'https://cdn.filestackcontent.com/uYQuauwNRdD43PfCQ4iW',
+    pusherChannel: 'pusher-channel-name',
+    roles: ['participant'],
+    isAnnouncement: false,
+    isDirectMessage: false,
+    readonly: false,
+    unreadMessageCount: 2,
+    lastMessageCreated: '2020-01-25 06:18:45',
+    lastMessage: 'test 1',
+    canEdit: false
+  },
+  {
+    uuid: '2',
+    name: 'Team 2',
+    avatar: 'https://cdn.filestackcontent.com/uYQuauwNRdD43PfCQ4iW',
+    pusherChannel: 'pusher-channel-name',
+    roles: ['participant'],
+    isAnnouncement: false,
+    isDirectMessage: false,
+    readonly: false,
+    unreadMessageCount: 2,
+    lastMessageCreated: '2020-01-30 04:18:45',
+    lastMessage: 'test 2',
+    canEdit: false
+  },
+  {
+    uuid: '3',
+    name: 'Team 3',
+    avatar: 'https://cdn.filestackcontent.com/uYQuauwNRdD43PfCQ4iW',
+    pusherChannel: 'pusher-channel-name',
+    roles: ['participant'],
+    isAnnouncement: false,
+    isDirectMessage: false,
+    readonly: false,
+    unreadMessageCount: 2,
+    lastMessageCreated: null,
+    lastMessage: null,
+    canEdit: false
+  },
+  {
+    uuid: '4',
+    name: 'Team 4',
+    avatar: 'https://cdn.filestackcontent.com/uYQuauwNRdD43PfCQ4iW',
+    pusherChannel: 'pusher-channel-name',
+    roles: ['participant'],
+    isAnnouncement: false,
+    isDirectMessage: false,
+    readonly: false,
+    unreadMessageCount: 2,
+    lastMessageCreated: '2020-05-30 04:18:45',
+    lastMessage: 'test 4',
+    canEdit: false
+  },
+];
+
+export const ChatsFixture = chats;
