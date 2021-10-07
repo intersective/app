46482056a3d1fb5294534cc31d252d652ad265c0
Sunil Badyankal <sunilsbcloud@gmail.com>
2021-10-07 19:39:28 +1100

    release us hot fix

--- env.sh
+++ env.sh
@@ -3,6 +3,6 @@ while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOM_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
         sed -i "s#<$name>#${!name}#g" angular.json
-        echo "replacing <$name> to ${!name}"
+        # echo "replacing <$name> to ${!name}"
     fi
 done < <(env)
