2a1ec3b1456e36f3a860c0b4008ea31504f93558
chaw <cshen_581@hotmail.com>
2022-01-24 15:56:16 +0800

    Revert "Merge pull request #1333 from intersective/revert-1332-ng12/resolve-ngx-embed-video"
    This reverts commit a9f2105ee584d6e02295b4fa210e31e8bfe34b1a, reversing
    changes made to 35167d46e3dc60805b39fd6d9a2e872e355e1341.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { AppComponent } from './app.component';
 import { UtilsService } from './services/utils.service';
 import { VersionCheckService } from './services/version-check.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { EmbedVideo } from 'ngx-embed-video';
+import { EmbedVideoModule } from '@trtshen/ngx-embed-video';
 import { environment } from '@environments/environment';
 import { IntercomModule } from 'ng-intercom';
 import { PusherModule } from '@shared/pusher/pusher.module';
@@ -88,7 +88,7 @@ function initializeApp(
     HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    EmbedVideo.forRoot(),
+    EmbedVideoModule.forRoot(),
     NewRelicModule.forRoot(),
     NotificationModule,
     FastFeedbackModule,
