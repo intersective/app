e3eb21f60d2bdb678bbd2d8a9e7feb178fb84f40
chaw <cshen_581@hotmail.com>
2022-01-25 23:34:57 +0800

    Update preference.component.ts

--- src/app/preference/preference.component.ts
+++ src/app/preference/preference.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PreferenceService } from './preference.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-preference',
