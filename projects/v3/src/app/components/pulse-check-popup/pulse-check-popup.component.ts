13201f35a45a28d74bd6aa9532dce5ff495c1658
chaw <cshen_581@hotmail.com>
2022-03-07 13:23:12 +0800

    [AV2-1002] placeholder components

---
+++ projects/v3/src/app/components/pulse-check-popup/pulse-check-popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pulse-check-popup',
+  templateUrl: './pulse-check-popup.component.html',
+  styleUrls: ['./pulse-check-popup.component.scss'],
+})
+export class PulseCheckPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
