edafdc869f35bb3312c9c1bf8dca1a6b2766b323
Shawn <shawn@practera.com>
2022-03-08 11:08:03 +0800

    av2-1002

--- projects/v3/src/app/pages/home/home.page.ts
+++ projects/v3/src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeService } from '../../services/home.service';
-import { UtilsService } from '../../services/utils.service';
+import { HomeService } from '@v3/services/home.service';
+import { UtilsService } from '@v3/services/utils.service';
 
 @Component({
   selector: 'app-home',
