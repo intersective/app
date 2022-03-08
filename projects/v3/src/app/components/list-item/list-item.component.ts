797eb723ebb6f721185ad13e0fdd417c30fdc4b5
Shawn <shawn@practera.com>
2022-03-08 23:36:25 +0800

    av2-1002

---
+++ projects/v3/src/app/components/list-item/list-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss']
+})
+export class ListItemComponent {
+  @Input() loading: boolean;
+  @Input() lines = '';
+  @Input() leadingIcon: string;
+  @Input() leadingIconColor: string;
+  @Input() leadingIconPulsing: boolean;
+  @Input() title: string;
+  @Input() titleColor: string;
+  @Input() subtitle1: string;
+  @Input() subtitle1Color: string;
+  @Input() subtitle2: string;
+  @Input() endingText: string | number;
+  @Input() endingIcon: string;
+  @Input() endingIconColor: string;
+  // whether hightlight the background or not
+  @Input() active: boolean;
+  @Input() isEventItem: boolean;
+  @Input() eventExpired: boolean;
+  @Input() eventVideoConference: any;
+  @Input() eventFullyBooked: boolean;
+  @Input() eventDayCount: string;
+
+}
