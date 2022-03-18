8a088c55e2d1dec51a2e6ddd599b94ea904bd9f5
chaw <cshen_581@hotmail.com>
2022-03-18 10:20:30 +0800

    [AV2-1007]

--- projects/v3/src/app/personalised-header/personalised-header.component.ts
+++ projects/v3/src/app/personalised-header/personalised-header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { SettingsPage } from '@v3/app/pages/settings/settings.page';
 import { AnimationsService } from '@v3/services/animations.service';
-// import { NotificationsComponent } from '../components/notifications/notifications.component';
+import { NotificationsComponent } from '../components/notifications/notifications.component';
 
 @Component({
   selector: 'app-personalised-header',
@@ -20,15 +20,15 @@ export class PersonalisedHeaderComponent implements OnInit {
     console.log('personalised header')
   }
 
-  // async notifications(): Promise<void> {
-  //   const modal = await this.modalController.create({
-  //     component: NotificationsComponent,
-  //     enterAnimation: this.animationService.enterAnimation,
-  //     leaveAnimation: this.animationService.leaveAnimation,
-  //     cssClass: 'right-affixed',
-  //   });
-  //   return modal.present();
-  // }
+  async notifications(): Promise<void> {
+    const modal = await this.modalController.create({
+      component: NotificationsComponent,
+      enterAnimation: this.animationService.enterAnimation,
+      leaveAnimation: this.animationService.leaveAnimation,
+      cssClass: 'right-affixed',
+    });
+    return modal.present();
+  }
 
   async settings(): Promise<void> {
     const modal = await this.modalController.create({
