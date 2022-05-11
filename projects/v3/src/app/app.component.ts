6df975ef8441a313f2fa40e4ec14575224088f03
sasangachathumal <chathumal.sasanga02@gmail.com>
2022-05-11 10:53:17 +0530

    update app component to get config for domain. update app component to check query params and navigate user. add version check service.

--- projects/v3/src/app/app.component.ts
+++ projects/v3/src/app/app.component.ts
@@ -4,32 +4,170 @@ import { Platform } from '@ionic/angular';
 import { SharedService } from '@v3/services/shared.service';
 import { Observable } from 'rxjs';
 import { environment } from '@v3/environments/environment';
+import { BrowserStorageService } from '@v3/services/storage.service';
+import { UtilsService } from '@v3/services/utils.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AuthService } from '@v3/services/auth.service';
+import { VersionCheckService } from '@v3/services/version-check.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'v3';
+  customHeader: string | any;
 
   constructor(
     private platform: Platform,
     private router: Router,
     private sharedService: SharedService,
+    private ngZone: NgZone,
+    private storage: BrowserStorageService,
+    public utils: UtilsService,
+    public sanitizer: DomSanitizer,
+    private authService: AuthService,
+    private versionCheckService: VersionCheckService,
   ) {
+    this.customHeader = null;
     this.initializeApp();
+  }
+
+  // force every navigation happen under radar of angular
+  private navigate(direction): Promise<boolean> {
+    return this.ngZone.run(() => {
+      return this.router.navigate(direction);
+    });
+  }
+
+  private configVerification(): void {
+    if (this.storage.get('fastFeedbackOpening')) { // set default modal status
+      this.storage.set('fastFeedbackOpening', false);
+    }
+  }
+
+  ngOnInit() {
+    this.configVerification();
+    this.sharedService.onPageLoad();
+
+    // @TODO: need to build a new micro service to get the config and serve the custom branding config from a microservice
+    // Get the custom branding info and update the theme color if needed
+    const domain = window.location.hostname;
+    this.authService.getConfig({ domain }).subscribe(
+      (response: any) => {
+        if (response !== null) {
+          const expConfig = response.data;
+          const numOfConfigs = expConfig.length;
+          if (numOfConfigs > 0 && numOfConfigs < 2) {
+            let logo = expConfig[0].logo;
+
+            const config = expConfig[0].config || {}; // let it fail gracefully
+
+            if (config.html_branding && config.html_branding.header) {
+              this.customHeader = config.html_branding.header;
+            }
+            if (this.customHeader) {
+              this.customHeader = this.sanitizer.bypassSecurityTrustHtml(this.customHeader);
+            }
+
+            // add the domain if the logo url is not a full url
+            if (!logo.includes('http') && !this.utils.isEmpty(logo)) {
+              logo = environment.APIEndpoint + logo;
+            }
+            const colors = {
+              theme: config.theme_color,
+            };
+            this.storage.setConfig({
+              logo,
+              colors,
+            });
+
+            // use brand color from getConfig API if no cached color available
+            // in storage.getUser()
+            if (!this.utils.has(this.storage.getUser(), 'colors') || !this.storage.getUser().colors) {
+              this.utils.changeThemeColor(colors);
+            }
+          }
+        }
+      },
+      err => {
+
+      }
+    );
+
+    let searchParams = null;
+    let queryString = '';
+    if (window.location.search) {
+      queryString = window.location.search.substring(1);
+    } else if (window.location.hash) {
+      queryString = window.location.hash.substring(2);
+    }
+    searchParams = new URLSearchParams(queryString);
+
+    if (searchParams.has('apikey')) {
+      const queries = this.utils.urlQueryToObject(queryString);
+      return this.navigate(['auth', 'global_login', searchParams.get('apikey'), queries]);
+    }
+
+    if (searchParams.has('do')) {
+      switch (searchParams.get('do')) {
+        case 'secure':
+          if (searchParams.has('auth_token')) {
+            const queries = this.utils.urlQueryToObject(queryString);
+            this.navigate([
+              'auth',
+              'secure',
+              searchParams.get('auth_token'),
+              queries
+            ]);
+          }
+          break;
+        case 'resetpassword':
+          if (searchParams.has('key') && searchParams.has('email')) {
+            this.navigate([
+              'auth',
+              'reset_password',
+              searchParams.get('key'),
+              searchParams.get('email')
+            ]);
+          }
+          break;
+
+        case 'registration':
+          if (searchParams.has('key') && searchParams.has('email')) {
+            this.navigate([
+              'auth',
+              'registration',
+              searchParams.get('email'),
+              searchParams.get('key')
+            ]);
+          }
+          break;
+      }
+    }
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // if (environment.production) {
-      //   // watch version update
-      //   this.versionCheckService.initiateVersionCheck();
-      // }
+      if (environment.production) {
+        // watch version update
+        this.versionCheckService.initiateVersionCheck();
+      }
       // initialise Pusher when app loading
       this.sharedService.initWebServices();
     });
   }
+
+  /**
+   * checking conditions to show custom header
+   * @param type header
+   */
+     checkCustom(type: string): boolean {
+      if (type === 'header' && this.customHeader && this.authService.isAuthenticated()) {
+        return true;
+      }
+      return false;
+    }
 
 }
---
+++ projects/v3/src/app/services/version-check.service.ts
@@ -0,0 +1,58 @@
+import { Inject, Injectable, InjectionToken, NgZone } from '@angular/core';
+import { Observable, interval, pipe } from 'rxjs';
+import { switchMap, concatMap, tap, retryWhen, take, delay } from 'rxjs/operators';
+import { RequestService } from 'request';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class VersionCheckService {
+  private currentHash = '{{POST_BUILD_ENTERS_HASH_HERE}}';
+
+  constructor(
+    private requestService: RequestService,
+    private router: Router,
+    private ngZone: NgZone
+  ) {}
+
+  // check every 3 minutes
+  initiateVersionCheck(frequency = 1000 * 60 * 3) {
+    // make the interval run outside of angular so that we can benifit from the automation test
+    return this.ngZone.runOutsideAngular(() => {
+      this.trackVersion(frequency).subscribe(
+        (res: { hash: string; version: string; }) => {
+          if (this.hasHashChanged(this.currentHash, res.hash)) {
+            this.router.navigate(['logout', { t: new Date().getTime() }]);
+          }
+        },
+        (err) => console.log
+      );
+    });
+  }
+
+  trackVersion(frequency): Observable<any> {
+    return interval(frequency).pipe(
+      switchMap(() => this.requestService.get(`${window.location.origin}/version.json?t=${new Date().getTime()}`, {
+        headers: {
+          'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+          'Pragma': 'no-cache',
+          'Expires': 0,
+        }}),
+      ),
+      retryWhen(errors => {
+        // retry for 5 times if anything go wrong
+        return errors.pipe(delay(1000), take(5));
+      })
+    );
+  }
+
+  private hasHashChanged(currentHash, newHash) {
+    if (!currentHash || currentHash === '{{POST_BUILD_ENTERS_HASH_HERE}}') {
+      return false;
+    }
+
+    return currentHash !== newHash;
+  }
+}
