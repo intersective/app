814bd528d1ef1a0a1f17194a8bb96a2e16195f3e
chaw <cshen_581@hotmail.com>
2022-05-20 12:49:12 +0800

    [AV2-1083]

--- projects/v3/src/app/components/slidable/slidable.component.ts
+++ projects/v3/src/app/components/slidable/slidable.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, OnChanges, SimpleChanges, EventEmitter } from '@angular/core';
-import { UtilsService } from '@services/utils.service';
+import { UtilsService } from '@v3/app/services/utils.service';
 import * as moment from 'moment';
 
 @Component({
