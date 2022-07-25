fb0927c83635e5b3aa6a0d0c964148dc1d93fa1d
chaw <cshen_581@hotmail.com>
2022-07-25 15:56:14 +0800

    [AV2-1211]

--- projects/v3/src/app/components/video-conversion/video-conversion.component.ts
+++ projects/v3/src/app/components/video-conversion/video-conversion.component.ts
@@ -1,7 +1,14 @@
-import { Component, Input, Output, OnChanges, SimpleChanges, ViewChild, ElementRef, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, Input, Output, OnChanges, SimpleChanges, EventEmitter, ViewEncapsulation, OnDestroy, OnInit } from '@angular/core';
 import { FilestackService } from '@v3/app/services/filestack.service';
-// import { UtilsService } from '@services/utils.service';
-// import { environment } from '@environments/environment';
+import { Subject, Subscription } from 'rxjs';
+import { delay, repeat, takeUntil } from 'rxjs/operators';
+
+interface FilestackConversionResponse {
+  status: string;
+  data: {
+    url: string;
+  };
+}
 
 @Component({
   selector: 'app-video-conversion',
@@ -9,34 +16,59 @@ import { FilestackService } from '@v3/app/services/filestack.service';
   styleUrls: ['video-conversion.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class VideoConversionComponent implements OnChanges {
+export class VideoConversionComponent implements OnInit, OnChanges, OnDestroy {
   @Input() video?;
-  @Input() roles?;
   @Output() preview = new EventEmitter();
   result = null;
+  stop$: Subject<boolean> = new Subject<boolean>();
+  subscriptions: Subscription[] = [];
+  waitedTooLong: boolean = false;
 
   constructor(private filestackService: FilestackService) {}
 
+  ngOnInit(): void {
+    const stillWaiting = setTimeout(() => {
+      this.waitedTooLong = true;
+    }, 10000);
+
+    this.stop$.subscribe(res => {
+      if (res === true) {
+        clearTimeout(stillWaiting);
+      }
+    });
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('video::', this.video);
     if (this.video?.fileObject?.mimetype !== 'video/mp4') {
       this.convertVideo(this.video.fileObject);
     }
-    console.log('mmm::', changes);
+  }
+
+  ngOnDestroy(): void {
+    this.stop$.next(true);
+    if (this.subscriptions.length > 0) {
+      this.subscriptions.forEach(subs => subs.unsubscribe());
+    }
   }
 
   convertVideo(file) {
-    this.filestackService.videoConversion(file.handle).subscribe(res => {
+    this.subscriptions.push(this.filestackService.videoConversion(file.handle).pipe(
+      delay(2000),
+      repeat(10),
+      takeUntil(this.stop$),
+    ).subscribe((res: FilestackConversionResponse) => {
       this.result = res;
-    });
+      if (res?.status === 'completed') {
+        this.stop$.next(true);
+      }
+    }));
   }
 
-  showInFilestackPreview(file) {
-    return this.filestackService.previewModal(file.data.url, {
-      url: this.video.fileObject.url
-      // url: file.data.url,
-      // handle: this.video.fileObject.handle,
+  showInFilestackPreview(file: FilestackConversionResponse) {
+    const downloadURL = file.data.url;
+    const streamURL = this.video.fileObject.url;
+    return this.filestackService.previewModal(downloadURL, {
+      url: streamURL
     });
-    // return this.preview.emit(this.video.fileObject);
   }
 }
