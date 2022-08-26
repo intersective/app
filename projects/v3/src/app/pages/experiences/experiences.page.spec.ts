2489cdbdaf8ddc8d54d11b1b1f39a0cb9ad16a70
chaw <cshen_581@hotmail.com>
2022-08-26 10:50:18 +0800

    [AV2-1232]

--- projects/v3/src/app/pages/experiences/experiences.page.spec.ts
+++ projects/v3/src/app/pages/experiences/experiences.page.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import { BrowserStorageService } from '@app/services/storage.service';
+import { UtilsService } from '@app/services/utils.service';
+import { IonicModule, LoadingController } from '@ionic/angular';
+import { ExperienceService } from '@v3/app/services/experience.service';
+import { NotificationsService } from '@v3/app/services/notifications.service';
 
 import { ExperiencesPage } from './experiences.page';
 
@@ -10,7 +15,37 @@ describe('ExperiencesPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ExperiencesPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: Router,
+          useValue: jasmine.createSpyObj('Router', ['']),
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: jasmine.createSpyObj('ActivatedRoute', ['']),
+        },
+        {
+          provide: ExperienceService,
+          useValue: jasmine.createSpyObj('ExperienceService', ['']),
+        },
+        {
+          provide: LoadingController,
+          useValue: jasmine.createSpyObj('LoadingController', ['']),
+        },
+        {
+          provide: NotificationsService,
+          useValue: jasmine.createSpyObj('NotificationsService', ['']),
+        },
+        {
+          provide: UtilsService,
+          useValue: jasmine.createSpyObj('UtilsService', ['']),
+        },
+        {
+          provide: BrowserStorageService,
+          useValue: jasmine.createSpyObj('BrowserStorageService', ['']),
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ExperiencesPage);
--- projects/v3/src/app/pages/review-mobile/review-mobile.page.spec.ts
+++ projects/v3/src/app/pages/review-mobile/review-mobile.page.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UtilsService } from '@app/services/utils.service';
 import { IonicModule } from '@ionic/angular';
+import { ReviewService } from '@v3/app/services/review.service';
 
 import { ReviewMobilePage } from './review-mobile.page';
 
@@ -10,7 +13,23 @@ describe('ReviewMobilePage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReviewMobilePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: jasmine.createSpyObj('ActivatedRoute', ['queryParams']),
+        },
+        {
+          provide: Router,
+          useValue: jasmine.createSpyObj('Router', ['navigate']),
+        },
+        {
+          provide: ReviewService,
+          useValue: jasmine.createSpyObj('ReviewService', [
+            'reviews$', 'getReviews'
+          ]),
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReviewMobilePage);
