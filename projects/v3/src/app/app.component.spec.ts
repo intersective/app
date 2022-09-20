1525f6bea73ca623f8042d2421fd24e8fb888f5a
chaw <cshen_581@hotmail.com>
2022-09-20 17:04:42 +0800

    [AV2-1242] appcomponent 93%

--- projects/v3/src/app/app.component.spec.ts
+++ projects/v3/src/app/app.component.spec.ts
@@ -19,6 +19,8 @@ describe('AppComponent', () => {
   let sharedServiceSpy: SharedService;
   let versionCheckServiceSpy: VersionCheckService;
   let utilsSpy: UtilsService;
+  let storageSpy: BrowserStorageService;
+  let authSpy: AuthService;
   let routerSpy: Router;
   let /* statusBarSpy, splashScreenSpy, platformReadySpy, */ platformSpy;
 
@@ -55,6 +57,8 @@ describe('AppComponent', () => {
             'get',
             'setUser',
             'getConfig',
+            'setConfig',
+            'getUser',
           ]),
         },
         {
@@ -79,9 +83,11 @@ describe('AppComponent', () => {
     }).compileComponents();
     platformSpy = TestBed.inject(Platform);
     sharedServiceSpy = TestBed.inject(SharedService) as jasmine.SpyObj<SharedService>;
+    storageSpy = TestBed.inject(BrowserStorageService) as jasmine.SpyObj<BrowserStorageService>;
     versionCheckServiceSpy = TestBed.inject(VersionCheckService) as jasmine.SpyObj<VersionCheckService>;
     utilsSpy = TestBed.inject(UtilsService) as jasmine.SpyObj<UtilsService>;
     routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    authSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
   });
 
   it('should create the app', () => {
@@ -116,6 +122,7 @@ describe('AppComponent', () => {
 
   describe('ngOnInit()', () => {
     it('should initialize app', fakeAsync(() => {
+      storageSpy.get = jasmine.createSpy('storageSpy.get').and.callThrough();
       utilsSpy.getCurrentLocation = jasmine.createSpy('getCurrentURL').and.returnValue({
         domain: '',
         search: '?apikey=abcdefg'
@@ -124,7 +131,79 @@ describe('AppComponent', () => {
       const app = fixture.componentInstance;
       app.ngOnInit();
       tick();
-      // storageSpy
+      expect(storageSpy.get).toHaveBeenCalled();
+      expect(routerSpy.navigate).toHaveBeenCalled();
+    }));
+
+    it('should initialize app with config', fakeAsync(() => {
+      storageSpy.get = jasmine.createSpy('storageSpy.get').and.callThrough();
+      authSpy.getConfig = jasmine.createSpy('getConfig').and.returnValue(of({ data: [
+        {
+          logo: '',
+          config: {
+            html_branding: {
+              header: '',
+              theme_color: '',
+            }
+          }
+        }
+      ] }));
+      utilsSpy.getCurrentLocation = jasmine.createSpy('getCurrentURL').and.returnValue({
+        domain: '',
+        search: '?apikey=abcdefg'
+      });
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.ngOnInit();
+      tick();
+      expect(storageSpy.get).toHaveBeenCalled();
+      expect(storageSpy.setConfig).toHaveBeenCalled();
+      expect(storageSpy.getUser).toHaveBeenCalled();
+      expect(routerSpy.navigate).toHaveBeenCalled();
+    }));
+
+    it('should initiate secure login (direct login)', fakeAsync(() => {
+      storageSpy.get = jasmine.createSpy('storageSpy.get').and.callThrough();
+      utilsSpy.getCurrentLocation = jasmine.createSpy('getCurrentURL').and.returnValue({
+        domain: '',
+        search: '?do=secure&auth_token=abcdefg'
+      });
+
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.ngOnInit();
+      tick();
+      expect(storageSpy.get).toHaveBeenCalled();
+      expect(routerSpy.navigate).toHaveBeenCalled();
+    }));
+
+    it('should initiate reset password', fakeAsync(() => {
+      storageSpy.get = jasmine.createSpy('storageSpy.get').and.callThrough();
+      utilsSpy.getCurrentLocation = jasmine.createSpy('getCurrentURL').and.returnValue({
+        domain: '',
+        search: '?do=resetpassword&key=abcdefg&email=dummy@email.com'
+      });
+
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.ngOnInit();
+      tick();
+      expect(storageSpy.get).toHaveBeenCalled();
+      expect(routerSpy.navigate).toHaveBeenCalled();
+    }));
+
+    it('should initiate registration', fakeAsync(() => {
+      storageSpy.get = jasmine.createSpy('storageSpy.get').and.callThrough();
+      utilsSpy.getCurrentLocation = jasmine.createSpy('getCurrentURL').and.returnValue({
+        domain: '',
+        hash: '/?do=registration&key=abcdefg&email=dummy2@email.com'
+      });
+
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.ngOnInit();
+      tick();
+      expect(storageSpy.get).toHaveBeenCalled();
       expect(routerSpy.navigate).toHaveBeenCalled();
     }));
   });
