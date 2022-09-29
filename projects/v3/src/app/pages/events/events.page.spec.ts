0e9fd4d13a0c709ed2921fa495c11a2e3439ef4f
chaw <cshen_581@hotmail.com>
2022-09-30 07:33:55 +0800

    [AV2-1248]

--- projects/v3/src/app/pages/events/events.page.spec.ts
+++ projects/v3/src/app/pages/events/events.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { UtilsService } from '@v3/services/utils.service';
 import { IonicModule } from '@ionic/angular';
@@ -41,5 +41,57 @@ describe('EventsPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('goto()', () => {
+    it('should display selected event', () => {
+      const spy = spyOn<any>(component['document'], 'getElementById').and.returnValue({ focus: jasmine.createSpy() });
+      const curEvent = {
+        id: 1,
+        isMultiDay: false,
+        multiDayInfo: {
+          id: 1
+        },
+      };
+      component.goto(curEvent);
+      expect(component.currentEvent).toEqual(jasmine.objectContaining(curEvent));
+      expect(component.eventId).toEqual(curEvent.id);
+      expect(spy).toHaveBeenCalledWith('eventDetail');
+    });
+  });
+
+  describe('checkin()', () => {
+    it('should check-in an event', fakeAsync(() => {
+      component.assessment = {
+        onEnter: () => true,
+      };
+      const spy = spyOn(component.assessment, 'onEnter');
+      component.checkin({
+        assessmentId: 1,
+        contextId: 2,
+      });
+
+      expect(component.assessmentId).toEqual(1);
+      expect(component.contextId).toEqual(2);
+      tick(500);
+      expect(spy).toHaveBeenCalled();
+    }));
+
+    it('should return void when id not provided', () => {
+      expect(component.checkin({
+        assessmentId: null,
+        contextId: 2,
+      })).toEqual(undefined);
+
+      expect(component.checkin({
+        assessmentId: 1,
+        contextId: null,
+      })).toEqual(undefined);
+
+      expect(component.checkin({
+        assessmentId: null,
+        contextId: null,
+      })).toEqual(undefined);
+    });
   });
 });
