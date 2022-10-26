7a35b608276051d8e2e833a7179377acdb0e2d79
chaw <cshen_581@hotmail.com>
2022-10-26 16:32:58 +0800

    [AV2-1258]

--- projects/v3/src/app/pages/notifications/notifications.page.spec.ts
+++ projects/v3/src/app/pages/notifications/notifications.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flushMicrotasks, TestBed, waitForAsync } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { UtilsService } from '@v3/services/utils.service';
 import { IonicModule, ModalController } from '@ionic/angular';
@@ -12,6 +12,9 @@ import { MockRouter } from '@testingv3/mocked.service';
 describe('NotificationsPage', () => {
   let component: NotificationsPage;
   let fixture: ComponentFixture<NotificationsPage>;
+  let routerSpy: Router;
+  let utilsSpy: UtilsService;
+  let modalSpy: ModalController;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -45,10 +48,54 @@ describe('NotificationsPage', () => {
 
     fixture = TestBed.createComponent(NotificationsPage);
     component = fixture.componentInstance;
+    routerSpy = TestBed.inject(Router);
+    utilsSpy = TestBed.inject(UtilsService);
+    modalSpy = TestBed.inject(ModalController);
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('timeFormatter()', () => {
+    it('should format time with UtilsService', () => {
+      component.timeFormatter('abcdefg');
+      expect(utilsSpy.timeFormatter).toHaveBeenCalledWith('abcdefg');
+    });
+  });
+
+  describe('goToReview()', () => {
+    it('should go to review page', fakeAsync(() => {
+      utilsSpy.isMobile = jasmine.createSpy('isMobile').and.returnValue(false);
+
+      const submissionId = 3;
+      component.goToReview(1, 2, submissionId);
+
+      flushMicrotasks();
+
+      expect(routerSpy.navigate).toHaveBeenCalledWith([
+        'v3',
+        'review-desktop',
+        submissionId
+      ]);
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    }));
+  });
+
+  describe('goToChat()', () => {
+    it('should navigate to v3/messages', () => {
+      component.goToChat();
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['v3', 'messages']);
+    });
+  });
+
+  describe('goBack()', () => {
+    it('should dismiss notification modal', () => {
+      utilsSpy.isMobile = jasmine.createSpy('isMobile').and.returnValue(false);
+      component.goBack();
+
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    });
   });
 });
