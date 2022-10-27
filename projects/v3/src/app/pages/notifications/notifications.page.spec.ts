7d5b941e9e73a91b94d727b90d25753eb3eee35f
chaw <cshen_581@hotmail.com>
2022-10-27 12:58:13 +0800

    [AV2-1258]

--- projects/v3/src/app/pages/notifications/notifications.page.spec.ts
+++ projects/v3/src/app/pages/notifications/notifications.page.spec.ts
@@ -8,6 +8,9 @@ import { NotificationsService } from '@v3/services/notifications.service';
 import { NotificationsPage } from './notifications.page';
 import { of } from 'rxjs';
 import { MockRouter } from '@testingv3/mocked.service';
+import { ComponentsModule } from '@v3/app/components/components.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 describe('NotificationsPage', () => {
   let component: NotificationsPage;
@@ -15,11 +18,13 @@ describe('NotificationsPage', () => {
   let routerSpy: Router;
   let utilsSpy: UtilsService;
   let modalSpy: ModalController;
+  let notificationSpy: NotificationsService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NotificationsPage ],
-      imports: [IonicModule.forRoot()],
+      imports: [IonicModule.forRoot(), ComponentsModule, CommonModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         {
           provide: UtilsService,
@@ -51,11 +56,27 @@ describe('NotificationsPage', () => {
     routerSpy = TestBed.inject(Router);
     utilsSpy = TestBed.inject(UtilsService);
     modalSpy = TestBed.inject(ModalController);
+    notificationSpy = TestBed.inject(NotificationsService);
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('showEventDetail()', () => {
+    it('should go to event page', () => {
+      component.showEventDetail({ id: 1 } as any);
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['v3', 'events', {
+        event_id: 1,
+      }]);
+    });
+
+    it('should show modal in mobile view', () => {
+      utilsSpy.isMobile = jasmine.createSpy('isMobile').and.returnValue(true);
+      component.showEventDetail({ id: 1 } as any);
+      expect(notificationSpy.modal).toHaveBeenCalled();
+    });
   });
 
   describe('timeFormatter()', () => {
@@ -64,6 +85,31 @@ describe('NotificationsPage', () => {
       expect(utilsSpy.timeFormatter).toHaveBeenCalledWith('abcdefg');
     });
   });
+
+  describe('clickTodoItem()', () => {
+    it('should go to assessment', fakeAsync(() => {
+      component.goToAssessment = spyOn(component, 'goToAssessment');
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'feedback_available',
+        // type: 'review_submission',
+        // type: 'chat',
+        // type: 'assessment_submission_reminder',
+        // type: '', // default
+      } as any);
+
+      flushMicrotasks();
+
+
+      expect(component.goToAssessment).toHaveBeenCalled();
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    }))
+});
 
   describe('goToReview()', () => {
     it('should go to review page', fakeAsync(() => {
