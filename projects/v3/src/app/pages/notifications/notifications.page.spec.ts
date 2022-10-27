79c2418437558e2af6be134719aadb440d404a7e
chaw <cshen_581@hotmail.com>
2022-10-27 13:28:26 +0800

    [AV2-1258]

--- projects/v3/src/app/pages/notifications/notifications.page.spec.ts
+++ projects/v3/src/app/pages/notifications/notifications.page.spec.ts
@@ -8,9 +8,7 @@ import { NotificationsService } from '@v3/services/notifications.service';
 import { NotificationsPage } from './notifications.page';
 import { of } from 'rxjs';
 import { MockRouter } from '@testingv3/mocked.service';
-import { ComponentsModule } from '@v3/app/components/components.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 describe('NotificationsPage', () => {
   let component: NotificationsPage;
@@ -23,7 +21,7 @@ describe('NotificationsPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NotificationsPage ],
-      imports: [IonicModule.forRoot(), ComponentsModule, CommonModule],
+      imports: [IonicModule.forRoot()],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         {
@@ -35,9 +33,9 @@ describe('NotificationsPage', () => {
           useValue: jasmine.createSpyObj('NotificationsService', [
             'modal',
           ], {
-            'notification$': of(),
-            'eventReminder$': of(),
-            'newMessage$': of(),
+            'notification$': of(true),
+            'eventReminder$': of(true),
+            'newMessage$': of(true),
           }),
         },
         {
@@ -62,6 +60,21 @@ describe('NotificationsPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit()', () => {
+    it('should initiate subscriptions', () => {
+      utilsSpy.isEmpty = jasmine.createSpy('isEmpty').and.returnValue(false);
+      component['_addChatTodoItem'] = jasmine.createSpy('_addChatTodoItem');
+      // notificationSpy.notification$
+      // notificationSpy.eventReminder$
+      // notificationSpy.newMessage$
+      component.ngOnInit();
+
+      expect(component.todoItems).toEqual(true as any);
+      expect(component.eventReminders).toContain(true);
+      expect(component['_addChatTodoItem']).toHaveBeenCalledWith(true);
+    });
   });
 
   describe('showEventDetail()', () => {
@@ -97,18 +110,87 @@ describe('NotificationsPage', () => {
           assessment_submission_id: 4,
         },
         type: 'feedback_available',
-        // type: 'review_submission',
-        // type: 'chat',
         // type: 'assessment_submission_reminder',
         // type: '', // default
       } as any);
 
       flushMicrotasks();
 
+      expect(component.goToAssessment).toHaveBeenCalled();
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    }));
+
+    it('should go to review', fakeAsync(() => {
+      component.goToReview = spyOn(component, 'goToReview');
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'review_submission',
+      } as any);
+
+      flushMicrotasks();
+
+      expect(component.goToReview).toHaveBeenCalled();
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    }));
+
+    it('should go to chat', fakeAsync(() => {
+      component.goToChat = spyOn(component, 'goToChat');
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'chat',
+      } as any);
+
+      flushMicrotasks();
+
+      expect(component.goToChat).toHaveBeenCalled();
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    }));
+
+    it('should go to assessment (submission reminder)', fakeAsync(() => {
+      component.goToAssessment = spyOn(component, 'goToAssessment');
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'assessment_submission_reminder',
+      } as any);
+
+      flushMicrotasks();
 
       expect(component.goToAssessment).toHaveBeenCalled();
       expect(modalSpy.dismiss).toHaveBeenCalled();
-    }))
+    }));
+
+    it('should by default, go to show event detail', fakeAsync(() => {
+      component.showEventDetail = spyOn(component, 'showEventDetail');
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'any other type',
+      } as any);
+
+      flushMicrotasks();
+
+      expect(component.showEventDetail).toHaveBeenCalled();
+      expect(modalSpy.dismiss).toHaveBeenCalled();
+    }));
 });
 
   describe('goToReview()', () => {
@@ -143,5 +225,65 @@ describe('NotificationsPage', () => {
 
       expect(modalSpy.dismiss).toHaveBeenCalled();
     });
+  });
+
+  describe('keyboardEvent', () => {
+    it('should fail keyboardEvent', fakeAsync(() => {
+      const keyboardEvent = new KeyboardEvent('keydown', {
+        key: 'Tab',
+        code: 'Tab',
+      });
+
+      keyboardEvent.preventDefault = spyOn(keyboardEvent, 'preventDefault');
+
+      component.dismiss(keyboardEvent).then(resultDismiss => {
+        expect(resultDismiss).toBeUndefined();
+      });
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'any type',
+      }, keyboardEvent).then(clickTodoItem => {
+        expect(clickTodoItem).toBeUndefined();
+      });
+      const showEventDetail = component.showEventDetail({} as any, keyboardEvent);
+      const goBack = component.goBack(keyboardEvent);
+      flushMicrotasks();
+
+      expect(showEventDetail).toBeUndefined();
+      expect(goBack).toBeUndefined();
+      expect(keyboardEvent.preventDefault).not.toHaveBeenCalled();
+    }));
+
+
+    it('should accept correct keyboardEvent', fakeAsync(() => {
+      const keyboardEvent = new KeyboardEvent('keydown', {
+        key: 'Space',
+        code: 'Space',
+      });
+
+      keyboardEvent.preventDefault = spyOn(keyboardEvent, 'preventDefault');
+
+      component.dismiss(keyboardEvent);
+      component.clickTodoItem({
+        meta: {
+          activity_id: 1,
+          context_id: 2,
+          assessment_id: 3,
+          assessment_submission_id: 4,
+        },
+        type: 'any type',
+      }, keyboardEvent);
+      const showEventDetail = component.showEventDetail({} as any, keyboardEvent);
+      component.goBack(keyboardEvent);
+      flushMicrotasks();
+
+      expect(showEventDetail).toBeUndefined();
+      expect(keyboardEvent.preventDefault).toHaveBeenCalledTimes(4);
+    }));
   });
 });
