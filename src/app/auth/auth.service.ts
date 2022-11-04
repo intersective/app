0cf3f898b007fce13df568cfc762aa30b2245b95
chaw <3895281+trtshen@users.noreply.github.com>
2022-11-04 10:25:53 +0800

    [AV2-1280]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -91,6 +91,13 @@ export class AuthService {
         this.storage.setAppV3(true);
         let finalURL = '';
         if (this.deeplink) {
+          const onePageOnly = this.deeplink.match(/(one_page_only=true)/g);
+          const redirectReview = this.deeplink.match(/(redirect=review)/g);
+          if (onePageOnly !== null && redirectReview !== null) { // temporary allow review to be done on AppV2
+            this.deeplink = null;
+            return;
+          }
+
           finalURL = this.deeplink.replace(/https?\:\/\/[\w\W]+\//g, environment.appv3URL);
         } else {
           finalURL = `${environment.appv3URL}?apikey=${res.data.apikey}`;
