5c4d65cce1b832c58e3574e117e288dda8f8aa28
sasangachathumal <chathumal.sasanga02@gmail.com>
2023-05-15 14:37:37 +0530

    [AV2-1376] update environment file with hubspot ids. update hubspot service api params. Added all the support questions provided. Added success and error messages to popup.

--- projects/v3/src/environments/environment.custom.ts
+++ projects/v3/src/environments/environment.custom.ts
@@ -11,7 +11,6 @@ export const environment = {
   chatGraphQL: '<CUSTOM_CHAT_GRAPH_QL>',
   globalLoginUrl: '<CUSTOM_GLOBAL_LOGIN_URL>',
   stackUuid: '<CUSTOM_STACK_UUID>',
-  liveServerRegion: '<CUSTOM_LIVE_SERVER_REGION>',
   intercomAppId: '<CUSTOM_INTERCOM>',
   filestack: {
     key: '<CUSTOM_FILESTACK_KEY>',
