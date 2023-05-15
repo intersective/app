c5354d2e27331fc3d67a8b2ea3c3002eb7348d1b
sasangachathumal <chathumal.sasanga02@gmail.com>
2023-05-15 14:37:37 +0530

    [AV2-1376] update environment file with hubspot ids. update hubspot service api params. Added all the support questions provided. Added success and error messages to popup.

--- projects/v3/src/app/components/support-popup/support-questions.ts
+++ projects/v3/src/app/components/support-popup/support-questions.ts
@@ -2,51 +2,136 @@ export const supportQuestionList = [
   {
     questionId: "one",
     question: "Why is something locked?",
-    answer: "If you come across locked or hidden content, don't get discouraged!  When content is locked or hidden, it's usually because there are certain requirements that need to be met before you can access it. This could include submitting an assessment or completing a certain activity, such as watching a video or reading an article."
+    answer: [
+      "If you come across locked or hidden content, don't get discouraged!  When content is locked or hidden, it's usually because there are certain requirements that need to be met before you can access it. This could include submitting an assessment or completing a certain activity, such as watching a video or reading an article.",
+      "By having these requirements in place, it ensures that learners are engaging with the content in a meaningful way and are gaining the necessary knowledge and skills to succeed. It also helps to ensure that everyone is on the same page and has a similar level of understanding before moving on to more advanced topics."
+    ]
   },
   {
     questionId: "two",
     question: "I'VE SUBMITTED THE WRONG FILE!",
-    answer: "If you've accidentally submitted the wrong file or have encountered any other issues while submitting an assessment, it's important to reach out to your program coordinator as soon as possible. You can do this by (insert how students can gain additional feedback through the hubspot form) and providing them with the program name and assessment name."
+    answer: [
+      "If you've accidentally submitted the wrong file or have encountered any other issues while submitting an assessment, it's important to reach out to your program coordinator as soon as possible. You can do this by (insert how students can gain additional feedback through the contact form) and providing them with the program name and assessment name.",
+      "It's important to be proactive and take action quickly, as this will help ensure that any issues are resolved in a timely manner and that you're able to continue making progress in your learning journey. "
+    ]
   },
   {
     questionId: "three",
     question: "WHY IS MY SUBMISSION PENDING?",
-    answer: "Congratulations on submitting your assessment! Your submission is pending while awaiting the feedback from your mentor or client. Waiting for feedback can be a nerve-wracking experience, but it's an important part of the learning process. Once you receive and read the feedback, you'll have a better understanding of areas for improvement to progress your report."
+    answer: [
+      "Congratulations on submitting your assessment! Your submission is pending while awaiting the feedback from your mentor or client. Waiting for feedback can be a nerve-wracking experience, but it's an important part of the learning process. Once you receive and read the feedback, you'll have a better understanding of areas for improvement to progress your report.",
+      "It's important to remember that pending submissions don't affect your progress percentage, so there's no need to worry about your progress being impacted while you wait for feedback. By submitting your work and receiving feedback, you're taking an active role in your learning and demonstrating your commitment to growth and improvement. "
+    ]
   },
   {
     questionId: "four",
     question: "WHY ISN'T MY PROGRESS AT 100%?",
-    answer: "In order to successfully complete a course or program, it's important to make sure that all topics, assessments, and assessment feedback have been marked as completed. This means that you'll need to complete all of the required tasks and activities in each topic, submit all assessments, and review and incorporate feedback from your instructors."
+    answer: [
+      "In order to successfully complete a course or program, it's important to make sure that all topics, assessments, and assessment feedback have been marked as completed. This means that you'll need to complete all of the required tasks and activities in each topic, submit all assessments, and review and incorporate feedback from your instructors.",
+      "It's important to stay on top of your progress and make sure that you're not missing any outstanding tasks or assignments. By completing everything that's required, you'll be able to demonstrate your mastery of the material and your readiness to move on to more advanced topics."
+    ]
   },
   {
     questionId: "five",
     question: "HOW DO I CHANGE MY PASSWORD?",
-    answer: "On the login page, click on 'Problem Signing In?' (you may need to logout first!). Input your email address and follow the instructions that you receive via email."
+    answer: [
+      "On the login page, click on 'Problem Signing In?' (you may need to logout first!). Input your email address and follow the instructions that you receive via email."
+    ]
   },
   {
     questionId: "six",
     question: "HOW DO I WITHDRAW FROM THE PROGRAM?",
-    answer: "If you're considering withdrawing from a program, it's important to take action as soon as possible. Withdrawing from a program can be a difficult decision, but it's important to prioritise your well-being and make the choice that's best for you. To withdraw from a program, the first step is to get in touch with your program coordinator by (process that will need to be included for hubspot form). They will be able to guide you through the process and provide you with any necessary information about the steps you'll need to take."
+    answer: [
+      "If you're considering withdrawing from a program, it's important to take action as soon as possible. Withdrawing from a program can be a difficult decision, but it's important to prioritise your well-being and make the choice that's best for you. To withdraw from a program, the first step is to get in touch with your program coordinator by (process that will need to be included for hubspot form). They will be able to guide you through the process and provide you with any necessary information about the steps you'll need to take.",
+      "To withdraw from a program, the first step is to get in touch with your program coordinator by (process that will need to be included for support form). They will be able to guide you through the process and provide you with any necessary information about the steps you'll need to take.",
+      "Remember, there's no shame in withdrawing from a program if it's not the right fit for you. It takes courage to acknowledge when something isn't working and to take action to change it. So be kind to yourself, and don't be afraid to reach out for help and support. Your program coordinator is there to help you, and they'll do everything they can to make the withdrawal process as smooth and stress-free as possible."
+    ]
   },
   {
     questionId: "seven",
     question: "WHY HAVEN'T I RECEIVED MY CERTIFICATE?",
-    answer: "To access your digital certificate, the first step is to check your inbox, including your Spam folder, for an email called 'You've been awarded a badge.' Once you've located the email, simply click the link 'View your Digital Certificate' at the bottom of the email to access your certificate. This is a great way to showcase your accomplishment to potential employers and colleagues."
+    answer: [
+      "Congratulations on completing your course and earning a badge! The journey of learning is often challenging, but receiving a badge is a fantastic way to acknowledge your hard work and achievements.",
+      "To access your digital certificate, the first step is to check your inbox, including your Spam folder, for an email called 'You've been awarded a badge.' Once you've located the email, simply click the link 'View your Digital Certificate' at the bottom of the email to access your certificate. This is a great way to showcase your accomplishment to potential employers and colleagues.",
+      "If you haven't received the email or access to your certificate, it's possible that you haven't yet met all the requirements explained in the final milestone. To ensure that you receive your certificate, make sure that you have submitted all assessments, including the feedback survey. Double-check the final milestone requirements to ensure that you've met all the criteria for completion.",
+      "Check your inbox, including your Spam folder, for an email called 'You've been awarded a badge.' To access your certificate click the link 'View your Digital Certificate' at the bottom of the email. Alternatively, you might not have been awarded the certificate yet. Check the requirements explained in the final milestone. Tip: remember to submit all assessments, including the feedback survey."
+    ]
   },
   {
     questionId: "eight",
     question: "HOW DO I ADD MY CERTIFICATE TO LINKEDIN?",
-    answer: "In the top right-hand corner of the certificate, click the LinkedIn share button. Paste this link under an Experience or Credential on your profile"
+    answer: [
+      "In the top right-hand corner of the certificate, click the LinkedIn share button. Paste this link under an Experience or Credential on your profile"
+    ]
   },
   {
     questionId: "nine",
     question: "HOW DO I ADD MY BADGE TO LINKEDIN?",
-    answer: "Save your badge as a PNG file. Then, upload it to a 'backpack', e.g. Open Badge Passport (using the same email address as per your Practera registration). Click on 'Share' to generate a media link. To add to LinkedIn, create an experience describing the program and add the media link."
+    answer: [
+      "Save your badge as a PNG file. Then, upload it to a 'backpack', e.g. Open Badge Passport (using the same email address as per your Practera registration). Click on 'Share' to generate a media link. To add to LinkedIn, create an experience describing the program and add the media link."
+    ]
   },
   {
     questionId: "ten",
     question: "Do I get a certificate and a Practera digital badge?",
-    answer: "You will receive a Practera Digital badge upon completion of the project, providing you have met all program requirements (time commitment, active participation and completion of all activities in Practera)."
+    answer: [
+      "You will receive a Practera Digital badge upon completion of the project, providing you have met all program requirements (time commitment, active participation and completion of all activities in Practera)."
+    ]
+  },
+  {
+    questionId: "eleven",
+    question: "Do I have to be in the country or city location of the client?",
+    answer: [
+      "No. This program supports remote working conditions with fully virtual industry and team engagement through the Practera App, so you aren’t required to be physically in the location of the client to participate"
+    ]
+  },
+  {
+    questionId: "twelve",
+    question: "What if I have a part-time job?",
+    answer: [
+      "It is fine if you have a part-time job, but you must be able to commit to the required number of hours of work on the project over theduration of the program. You will coordinate with your team to ensure that you’re able to attend meetings and contribute equally to all deliverables."
+    ]
+  },
+  {
+    questionId: "thirteen",
+    question: "Do I get to choose the project I work on?",
+    answer: [
+      "No, projects will be assigned to teams. We ensure clients submit projects that are interesting and achievable within the program timeframe."
+    ]
+  },
+  {
+    questionId: "fourteen",
+    question: "Do I get to choose my team?",
+    answer: [
+      "No, all teams will be assigned randomly to allow for diverse student teams. The opportunity to work  with people from different backgrounds is both a powerful learning experience and an opportunity to improve your professional networks."
+    ]
+  },
+  {
+    questionId: "fifteen",
+    question: "Will the industry project be related to my degree?",
+    answer: [
+      "It could be, but if it isn’t, that’s fine too. The projects are structured in a way where you do not need specific technical knowledge. The main goal is to develop your employability skills through a professional industry project experience."
+    ]
+  },
+  {
+    questionId: "sixteen",
+    question: "What type of projects can I expect?",
+    answer: [
+      "Projects are diverse in activities and outcomes. They could include conducting research, doing a competitor analysis, implementing a new product or business approach for the organisation or creating a social media strategy. Clients could be from not-for-profit, start-up, SME or a large corporation."
+    ]
+  },
+  {
+    questionId: "seventeen",
+    question: "Will I be paid to work on this Project?",
+    answer: [
+      "No, you will not be paid to work on this project. This is first and foremost a learning experience that is part of a subject and your course."
+    ]
+  },
+  {
+    questionId: "ighteen",
+    question: "Can I skip the orientation session?",
+    answer: [
+      "The orientation session is compulsory. You will meet your team, mentor and client.  Students who do not attend will be removed from the program."
+    ]
   },
 ]
