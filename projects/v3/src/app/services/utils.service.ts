8017182c7f8d62767e5ee6cc7d816dcbe02790bc
chaw <cshen_581@hotmail.com>
2023-05-15 17:35:25 +0800

    [AV2-1377]

--- projects/v3/src/app/services/utils.service.ts
+++ projects/v3/src/app/services/utils.service.ts
@@ -685,7 +685,7 @@ export class UtilsService {
   async openSupportPopup(options?: { formOnly: boolean; }) {
     const componentProps = {
       mode: 'modal',
-      formOnly: options?.formOnly
+      isShowFormOnly: options?.formOnly
     };
 
     const modal = await this.modalController.create({
