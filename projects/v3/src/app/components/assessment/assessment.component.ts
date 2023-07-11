dea177ae37aed8344959668a7108b34dff7af6e5
chaw <cshen_581@hotmail.com>
2023-07-11 16:49:57 +0800

    [AV2-1457] continuous autosave/submission

--- projects/v3/src/app/components/assessment/assessment.component.ts
+++ projects/v3/src/app/components/assessment/assessment.component.ts
@@ -6,7 +6,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { BrowserStorageService } from '@v3/services/storage.service';
 import { SharedService } from '@v3/services/shared.service';
 import { BehaviorSubject, Observable, of, Subject, Subscription, throwError } from 'rxjs';
-import { concatMap, debounceTime, delay, tap } from 'rxjs/operators';
+import { concatMap, delay, tap } from 'rxjs/operators';
 
 // const SAVE_PROGRESS_TIMEOUT = 10000; - AV2-1326
 
@@ -101,7 +101,7 @@ export class AssessmentComponent implements OnChanges, OnDestroy {
             return this.saveQuestionAnswer(request.questionSave);
           }
           return of(request);
-        })
+        }),
       ).subscribe(
         (data: {
           saveInProgress: boolean;
@@ -111,7 +111,15 @@ export class AssessmentComponent implements OnChanges, OnDestroy {
             questionId: number;
             answer: string;
           };
-        }): Promise<void> => {
+          error?: any;
+        }): void | Promise<void> => {
+          if (!this.utils.isEmpty(data.error)) {
+            return this.notifications.assessmentSubmittedToast({
+              isFail: true,
+              label: $localize`Save failed.`,
+            });
+          }
+
           if (data.saveInProgress === false) {
             return this._submitWithoutAnswer(data);
           }
