395e9ec58e272492e3e9722d56be815b5faccdb3
chaw <cshen_581@hotmail.com>
2023-07-25 14:46:52 +0800

    [CORE-5930] removed autosave in mobile view

--- projects/v3/src/app/pages/assessment-mobile/assessment-mobile.page.ts
+++ projects/v3/src/app/pages/assessment-mobile/assessment-mobile.page.ts
@@ -30,8 +30,6 @@ export class AssessmentMobilePage implements OnInit {
 
   currentTask: Task;
 
-  @ViewChild('assessmentEle') assessmentEle;
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -86,7 +84,6 @@ export class AssessmentMobilePage implements OnInit {
   }
 
   goBack() {
-    this.assessmentEle.btnBackClicked();
     if (this.fromPage === 'reviews') {
       return this.router.navigate(['v3', 'reviews']);
     }
@@ -97,7 +94,7 @@ export class AssessmentMobilePage implements OnInit {
   }
 
   async saveAssessment(event) {
-    if (event.saveInProgress && this.saving) {
+    if (event.autoSave && this.saving) {
       return;
     }
 
@@ -119,7 +116,7 @@ export class AssessmentMobilePage implements OnInit {
           throw new Error("Error submitting assessment");
         }
 
-        if (this.assessment.pulseCheck === true && event.saveInProgress === false) {
+        if (this.assessment.pulseCheck === true && event.autoSave === false) {
           await this.assessmentService.pullFastFeedback();
         }
       } else if (this.action === 'review') {
@@ -133,7 +130,7 @@ export class AssessmentMobilePage implements OnInit {
       }
 
       this.savingText$.next($localize `Last saved ${this.utils.getFormatedCurrentTime()}`);
-      if (!event.saveInProgress) {
+      if (!event.autoSave) {
         this.notificationsService.assessmentSubmittedToast();
         // get the latest activity tasks and refresh the assessment submission data
         this.activityService.getActivity(this.activityId);
