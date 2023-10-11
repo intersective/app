e478f2b36973a1171fc466a1727ae7a80e7f678d
chaw <cshen_581@hotmail.com>
2023-10-11 13:52:09 +0800

    [CORE-5977]

--- projects/v3/src/app/services/auth.service.ts
+++ projects/v3/src/app/services/auth.service.ts
@@ -158,7 +158,7 @@ export class AuthService {
     apikey?: string;
   }): Observable<any> {
     this.logout({}, false);
-    return this.authenticate(data).pipe(
+    return this.authenticate({...data, ...{service: 'LOGIN'}}).pipe(
       map(res => this._handleAuthResponse(res)),
     );
   }
