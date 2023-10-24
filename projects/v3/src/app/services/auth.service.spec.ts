2a75a2e083a5dcbb88e96cc3ba71e242cec326a4
chaw <cshen_581@hotmail.com>
2023-10-24 12:30:12 +0800

    [CORE-6129] test updated

--- projects/v3/src/app/services/auth.service.spec.ts
+++ projects/v3/src/app/services/auth.service.spec.ts
@@ -81,38 +81,6 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('login()', () => {
-    it('should pass the correct data to API', () => {
-      requestSpy.post.and.returnValue(of({
-        success: true,
-        data: {
-          tutorial: null,
-          apikey: '123456',
-          Timelines: [
-            {
-              Program: {
-                config: {
-                  theme_color: 'abc'
-                }
-              },
-              Enrolment: {},
-              Project: {},
-              Timeline: {}
-            }
-          ]
-        }
-      }));
-      storageSpy.getConfig.and.returnValue(true);
-      utilsSpy.has.and.returnValue(true);
-
-      service.login({ email: 'test@test.com', password: '123' }).subscribe();
-      expect(requestSpy.post.calls.count()).toBe(1);
-      expect(requestSpy.post.calls.first().args[0].data).toContain('test%40test.com');
-      expect(requestSpy.post.calls.first().args[0].data).toContain('123');
-      expect(storageSpy.setUser).toHaveBeenCalledWith({ apikey: '123456' });
-    });
-  });
-
   it('when testing directLogin(), it should pass the correct data to API', () => {
     requestSpy.post.and.returnValue(of({
       success: true,
@@ -134,7 +102,7 @@ describe('AuthService', () => {
       }
     }));
     storageSpy.getConfig.and.returnValue(true);
-    service.directLogin({ authToken: 'abcd' }).subscribe();
+    service.authenticate({ authToken: 'abcd' }).subscribe();
     expect(requestSpy.post.calls.count()).toBe(1);
     expect(requestSpy.post.calls.first().args[0].data).toContain('abcd');
     expect(storageSpy.setUser.calls.first().args[0]).toEqual({ apikey: '123456' });
@@ -161,7 +129,7 @@ describe('AuthService', () => {
       }
     }));
     storageSpy.getConfig.and.returnValue(true);
-    service.globalLogin({ apikey: 'abcd', service: 'LOGIN' }).subscribe();
+    service.authenticate({ apikey: 'abcd', service: 'LOGIN' }).subscribe();
     expect(requestSpy.post.calls.count()).toBe(1);
     expect(requestSpy.post.calls.first().args[0].data).toContain('abcd');
     expect(storageSpy.setUser.calls.first().args[0]).toEqual({ apikey: '123456' });
