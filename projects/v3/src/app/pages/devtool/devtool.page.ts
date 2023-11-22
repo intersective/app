f533ff51deb417399aeeb2e6aa8dafbc1490b236
chaw <cshen_581@hotmail.com>
2023-11-22 17:58:31 +0800

    dark theme experiment

--- projects/v3/src/app/pages/devtool/devtool.page.ts
+++ projects/v3/src/app/pages/devtool/devtool.page.ts
@@ -13,6 +13,7 @@ import { BrowserStorageService } from '@v3/app/services/storage.service';
 export class DevtoolPage implements OnInit {
   doneLogin: boolean = false;
   user: any = {};
+  themeToggle = false;0
 
   constructor(
     private authService: AuthService,
@@ -27,6 +28,16 @@ export class DevtoolPage implements OnInit {
     if (this.doneLogin) {
       this.user = this.storageService.get('me');
     }
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark theme based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+
   }
 
   refresh() {
@@ -55,5 +66,21 @@ export class DevtoolPage implements OnInit {
 
   async reviewrating() {
     this.notificationsService.popUpReviewRating(1, false);
+  }
+
+  // Check/uncheck the toggle and update the theme based on isDark
+  initializeDarkTheme(isDark) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  }
+
+  // Listen for the toggle check/uncheck to toggle the dark theme
+  toggleChange(ev) {
+    this.toggleDarkTheme(ev.detail.checked);
+  }
+
+  // Add or remove the "dark" class on the document body
+  toggleDarkTheme(shouldAdd) {
+    document.body.classList.toggle('dark', shouldAdd);
   }
 }
