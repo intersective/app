1ec065a938d12e5632a7272112a3c8a854f898ac
chaw <3895281+trtshen@users.noreply.github.com>
2023-12-01 17:48:59 +0800

    [CORE-6216]

--- projects/v3/src/zone-flags.ts
+++ projects/v3/src/zone-flags.ts
@@ -3,3 +3,9 @@
  * running with certain Web Component callbacks
  */
 (window as any).__Zone_disable_customElements = true;
+
+// disable patching requestAnimationFrame
+(window as any).__Zone_disable_requestAnimationFrame = true;
+
+// disable patching specified eventNames
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
