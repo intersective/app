dd7ea7f9edad26c934ed67f6605388507e503d6f
chaw <cshen_581@hotmail.com>
2023-12-05 18:41:11 +0800

    [CORE-6241]

--- projects/v3/src/app/pages/v3/v3.page.ts
+++ projects/v3/src/app/pages/v3/v3.page.ts
@@ -240,6 +240,10 @@ export class V3Page implements OnInit, OnDestroy {
   }
 
   getInstitutionLogo(): string {
+    if (!this.storageService) {
+      return '/assets/logo.svg'; // Default logo or some fallback
+    }
+
     if (this.openMenu !== true) {
       return this.storageService.getUser().squareLogo || '';
     }
