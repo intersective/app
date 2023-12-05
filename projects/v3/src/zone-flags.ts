e4b3fc678d8428a35d3a2a67e2561da3b662dae4
chaw <cshen_581@hotmail.com>
2023-12-05 18:43:21 +0800

    [CORE-6241]

--- projects/v3/src/zone-flags.ts
+++ projects/v3/src/zone-flags.ts
@@ -4,8 +4,3 @@
  */
 (window as any).__Zone_disable_customElements = true;
 
-// disable patching requestAnimationFrame
-(window as any).__Zone_disable_requestAnimationFrame = true;
-
-// disable patching specified eventNames
-(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
