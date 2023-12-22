ec0b09dd24321c335452c7905096f5f98a00c2ad
chaw <3895281+trtshen@users.noreply.github.com>
2023-12-22 11:13:20 +0800

    [CORE-6262] localstorage support

--- projects/v3/src/app/services/unlock-indicator.service.ts
+++ projects/v3/src/app/services/unlock-indicator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { BrowserStorageService } from './storage.service';
 
 interface UnlockedTask {
   milestoneId: string;
@@ -16,27 +17,40 @@ export class UnlockIndicatorService {
   // Expose as an observable for components to subscribe
   public unlockedTasks$ = this.unlockedTasksSubject.asObservable();
 
-  constructor() { }
+  constructor(
+    private storageService: BrowserStorageService,
+  ) {
+    const storedTasks = this.storageService.get('unlockedTasks');
+    if (storedTasks) {
+      this.unlockedTasksSubject.next(storedTasks);
+    }
+  }
 
   // Method to add a new unlocked task
   unlockTask(milestoneId: string, activityId: string, taskId: string) {
     const currentTasks = this.unlockedTasksSubject.getValue();
-    this.unlockedTasksSubject.next([...currentTasks, { milestoneId, activityId, taskId }]);
+
+    const latestTasks = [...currentTasks, { milestoneId, activityId, taskId }];
+    this.storageService.set('unlockedTasks', latestTasks);
+    this.unlockedTasksSubject.next(latestTasks);
   }
 
   // Method to remove an accessed task
-  removeTask(milestoneId: string, activityId: string, taskId: string) {
+  removeTask(milestoneId?: string, activityId?: string, taskId?: string) {
     let currentTasks = this.unlockedTasksSubject.getValue();
     currentTasks = currentTasks.filter(task =>
       task.milestoneId !== milestoneId ||
       task.activityId !== activityId ||
       task.taskId !== taskId
     );
+
+    this.storageService.set('unlockedTasks', currentTasks);
     this.unlockedTasksSubject.next(currentTasks);
   }
 
   // Method to reset the unlocked tasks
   resetTasks() {
+    this.storageService.set('unlockedTasks', []);
     this.unlockedTasksSubject.next([]);
   }
 }
