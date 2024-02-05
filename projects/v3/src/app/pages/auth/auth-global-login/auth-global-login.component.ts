6e28a3e4462b31977c04d6817bba989325eaa20a
sasangachathumal <chathumal.sasanga02@gmail.com>
2024-02-05 15:08:04 +0530

    [CORE-6347] add logout to direct and global login components.

--- projects/v3/src/app/pages/auth/auth-global-login/auth-global-login.component.ts
+++ projects/v3/src/app/pages/auth/auth-global-login/auth-global-login.component.ts
@@ -64,7 +64,8 @@ export class AuthGlobalLoginComponent implements OnInit {
           text: $localize`OK`,
           role: 'cancel',
           handler: () => {
-            this.navigate(['auth', 'login']);
+            // calling auth service logout mentod to clear user data and redirect
+            this.authService.logout();
           }
         }
       ]
