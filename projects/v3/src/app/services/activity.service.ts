b3b5acf4143a37511f4f380e0a0e6f8c57350f9d
trtshen <cshen_581@hotmail.com>
2024-03-12 16:29:30 +0800

    [CORE-6262] revert the meta object checking

--- projects/v3/src/app/services/activity.service.ts
+++ projects/v3/src/app/services/activity.service.ts
@@ -296,7 +296,9 @@ export class ActivityService {
 
     // clear the task from the unlock indicator
     const cleared = this.unlockIndicatorService.removeTask(task.id);
-    this.notification.markTodoItemAsDone(cleared).subscribe();
+    if (cleared) {
+      this.notification.markTodoItemAsDone(cleared).subscribe();
+    }
 
     if (!getData) {
       return ;
