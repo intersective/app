f45a9f16ed5193945b25d83e73ea33749f08711b
trtshen <cshen_581@hotmail.com>
2024-03-18 16:49:51 +0800

    [CORE-6364] isLocked checked (#2106)

--- projects/v3/src/app/components/activity/activity.component.ts
+++ projects/v3/src/app/components/activity/activity.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 import { SharedService } from '@v3/app/services/shared.service';
 import { UnlockIndicatorService } from '@v3/app/services/unlock-indicator.service';
 import { Activity, ActivityService, Task } from '@v3/services/activity.service';
@@ -25,6 +26,7 @@ export class ActivityComponent implements OnInit, OnChanges {
   // false: at least one non-team task
   @Output() cannotAccessTeamActivity = new EventEmitter();
   isForTeamOnly: boolean = false;
+  popupBlocked: boolean = false;
 
   constructor(
     private utils: UtilsService,
@@ -33,6 +35,7 @@ export class ActivityComponent implements OnInit, OnChanges {
     private sharedService: SharedService,
     private activityService: ActivityService,
     private unlockIndicatorService: UnlockIndicatorService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -45,19 +48,44 @@ export class ActivityComponent implements OnInit, OnChanges {
     });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void | Promise<void> {
     if (changes.activity?.currentValue) {
       const currentValue = changes.activity.currentValue;
       const activities = this.storageService.get('activities');
       if (activities) {
         const currentActivity = activities[this.activity.id];
+
+        // if activity is locked, show popup and block access
+        if (currentActivity.isLocked === true && this.popupBlocked === false) {
+          this.popupBlocked = true;
+          return this.notificationsService.alert({
+            message: $localize`This part of the app is still locked. You can unlock the features by engaging with the app and completing all tasks.`,
+            backdropDismiss: false,
+            keyboardClose: false,
+            buttons: [
+              {
+                text: $localize`OK`,
+                handler: () => {
+                  this.popupBlocked = false;
+                  this.router.navigate(['/']);
+                },
+              }
+            ],
+          });
+        }
+
+        // added to prevent multiple popups
+        if (this.popupBlocked === true) {
+          return;
+        }
+
         if (currentActivity?.leadImage) {
           this.leadImage = currentActivity?.leadImage;
         }
       }
 
       if (currentValue.tasks?.length > 0) {
-        this.activityService.nonTeamActivity(changes.activity.currentValue.tasks).then((nonTeamActivity) => {
+        this.activityService.nonTeamActivity(changes.activity.currentValue?.tasks).then((nonTeamActivity) => {
           this.isForTeamOnly = !nonTeamActivity;
           this.cannotAccessTeamActivity.emit(this.isForTeamOnly);
         });
