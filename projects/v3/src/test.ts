e900666da4ca4ad042c7d082d512d956d0cca2ec
trtshen <cshen_581@hotmail.com>
2024-06-25 07:25:49 +0800

    [CORE-6591]

--- projects/v3/src/test.ts
+++ projects/v3/src/test.ts
@@ -22,6 +22,6 @@ getTestBed().initTestEnvironment(
 );
 
 // Then we find all the tests.
-const context = require.context('./', true, /assessment\.service\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
