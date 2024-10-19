badb953d70af771ca4d1e60d39551e3c3f83a38e
trtshen <cshen_581@hotmail.com>
2024-10-19 20:23:52 +0800

    trigger modal once click 'self'

--- projects/v3/src/app/services/fast-feedback.service.ts
+++ projects/v3/src/app/services/fast-feedback.service.ts
@@ -4,12 +4,12 @@ import { NotificationsService } from './notifications.service';
 import { BrowserStorageService } from '@v3/services/storage.service';
 import { UtilsService } from '@v3/services/utils.service';
 import { of, from, Observable } from 'rxjs';
-import { switchMap, delay, take, retryWhen } from 'rxjs/operators';
+import { switchMap, delay, take, retryWhen, map } from 'rxjs/operators';
 import { environment } from '@v3/environments/environment';
 import { DemoService } from './demo.service';
+import { ApolloService } from './apollo.service';
 
 const api = {
-  fastFeedback: 'api/v2/observation/slider/list.json',
   submit: 'api/v2/observation/slider/create.json',
 };
 
@@ -22,14 +22,36 @@ export class FastFeedbackService {
     private notificationsService: NotificationsService,
     private storage: BrowserStorageService,
     private utils: UtilsService,
-    private demo: DemoService
+    private demo: DemoService,
+    private apolloService: ApolloService,
   ) {}
 
   private _getFastFeedback() {
     if (environment.demo) {
       return this.demo.fastFeedback();
     }
-    return this.request.get(api.fastFeedback);
+    return this.apolloService.graphQLFetch(
+      `query pulseCheck {
+        pulseCheck {
+          questions {
+            id
+            name
+            description
+            choices {
+              id
+              name
+            }
+          }
+          meta {
+            teamId
+            teamName
+            targetUserId
+            contextId
+            assessmentName
+          }
+        }
+      }`
+    );
   }
 
   pullFastFeedback(options= {
@@ -42,19 +64,19 @@ export class FastFeedbackService {
 
         // if any of either slider or meta is empty or not available,
         // should just skip the modal popup
-        const { slider, meta } = res.data;
-        if (this.utils.isEmpty(slider) || this.utils.isEmpty(meta)) {
+        const { questions, meta } = res.data.pulseCheck;
+        if (this.utils.isEmpty(questions) || this.utils.isEmpty(meta)) {
           return of(res);
         }
 
         // popup instant feedback view if question quantity found > 0
-        if (!this.utils.isEmpty(res.data) && res.data.slider.length > 0 && !fastFeedbackIsOpened) {
+        if (!this.utils.isEmpty(res.data) && res.data.questions.length > 0 && !fastFeedbackIsOpened) {
           // add a flag to indicate that a fast feedback pop up is opening
           this.storage.set('fastFeedbackOpening', true);
 
           return from(this.notificationsService.fastFeedbackModal(
             {
-              questions: res.data.slider,
+              questions: res.data.questions,
               meta: res.data.meta
             },
             options.modalOnly,
@@ -74,11 +96,18 @@ export class FastFeedbackService {
       console.log('data', data, 'params', params);
       return this.demo.normalResponse();
     }
-    return this.request.post(
+    return this.apolloService.graphQLMutate(
+      `mutation submitPulseCheck($teamId: Int, $targetUserId: Int, $contextId: Int, $answers: [PulseCheckAnswerInput]) {
+        submitPulseCheck(teamId: $teamId, targetUserId: $targetUserId, contextId: $contextId, answers: $answers) {
+          success
+        }
+      }`,
+    );
+    /* return this.request.post(
       {
         endPoint: api.submit,
         data,
         httpOptions: { params }
-      });
+      }); */
   }
 }
