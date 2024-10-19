e55761d8dc7430e47d3631b6e51e69a524204699
trtshen <cshen_581@hotmail.com>
2024-10-19 20:23:52 +0800

    trigger modal once click 'self'

--- projects/v3/src/app/components/traffic-light-group/traffic-light-group.component.ts
+++ projects/v3/src/app/components/traffic-light-group/traffic-light-group.component.ts
@@ -1,16 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { FastFeedbackService } from "@v3/services/fast-feedback.service";
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-traffic-light-group',
-  templateUrl: './traffic-light-group.component.html',
-  styleUrls: ['./traffic-light-group.component.scss']
+  selector: "app-traffic-light-group",
+  templateUrl: "./traffic-light-group.component.html",
+  styleUrls: ["./traffic-light-group.component.scss"],
 })
-
 export class TrafficLightGroupComponent {
+  // @Input() lights: { groupLabel: string, group: { value: number | null, label: string}[] };
   @Input() lights: {
     self: any;
     expert: any;
     team: any;
   };
-  // @Input() lights: { groupLabel: string, group: { value: number | null, label: string}[] };
+  loading: {
+    [key: string]: boolean;
+  } = {};
+
+  constructor(private fastFeedbackService: FastFeedbackService) {}
+
+  navigateToPulseCheck(type: string) {
+    if (!this.loading[type]) {
+      this.loading[type] = true;
+      this.fastFeedbackService.pullFastFeedback().subscribe({
+        next: (response) => {
+          if (response) {
+            console.log(`Pulled fast feedback for type ${type}:`, response);
+          }
+        },
+        error: (error) => {
+          console.error(`Error pulling fast feedback for type ${type}:`, error);
+        },
+        complete: () => {
+          this.loading[type] = false;
+        },
+      });
+    }
+  }
 }
